62cbcbaed3193be984730fb8789c55713899e5d1
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-11 03:08:33 -0300

    Update buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] Ingresa un texto para buscar, ejemplo: ${usedPrefix + command} TheMystic-Bot-MD*`;
+  if (!text) throw `*Ingresa un texto para buscar, ejemplo: ${usedPrefix + command} Jotchua*`;
   const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
     q: text,
   }));
@@ -10,12 +10,16 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   const str = json.items.map((repo, index) => {
   return `
 *${1 + index}. ${repo.full_name}${repo.fork ? ' (fork)' : ''}*
-🔗 *Url:* ${repo.html_url}
-📅 *Creado el:* ${formatDate(repo.created_at)}
-🔄 *Actualizado el:* ${formatDate(repo.updated_at)}
-📥 *Clone:* $ git clone ${repo.clone_url}
-👁 ${repo.watchers} ◉ 🍴 ${repo.forks} ◉ ⭐ ${repo.stargazers_count} ◉ ❓ 
-${repo.description ? `📝 *Descripción:*\n${repo.description}` : ''}
+*Url:* ${repo.html_url}
+
+*Creado el:* ${formatDate(repo.created_at)}
+
+*Actualizado el:* ${formatDate(repo.updated_at)}
+
+*Clone:* $ git clone ${repo.clone_url}
+
+👁 ${repo.watchers}  🍴 ${repo.forks}  ⭐ ${repo.stargazers_count}  ❓ 
+${repo.description ? ` *Descripción:*\n${repo.description}` : ''}
 `.trim()}).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendMessage(m.chat, {text: str.trim()}, {quoted: m})
 //conn.sendMessage(m.chat, {text: str.trim(), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});  

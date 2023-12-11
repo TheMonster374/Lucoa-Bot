54daf7048c3a9c8f6cf655a4f0a32a1e1cbc445e
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-11 03:34:12 -0300

    Update propietario-actualizar.js

--- plugins/propietario-actualizar.js
+++ plugins/propietario-actualizar.js
@@ -4,8 +4,8 @@ const handler = async (m, { conn, text }) => {
   try {
           const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
           let messager = stdout.toString()
-          if (messager.includes('Already up to date.')) messager = '_*< PROPIETARIO - UPDATE />*_\n\n*[ ✅ ] No hay actualizaciones pendientes.*'
-          if (messager.includes('Updating')) messager = '_*< PROPIETARIO - ACTUALIZAR />*_\n\n*[ ℹ️ ] Actualización finalizada exitosamente.*\n\n' + stdout.toString()
+          if (messager.includes('Already up to date.')) messager = '_*PROPIETARIO - UPDATE*_\n\n*[ ✅ ] No hay actualizaciones pendientes.*'
+          if (messager.includes('Updating')) messager = '_*PROPIETARIO - ACTUALIZAR*_\n\n*Actualización finalizada exitosamente.*\n\n' + stdout.toString()
           conn.reply(m.chat, messager, m);
   } catch {      
  try {    
@@ -23,13 +23,13 @@ const handler = async (m, { conn, text }) => {
           })
           .filter(Boolean);
         if (conflictedFiles.length > 0) {
-          const errorMessage = `_*< PROPIETARIO - ACTUALIZAR />*_\n\n*[ ℹ️ ] Se han hecho cambios locales en archivos del bot que entran en conflicto con las actualizaciones del repositorio. Para actualizar, reinstala el bot o realiza las actualizaciones manualmente.*\n\n*Archivos en conflicto:*\n\n${conflictedFiles.join('\n')}.*`;
+          const errorMessage = `_*PROPIETARIO - ACTUALIZAR*_\n\n*Se han hecho cambios locales en archivos del bot que entran en conflicto con las actualizaciones del repositorio. Para actualizar, reinstala el bot o realiza las actualizaciones manualmente.*\n\n*Archivos en conflicto:*\n\n${conflictedFiles.join('\n')}.*`;
           await conn.reply(m.chat, errorMessage, m);  
         }
       }
   } catch (error) {
     console.error(error);
-    let errorMessage2 = '_*< PROPIETARIO - ACTUALIZAR />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';
+    let errorMessage2 = '_*< PROPIETARIO - ACTUALIZAR />*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';
     if (error.message) {
       errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
     }

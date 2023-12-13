87e87a0ab79112115a365b1acbe6a45959501e93
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-13 17:53:08 -0300

    Update _simsimi.js

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -1,32 +1,30 @@
+import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
-const handler = (m) => m;
-
-handler.before = async (m) => {
-  const chat = global.db.data.chats[m.chat];
-  if (chat.simi) {
-    if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
-    let textodem = m.text;
+const handler = async (m, {text, command, args, usedPrefix}) => {
+  if (!text) return conn.reply(m.chat, `*🚩 Ingresa un texto para empezar a hablar con el Bot*`, m, adReply)
+  try {
+    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+    const resSimi = await api.json();
+    m.reply(resSimi.success);
+  } catch {
     try {
-      const ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`);
-      const data = await ressimi.json();
-      if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`); /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
-      await m.reply(data.success);
-    } catch {
-      /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
-      if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
-      if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
-      if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
-      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + textodem);
+      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
+      if (text.includes('hola')) text = text.replace('hola', 'Hello');
+      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
       const resu = await reis.json();
       const nama = m.pushName || '1';
       const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
       const res = await api.json();
       const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
       const resu2 = await reis2.json();
-      await m.reply(resu2[0][0][0]);
+      await conn.reply(m.chat, resu2[0][0][0], m, adReply)
+    } catch {
+      return conn.reply(m.chat, `*☓ Ocurrió un error inesperado*`, m, adReply)
     }
-    return !0;
   }
-  return true;
 };
+handler.help = ['simi', 'bot']
+handler.tags = ['fun'];
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
 export default handler;

702bd6796be9947850f7611a57187dfcacf1f6a1
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-14 10:59:32 -0300

    Update gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -7,8 +7,8 @@ const handler = async (m, {conn, usedPrefix, text}) => {
     var number = text;
   }
 
-  if (!text && !m.quoted) return conn.reply(m.chat, `*[❗] USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin -> responder a un mensaje*\n*┷┯*`, m);
-  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] El número ingresado es incorrecto, por favor ingrese el número correcto*`, m);
+  if (!text && !m.quoted) return conn.reply(m.chat, `*USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin -> responder a un mensaje*\n*┷┯*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*El número ingresado es incorrecto, por favor ingrese el número correcto*`, m);
 
   try {
     if (text) {

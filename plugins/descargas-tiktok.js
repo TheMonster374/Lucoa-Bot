da915368e6cfe1bd123ef4294c9a02103013a5b0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-16 08:00:01 -0300

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,33 +1,33 @@
- 
+
 import fg from 'api-dylux'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     
-        if (!args[0]) throw `✳️ ${mssg.noLink('TikTok')}\n\n 📌 ${mssg.example} : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
-        if (!args[0].match(/tiktok/gi)) throw `❎ ${mssg.noLink('TikTok')}`
+        if (!args[0]) throw `✳️ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMjkj76X6/`
+        if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
         m.react(rwait)
       try {
         let res = await fetch(global.API('fgmods', '/api/downloader/tiktok2', { url: args[0] }, 'apikey'))
         let data = await res.json()
-
+        
         if (data.result.video) {
             let tex = `
-┌─⊷ *TIKTOK DL* 
-▢ *${mssg.name}:* ${data.result.author.name}
-▢ *${mssg.username}:* ${data.result.author.unique_id}
-▢ *${mssg.duration}:* ${data.result.video.durationFormatted}
-▢ *${mssg.quality}:* ${data.result.video.ratio}
+┌─⊷ *TIKTOK DL*
+▢ *Nombre:* ${data.result.author.name}
+▢ *Username:* ${data.result.author.unique_id}
+▢ *Duración:* ${data.result.video.durationFormatted}
+▢ *Calidad:* ${data.result.video.ratio}
 ▢ *Likes:* ${data.result.stats.likeCount}
-▢ *${mssg.views}:* ${data.result.stats.playCount}
-▢ *${mssg.desc}:* ${data.result.title}
+▢ *Vistas:* ${data.result.stats.playCount}
+▢ *Descripción:* ${data.result.title}
 └───────────
 `
             conn.sendFile(m.chat, data.result.video.noWatermark, 'tiktok.mp4', tex, m);
             m.react(done)
-        } else {
+   } else {
             let cap = `
+▢ *Descripción:* ${data.result.title}
 ▢ *Likes:* ${data.result.stats.likeCount}
-▢ *${mssg.desc}:* ${data.result.title}
 `
             for (let tt of data.result.images) {
                 conn.sendMessage(m.chat, { image: { url: tt.url }, caption: cap }, { quoted: m })
@@ -36,10 +36,9 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
             m.react(done)
         }
     } catch (error) {
-        m.reply(`❎ ${mssg.error}`)
+        m.reply(`❎ Error al descargar el video`)
     }
 }
-
 handler.help = ['tiktok']
 handler.tags = ['dl']
 handler.command = ['tiktok', 'tt', 'tiktokimg', 'tiktokslide']

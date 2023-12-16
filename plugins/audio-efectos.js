deb0b02e399d5a3b8812eff4a69f0728de5a39a0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-16 08:21:23 -0300

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -32,7 +32,7 @@ const handler = async (m, {conn, args, __dirname, usedPrefix, command}) => {
           ptt: true,
         });
       });
-    } else throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝙰𝚄𝙳𝙸𝙾 𝙾 𝙽𝙾𝚃𝙰 𝙳𝙴 𝚅𝙾𝚉 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙳𝙾, 𝚄𝚂𝙰𝙳𝙾 𝙴𝙻 𝙲𝙾𝙰𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+    } else throw `*responda al audio o nota de voz que sera modificado, usando el comando ${usedPrefix + command}*`;
   } catch (e) {
     throw e;
   }

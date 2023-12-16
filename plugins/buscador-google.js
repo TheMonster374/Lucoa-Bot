8d2e58dfd2b5d84a92322c4fd26fe9dd62d7214f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-16 09:15:48 -0300

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 let handler = async (m, { conn, command, args, usedPrefix }) => {
   const fetch = (await import('node-fetch')).default;
   const text = args.join` `;
-  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+  if (!text) return conn.reply(m.chat, '*ingrese el texto que desee buscar*', m);
 const url = 'https://google.com/search?q=' + encodeURIComponent(text);
 google({'query': text}).then(res => {
 let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n`
@@ -26,7 +26,7 @@ import axios from 'axios';
 const handler = async (m, {conn, command, args}) => {
   const fetch = (await import('node-fetch')).default;
   const text = args.join` `;
-  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+  if (!text) return conn.reply(m.chat, '*ingrese el texto que desee buscar*', m);
   const url = 'https://google.com/search?q=' + encodeURIComponent(text);
   const search = await googleIt(text);
   const msg = search.articles.map(({title, url, description}) => {

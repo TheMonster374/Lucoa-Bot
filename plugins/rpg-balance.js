91dfef04f42369369e6d60893ce32b2b7491c3fe
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-16 07:33:06 -0300

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -13,7 +13,7 @@ let handler = async (m, {conn, usedPrefix}) => {
 ▢  *tokens*: ${user.joincount}
 ▢ *📍rango* : ${user.role}
 ▢ *📍nivel* : ${user.level}
-▢ *⚜️premium*:* ${user.premiumTime > 0 ? '✅' : (user.isPrems ? '✅' : '❌') || ''}
+▢ *🎟️premium*:* ${user.premiumTime > 0 ? '✅' : (user.isPrems ? '✅' : '❌') || ''}
 └──────────────
 
 *NOTA :* 

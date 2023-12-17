211b719023b3c07956ac25cfd3a09625a4553bcc
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-17 02:49:54 -0300

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,11 +1,11 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {args}) {
-  if (!args[0]) throw '*[❗𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚂𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴, 𝚂𝙸 𝙽𝙾 𝙻𝙾 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰 𝙿𝚄𝙴𝙳𝙴 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #myns*';
+  if (!args[0]) throw '*Ingrese el numero de serie, si no lo recuerda usar el comando #myns*';
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw '*[❗𝐍𝐅𝐎❗] 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾, 𝙲𝙾𝙼𝙿𝚁𝚄𝙴𝙱𝙴 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝚈𝙰 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴!*\n\n*𝚂𝙸 𝙽𝙾 𝙻𝙾 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰 𝙿𝚄𝙴𝙳𝙴 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #myns*';
+  if (args[0] !== sn) throw '*numero de serie incorrecto, compruebe que lo haya escrito correctamente!*\n\n*si no lo recuerda puede usar el comando #myns*';
   user.registered = false;
-  m.reply(`*[ ✔ ] 𝚂𝙴 𝚁𝙴𝙰𝙻𝙸𝚉𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾, 𝚄𝚂𝚃𝙴𝙳 𝚈𝙰 𝙽𝙾 𝙴𝚂𝚃𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃*`);
+  m.reply(`*𝚂𝙴 𝚁𝙴𝙰𝙻𝙸𝚉𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾, 𝚄𝚂𝚃𝙴𝙳 𝚈𝙰 𝙽𝙾 𝙴𝚂𝚃𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃*`);
 };
 handler.help = ['', 'ister'].map((v) => 'unreg' + v + ' <numero de serie>');
 handler.tags = ['xp'];

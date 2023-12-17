5cd99c9e684d0e61284e0ca55a2d6600bba79152
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-16 21:26:36 -0300

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -65,7 +65,7 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
   }
 };
 handler.command = /^(play|play2)$/i;
-handler.diamond = 2
+handler.limit = 2
 export default handler;
 
 async function search(query, options = {}) {

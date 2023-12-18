87d571154793c1558459a35963bcc11f61b4c560
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-17 22:28:59 -0300

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
+  const text = `*tutorial de boxminehost*
 > Tutorial: https://youtu.be/eC9TfKICpcY
 > Pagina Oficial: https://boxmineworld.com
 > Dashboard: https://dash.boxmineworld.com/home
@@ -9,53 +9,53 @@ const handler = async (m, {conn, usedPrefix}) => {
 > Soporte: https://discord.gg/84qsr4v 
 
 ------------------------------------
-—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻-𝚃𝙴𝚁𝙼𝚄𝚇
+tutorial termux
 > https://youtu.be/yRS4m36Zwkw
 
 ------------------------------------
 
-*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+*comandos termux*
 > Comandos:
 - cd && termux-setup-storage
 - apt-get update -y && apt-get upgrade -y
 - pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
-- git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git && cd TheMystic-Bot-MD
+- git clone https://github.com/AleXD0009/Jotchua.git && cd Jotchua
 - yarn install
 - npm install
 - npm update
 - npm start
-
+NOTA: en caso de no funcionar intentar con yarn y luego npm start
 ------------------------------------
 
-—◉ ✔️ ACTIVAR EN CASO DE DETENERSE EN TERMUX ✔️
-ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
-> cd TheMystic-Bot-MD
+*activar en caso de detenerse en termux*
+escibre los siguientes comandos uno por uno
+> cd Jotchua
 > npm start
 
 ------------------------------------
 
-—◉ 👽 OBTENER OTRO CODIGO QR EN TERMUX 👽
-ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
-> cd TheMystic-Bot-MD
+*obtener otro codigo en termux*
+escribe los comandos uno por uno:
+> cd Jotchua
 > rm -rf MysticSession
 > npm start`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://github.com/AleXD0009/Jotchua`},
     'mimetype': `application/${document}`,
-    'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+    'fileName': `「 Jotchua-Bot 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://github.com/AleXD0009/Jotchua',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': 'https://github.com/AleXD0009'}},
     'caption': text,
     'footer': wm,
     // 'buttons':[

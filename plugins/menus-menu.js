63d9969dbe013465fd04f4d976b88096a186052f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-20 09:14:40 -0300

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -31,7 +31,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 ╭────────────────
 │𝙏𝙞𝙚𝙢𝙥𝙤 𝙖𝙘𝙩𝙞𝙫𝙤: ${uptime}
-│*Registrados* : ${totalreg} Usuarios
+│
 ╰────────────────         
      
  ⟼ _${usedPrefix}owner_

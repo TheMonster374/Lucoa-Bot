fed48d957c47b248608d1ac488d90c5714149918
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-22 08:37:50 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -12,7 +12,6 @@ var handler = async (m, {conn, args, text, command, usedPrefix}) => {
 *Dislikes :* ${ress.dislike}`;*/
      let quality = ress.media.find(item => item.quality === 480);
    if (quality.quality == 480) {
-    conn.sendMessage(m.chat, {document: {url: quality.url}, fileName: ress.title + '.mp4', mimetype: 'video/mp4'}, {quoted: m})
   };
 };
 handler.command = /^(porndl)$/i;

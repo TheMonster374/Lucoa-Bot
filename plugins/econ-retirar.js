ca8f8d346c54896c86fbc192a8dea951f90bd620
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-23 11:48:14 -0300

    Update econ-retirar.js

--- plugins/econ-retirar.js
+++ plugins/econ-retirar.js
@@ -1,13 +1,13 @@
 const xppercredit = 1
 let handler = async (m, { conn, command, args }) => {
     let count = command.replace(/^(wt|withdraw)/i, '')
-    count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / xppercredit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+    count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / xppercredit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
     count = Math.max(1, count)
-    if (global.db.data.users[m.sender].limit >= xppercredit * count) {
-      global.db.data.users[m.sender].limit -= xppercredit * count
-      global.db.data.users[m.sender].limit += count
-      conn.reply(m.chat, `transferiste 💎 ${count} diamantes a tu biletera`, m)
-    } else conn.reply(m.chat, `*No tienes suficiente cantidad de diamantes💎 en tu banco para realizar esta transacción*`, m)
+    if (global.db.data.users[m.sender].money >= xppercredit * count) {
+      global.db.data.users[m.sender].money -= xppercredit * count
+      global.db.data.users[m.sender].money += count
+      conn.reply(m.chat, `transferiste  ${count} coins 🪙 a tu biletera`, m)
+    } else conn.reply(m.chat, `*No tienes suficiente cantidad de coins🪙 en tu banco para realizar esta transacción*`, m)
   }
   handler.help = ['withdraw']
   handler.tags = ['economy']

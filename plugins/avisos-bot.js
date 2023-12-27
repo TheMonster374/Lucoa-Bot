90414bbf410e630147ae96f5e0d1fb7f1ccdf175
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2023-12-27 15:37:57 -0300

    Update avisos-bot.js

--- plugins/avisos-bot.js
+++ plugins/avisos-bot.js
@@ -23,11 +23,12 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     Cosas nuevas sobre el bot:
     
     Comandos nuevos:
-    ╭────────────────
-    │.dep
-    │.bank/banco
-    │.withdraw
-    ╰────────────────`.trim();
+╭────────────────
+│.dep
+│.bank/banco
+│.withdraw
+│.instagram
+╰────────────────`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};

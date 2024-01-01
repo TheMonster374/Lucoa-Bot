18cb3eac7d9a807d8cdc3f11287cfa8cd1f583bd
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-01 06:05:34 -0300

    Update owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,31 +1,26 @@
-import {tmpdir} from 'os';
-import path, {join} from 'path';
+import { tmpdir } from 'os'
+import path, { join } from 'path'
 import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch,
-} from 'fs';
-const handler = async (m, {conn, usedPrefix: _p, __dirname, args}) => {
-  conn.reply(m.chat, '*ARCHIVOS DE LA CARPETA TMP ELIMINADOS CON EXITO!!*', m);
+readdirSync,
+statSync,
+unlinkSync,
+existsSync,
+readFileSync,
+watch
+} from 'fs'
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 
-  const tmp = [tmpdir(), join(__dirname, '../tmp')];
-  const filename = [];
-  tmp.forEach((dirname) => readdirSync(dirname).forEach((file) => filename.push(join(dirname, file))));
-  return filename.map((file) => {
-    const stats = statSync(file);
-    unlinkSync(file);
-  });
-};
-handler.help = ['cleartmp'];
-handler.tags = ['owner'];
-handler.command = /^(cleartmp|cleartemp)$/i;
-handler.rowner = true;
-export default handler;
-handler.help = ['cleartmp'];
-handler.tags = ['owner'];
-handler.command = /^(cleartmp|cleartemp)$/i;
-handler.rowner = true;
-export default handler;
+conn.reply(m.chat, '*[ ✔ ] ARCHIVOS DE LA CARPETA TMP ELIMINADOS CON EXITO!!*', m)
+
+const tmp = [tmpdir(), join(__dirname, '../tmp')]
+const filename = []
+tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+return filename.map(file => {
+const stats = statSync(file)
+unlinkSync(file)
+})}
+handler.help = ['cleartmp']
+handler.tags = ['owner']
+handler.command = /^(cleartmp|cleartemp)$/i
+handler.rowner = true
+export default handler

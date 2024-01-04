88a1008c589e340831eed19cfd40fd7f2a619256
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-04 13:18:07 -0300

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -8,7 +8,7 @@ let limit2 = 400;
 let limit_a1 = 50;
 let limit_a2 = 400;
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-  if (!text) throw `_*⛄𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃♔わ⛄ - PLAY*_\n\n*Hace falta el título o enlace del video de YouTube.*\n\n*Ejemplo:* _${usedPrefix + command} turn it up (feat. 6arelyhuman)_`;
+  if (!text) throw `_*DESCARGA - PLAY*_\n\n*Hace falta el título o enlace del video de YouTube.*\n\n*Ejemplo:* _${usedPrefix + command} turn it up (feat. 6arelyhuman)_`;
     const yt_play = await search(args.join(' '));
     let additionalText = '';
     if (command === 'play') {
@@ -16,11 +16,11 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     } else if (command === 'play2') {
       additionalText = 'vídeo';
     }
-    const texto1 = `_*⛄𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃♔わ⛄- PLAY*_\n\n*Título:* ${yt_play[0].title}\n\n▢ *Publicado:* ${yt_play[0].ago}\n\n▢ *Duración:* ${secondString(yt_play[0].duration.seconds)}\n\n▢ *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}\n\n▢ *Autor:* ${yt_play[0].author.name}\n\n▢ *ID:* ${yt_play[0].videoId}\n\n▢ *Tipo:* ${yt_play[0].type}\n\n▢ *Enlace:* ${yt_play[0].url}\n\n▢ *Canal:* ${yt_play[0].author.url}\n\n*[ ℹ️ ] Se está enviando el ${additionalText}. espere...*`.trim();
+    const texto1 = `_*< DESCARGAS - PLAY />*_\n\n▢ *Título:* ${yt_play[0].title}\n\n▢ *Publicado:* ${yt_play[0].ago}\n\n▢ *Duración:* ${secondString(yt_play[0].duration.seconds)}\n\n▢ *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}\n\n▢ *Autor:* ${yt_play[0].author.name}\n\n▢ *ID:* ${yt_play[0].videoId}\n\n▢ *Tipo:* ${yt_play[0].type}\n\n▢ *Enlace:* ${yt_play[0].url}\n\n▢ *Canal:* ${yt_play[0].author.url}\n\n*Se está enviando el ${additionalText}. espere...*`.trim();
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {   
-    const audio = global.API('ApiEmpire', `/api/v1/ytmp3?url=${yt_play[0].url}`)
+    const audio = global.API('CFROSAPI', `/api/v1/ytmp3?url=${yt_play[0].url}`)
     const ttl = await yt_play[0].title
     const buff_aud = await getBuffer(audio);
     const fileSizeInBytes = buff_aud.byteLength;
@@ -38,11 +38,11 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     await conn.sendMessage(m.chat, {audio: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3`}, {quoted: m});   
     return;    
     }} catch {
-    throw '_*⛄𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃♔わ⛄ - PLAY*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';    
+    throw '_*DESCARGAS - PLAY*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';    
     }}
     if (command == 'play2') {
     try {   
-    const video = global.API('ApiEmpire', `/api/v1/ytmp4?url=${yt_play[0].url}`)
+    const video = global.API('CFROSAPI', `/api/v1/ytmp4?url=${yt_play[0].url}`)
     const ttl2 = await yt_play[0].title
     const buff_vid = await getBuffer(video);
     const fileSizeInBytes2 = buff_vid.byteLength;
@@ -50,7 +50,7 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     const fileSizeInMB2 = fileSizeInKB2 / 1024;
     const size2 = fileSizeInMB2.toFixed(2);       
     if (size2 >= limit2) {  
-    await conn.sendMessage(m.chat, {text: `_*⛄𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃♔わ⛄ - PLAY*_\n\n*Descargue su vídeo en ${video}*`}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: `_*DESCARGA - PLAY*_\n\n*Descargue su vídeo en ${video}*`}, {quoted: m});
     return;    
     }     
     if (size2 >= limit1 && size2 <= limit2) {  
@@ -60,12 +60,11 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     await conn.sendMessage(m.chat, {video: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4`}, {quoted: m});   
     return;    
     }} catch {
-    throw '_*⛄𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐛𝐨𝐭-𝐌𝐃♔わ⛄ - PLAY*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';    
+    throw '_*DESCARGA - PLAY*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';    
     }
   }
 };
-handler.command = /^(play|play2)$/i;
-handler.limit = 1
+handler.command = /^(play|play2|musica)$/i;
 export default handler;
 
 async function search(query, options = {}) {

1a3011e7e563fa62b4afdf18f04e0cfddfceea49
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-05 16:29:51 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkg install yarn
+git clone https://github.com/AleXD0009/Jotchua-Test
+cd Jotchua-Test
+yarn install
+npm install
+npm start

2801a702f62f6eca7232ab67be92941396bd45f8
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-05 12:19:53 -0300

    Jotchua-Bot🐶

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/a.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `Jotchua-Bot`, "body": `=> ᴀᴜᴅɪᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴏ`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/AleXD0009/Jotchua`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `Jotchua-Bot`, "body": `=> ᴀᴜᴅɪᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴏ`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/AleXD0009/Jotchua-Bot`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
 handler.customPrefix = /ª|a|A/
 handler.command = /^(a|ª|A?$)/
 export default handler

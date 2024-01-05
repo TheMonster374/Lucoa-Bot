f1c94318b586b34b2920312a8034eb98dceb28b0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-05 15:43:03 -0300

    Update grupos-eliminar.js

--- plugins/grupos-eliminar.js
+++ plugins/grupos-eliminar.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, '🎌 *Etiquete o responda al mensaje de la persona que quiere eliminar*', m, fake, ) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+await conn.groupParticipants(m.chat, [user], 'remove')
 
 }
 handler.help = ['kick']

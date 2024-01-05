4c9b0559349b0fef91985ebdf808b4ebe97f7dbe
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-05 16:01:01 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,76 +1,31 @@
-import translate from '@vitalets/google-translate-api'
-import fetch from 'node-fetch'
-let handler = async (m, {conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] INGRESE UN TEXTO PARA HABLAR CON SIMSIMI O EL BOT*\n\n*EJEMPLO: ${usedPrefix + command} Hola bot*`
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
+const handler = async (m, {text, command, args, usedPrefix}) => {
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Gata Bot*`
 try {
-let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
-let json = await res.json()
-let resp = json.success.trim()
-let txt = '';
-        let count = 0;
-        for (const c of resp) {
-            await new Promise(resolve => setTimeout(resolve, 50));
-            txt += c;
-            count++;
-        
-            if (count % 10 === 0) {
-                conn.sendPresenceUpdate('composing' , m.chat);
-            }
-        }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
+await conn.sendPresenceUpdate('composing', m.chat)
+const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+const resSimi = await api.json();
+m.reply(resSimi.success);
 } catch {
-    try {
-    if (text.includes('Hola')) text = text.replace('Hola', 'Hello')
-    if (text.includes('hola')) text = text.replace('hola', 'Hello')
-    if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO')    
-    let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
-    let resu = await reis.json()  
-    let nama = m.pushName || '1'
-    let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
-    let res = await api.json()
-    let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
-    let resu2 = await reis2.json()
-    let resp = resu2[0][0][0]
-    let txt = '';
-        let count = 0;
-        for (const c of resp) {
-            await new Promise(resolve => setTimeout(resolve, 50));
-            txt += c;
-            count++;
-        
-            if (count % 10 === 0) {
-                conn.sendPresenceUpdate('composing' , m.chat);
-            }
-        }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
-    //m.reply(resu2[0][0][0])
+try {
+if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
+if (text.includes('hola')) text = text.replace('hola', 'Hello');
+if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
+const resu = await reis.json();
+const nama = m.pushName || '1';
+const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+const res = await api.json();
+const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+const resu2 = await reis2.json();
+m.reply(resu2[0][0][0]);
 } catch (e) {
-    console.log(`este es el error del codigo anterior: ${e}`)
-/*let res2 = await fetch(`https://api.phamvandien.xyz/sim?type=ask&ask=${text}`)  
-let json2 = await res2.json()
-let result = json2.answer*/  
-let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
-  let resuuu = await reisss.json()
-let res2 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)  
-let json2 = await res2.json()
-let result = json2.result
-let lol = await translate(`${result}`, { to: 'es', autoCorrect: true })
-let txt = '';
-        let count = 0;
-        for (const c of lol.text.trim()) {
-            await new Promise(resolve => setTimeout(resolve, 50));
-            txt += c;
-            count++;
-        
-            if (count % 10 === 0) {
-                conn.sendPresenceUpdate('composing' , m.chat);
-            }
-        }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
-
-}}
-}
-handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
-handler.tags = ['fun']
-handler.command = /
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}}}
+handler.help = ['simsimi']
+handler.tags = ['General']
+handler.command = ['bot', 'simi', 'simsimi'] 
 export default handler

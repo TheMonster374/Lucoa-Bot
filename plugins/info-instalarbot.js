9652c5a059d92cee1649c7165410c2a3ee585caa
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-05 12:31:11 -0300

    Jotchua-Bot🐶

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,13 +1,7 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*tutorial de boxminehost*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-> Soporte: https://discord.gg/84qsr4v 
-
+  const text = `
 ------------------------------------
 tutorial termux
 > https://youtu.be/yRS4m36Zwkw
@@ -19,7 +13,7 @@ tutorial termux
 - cd && termux-setup-storage
 - apt-get update -y && apt-get upgrade -y
 - pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
-- git clone https://github.com/AleXD0009/Jotchua.git && cd Jotchua
+- git clone https://github.com/AleXD0009/Jotchua-Bot.git && cd Jotchua-Bot
 - yarn install
 - npm install
 - npm update
@@ -29,18 +23,18 @@ NOTA: en caso de no funcionar intentar con yarn y luego npm start
 
 *activar en caso de detenerse en termux*
 escibre los siguientes comandos uno por uno
-> cd Jotchua
+> cd Jotchua-Bot
 > npm start
 
 ------------------------------------
 
 *obtener otro codigo en termux*
 escribe los comandos uno por uno:
-> cd Jotchua
+> cd Jotchua-Bot
 > rm -rf MysticSession
 > npm start`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/AleXD0009/Jotchua`},
+    'document': {url: `https://github.com/AleXD0009/Jotchua-Bot`},
     'mimetype': `application/${document}`,
     'fileName': `「 Jotchua-Bot 」`,
     'fileLength': 99999999999999,
@@ -49,10 +43,10 @@ escribe los comandos uno por uno:
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/AleXD0009/Jotchua',
+        'mediaUrl': 'https://github.com/AleXD0009/Jotchua-Bot',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+        'title': 'ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
         'sourceUrl': 'https://github.com/AleXD0009'}},
@@ -64,5 +58,5 @@ escribe los comandos uno por uno:
     'headerType': 6};
   conn.sendMessage(m.chat, buttonMessage, {quoted: m});
 };
-handler.command = ['instalarbot', 'instalarbot'];
+handler.command = ['instalarbot', 'infoinstalar'];
 export default handler;

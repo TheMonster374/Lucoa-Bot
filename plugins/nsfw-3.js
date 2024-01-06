298787c12d3fcf22f4fe4db201951fe57fd83fd7
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-06 11:08:09 -0300

    Update nsfw-3.js

--- plugins/nsfw-3.js
+++ plugins/nsfw-3.js
@@ -4,5 +4,5 @@ let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
 handler.tags = ['nsfw']
 handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
-//buatan hyzer, jgn hapus atuh 😊
+
 module.exports = handler

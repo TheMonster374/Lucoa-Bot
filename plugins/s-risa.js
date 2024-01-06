0ae59521c7416e86abbde5e86db7e45cfc34cdf3
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-06 11:34:11 -0300

    Update s-risa.js

--- plugins/s-risa.js
+++ plugins/s-risa.js
@@ -1,7 +1,4 @@
-/* import { sticker } from '../lib/sticker.js'
-let handler = async(m, { conn }) => {
-//if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on stickers* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
-if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
+import { sticker } from '../lib/sticker.js'
  
 let nombre = '𝑭𝒂𝒏𝒕𝒂𝒔𝒚𝑩𝒐𝒕-𝑴𝑫'
 let nombre2 = '𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄'
@@ -36,4 +33,4 @@ handler.customPrefix = /risa|xd|😂|🤣|🤪/i
 handler.command = new RegExp
 handler.exp = 50
 export default handler
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))*/
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))

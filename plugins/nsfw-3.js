ba18f8daa7f950f86fa0277dde152c40da7ae138
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-06 11:28:33 -0300

    Delete plugins/nsfw-3.js

--- plugins/nsfw-3.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/${command}?apikey=${lolkey}`
-}
-handler.help = ['hollolewd', 'sideoppai', 'animefeets', 'animebooty', 'animethighss', 'animearmpits', 'lewdanimegirls', 'biganimetiddies']
-handler.tags = ['nsfw']
-handler.command = /^(hollolewd|sideoppai|animefeets|animebooty|animethighss|animearmpits|lewdanimegirls|biganimetiddies)$/i
-
-module.exports = handler

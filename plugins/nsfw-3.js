afb7fdd2d57846e2345d8d815897ec4972ca861e
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-06 11:14:27 -0300

    Update nsfw-3.js

--- plugins/nsfw-3.js
+++ plugins/nsfw-3.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
+let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=BagasPrdn`
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
 handler.tags = ['nsfw']

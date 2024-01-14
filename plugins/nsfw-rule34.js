36bd3213e4d0e9b95ea29c5b9fbc64746280b2d2
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-14 07:03:45 -0300

    Create nsfw-rule34.js

---
+++ plugins/nsfw-rule34.js
@@ -0,0 +1,13 @@
+import { googleImage, pinterest } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!global.db.data.chats[m.chat].modohorny) return conn.reply(m.chat, `❎ En este grupo no esta permitido el contenido *+18*`, m, )
+
+if (!text) return conn.reply(m.chat, `*🚩 Ingresa un texto junto al comando.*`, m, )
+await conn.reply(m.chat, `*↻ Espera @${m.sender.split`@`[0]}, soy lento. . .*`,)
+const res = await (await googleImage('rule34 ' + text)).getRandom()
+await conn.sendFile(m.chat, res, 'error.jpg', `*––––––『 NSFW RULE34 』––––––*\n\n*Resultado de ∙* ${text ? text.capitalize() : false}\n\nJotchua-Bot`, )
+}
+handler.help = ['rule34 <texto>']
+handler.tags = ['nsfw', 'img']
+handler.command = ['rule34']
+export default handler

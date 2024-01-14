d3930b783712640495acbbb4ba66bebd8ba95b31
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-14 07:27:25 -0300

    Update Menu-Creador.js

--- plugins/Menu-Creador.js
+++ plugins/Menu-Creador.js
@@ -18,76 +18,78 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `   「 𝐎𝐖𝐍𝐄𝐑 𝐘 𝐌𝐎𝐃𝐄𝐑𝐀𝐃𝐎𝐑𝐄𝐒 」
-   
-> *<funcion>*
-=> *<funcion>*
-$ *<funcion>*
-_${usedPrefix}setprefix *<prefijo>*_
-_${usedPrefix}resetprefix_
-_${usedPrefix}autoadmin_
-_${usedPrefix}grouplist_
-_${usedPrefix}chetar_
-_${usedPrefix}leavegc_
-_${usedPrefix}cajafuerte_
-_${usedPrefix}blocklist_
-_${usedPrefix}addowner *<@tag / numero>*_
-_${usedPrefix}delowner *<@tag / numero>*_
-_${usedPrefix}block *<@tag / numero>*_
-_${usedPrefix}unblock *<@tag / numero>*_
-_${usedPrefix}enable *restrict*_
-_${usedPrefix}disable *restrict*_
-_${usedPrefix}enable *autoread*_
-_${usedPrefix}disable *autoread*_
-_${usedPrefix}enable *public*_
-_${usedPrefix}disable *public*_
-_${usedPrefix}enable *pconly*_
-_${usedPrefix}disable *pconly*_
-_${usedPrefix}enable *gconly*_
-_${usedPrefix}disable *gconly*_
-_${usedPrefix}enable *anticall*_
-_${usedPrefix}disable *anticall*_
-_${usedPrefix}enable *antiprivado*_
-_${usedPrefix}disable *antiprivado*_
-_${usedPrefix}enable *modejadibot*_
-_${usedPrefix}disable *modejadibot*_
-_${usedPrefix}enable *audios_bot*_
-_${usedPrefix}disable *audios_bot*_
-_${usedPrefix}enable *antispam*_
-_${usedPrefix}disable *antispam*_
-_${usedPrefix}msg *<texto>*_
-_${usedPrefix}backup | copia | respaldo_
-_${usedPrefix}banchat_
-_${usedPrefix}unbanchat_
-_${usedPrefix}resetuser *<@tag>*_
-_${usedPrefix}banuser *<@tag>*_
-_${usedPrefix}unbanuser *<@tag>*_
-_${usedPrefix}dardiamantes *<@tag> <cantidad>*_
-_${usedPrefix}añadirxp *<@tag> <cantidad>*_
-_${usedPrefix}banuser *<@tag>*_
-_${usedPrefix}bc *<texto>*_
-_${usedPrefix}bcchats *<texto>*_
-_${usedPrefix}bcgc *<texto>*_
-_${usedPrefix}bcgc2 *<audio>*_
-_${usedPrefix}bcgc2 *<video>*_
-_${usedPrefix}bcgc2 *<imagen>*_
-_${usedPrefix}bcbot *<texto>*_
-_${usedPrefix}cleartpm_
-_${usedPrefix}restart_
-_${usedPrefix}update_
-_${usedPrefix}banlist_
-_${usedPrefix}addprem *<@tag> <tiempo>*_
-_${usedPrefix}addprem2 *<@tag> <tiempo>*_
-_${usedPrefix}addprem3 *<@tag> <tiempo>*_
-_${usedPrefix}addprem4 *<@tag> <tiempo>*_
-_${usedPrefix}delprem *<@tag>*_
-_${usedPrefix}listcmd_
-_${usedPrefix}setppbot *<responder a imagen>*_
-_${usedPrefix}addcmd *<texto> <responder a sticker/imagen>*_
-_${usedPrefix}delcmd *<responder a sticker/imagen con comando o texto asignado>*_
-_${usedPrefix}saveimage
-_${usedPrefix}viewimage
-_${usedPrefix}dsowner_`.trim();
+    const str = `   
+┌──𝐎𝐖𝐍𝐄𝐑 𝐘 𝐌𝐎𝐃𝐄𝐑𝐀𝐃𝐎𝐑𝐄𝐒──┐
+┊   
+┊─•✧> *<funcion>*
+┊─•✧=> *<funcion>*
+┊─•✧$ *<funcion>*
+┊─•✧ _${usedPrefix}setprefix *<prefijo>*_
+┊─•✧ _${usedPrefix}resetprefix_
+┊─•✧ _${usedPrefix}autoadmin_
+┊─•✧ _${usedPrefix}grouplist_
+┊─•✧ _${usedPrefix}chetar_
+┊─•✧ _${usedPrefix}leavegc_
+┊─•✧ _${usedPrefix}cajafuerte_
+┊─•✧ _${usedPrefix}blocklist_
+┊─•✧ _${usedPrefix}addowner *<@tag / numero>*_
+┊─•✧ _${usedPrefix}delowner *<@tag / numero>*_
+┊─•✧ _${usedPrefix}block *<@tag / numero>*_
+┊─•✧ _${usedPrefix}unblock *<@tag / numero>*_
+┊─•✧ _${usedPrefix}enable *restrict*_
+┊─•✧ _${usedPrefix}disable *restrict*_
+┊─•✧ _${usedPrefix}enable *autoread*_
+┊─•✧ _${usedPrefix}disable *autoread*_
+┊─•✧ _${usedPrefix}enable *public*_
+┊─•✧ _${usedPrefix}disable *public*_
+┊─•✧ _${usedPrefix}enable *pconly*_
+┊─•✧ _${usedPrefix}disable *pconly*_
+┊─•✧ _${usedPrefix}enable *gconly*_
+┊─•✧ _${usedPrefix}disable *gconly*_
+┊─•✧ _${usedPrefix}enable *anticall*_
+┊─•✧ _${usedPrefix}disable *anticall*_
+┊─•✧ _${usedPrefix}enable *antiprivado*_
+┊─•✧ _${usedPrefix}disable *antiprivado*_
+┊─•✧ _${usedPrefix}enable *modejadibot*_
+┊─•✧ _${usedPrefix}disable *modejadibot*_
+┊─•✧ _${usedPrefix}enable *audios_bot*_
+┊─•✧ _${usedPrefix}disable *audios_bot*_
+┊─•✧ _${usedPrefix}enable *antispam*_
+┊─•✧ _${usedPrefix}disable *antispam*_
+┊─•✧ _${usedPrefix}msg *<texto>*_
+┊─•✧ _${usedPrefix}backup | copia | respaldo_
+┊─•✧ _${usedPrefix}banchat_
+┊─•✧ _${usedPrefix}unbanchat_
+┊─•✧ _${usedPrefix}resetuser *<@tag>*_
+┊─•✧ _${usedPrefix}banuser *<@tag>*_
+┊─•✧ _${usedPrefix}unbanuser *<@tag>*_
+┊─•✧ _${usedPrefix}dardiamantes *<@tag> <cantidad>*_
+┊─•✧ _${usedPrefix}añadirxp *<@tag> <cantidad>*_
+┊─•✧ _${usedPrefix}banuser *<@tag>*_
+┊─•✧ _${usedPrefix}bc *<texto>*_
+┊─•✧ _${usedPrefix}bcchats *<texto>*_
+┊─•✧ _${usedPrefix}bcgc *<texto>*_
+┊─•✧ _${usedPrefix}bcgc2 *<audio>*_
+┊─•✧ _${usedPrefix}bcgc2 *<video>*_
+┊─•✧ _${usedPrefix}bcgc2 *<imagen>*_
+┊─•✧ _${usedPrefix}bcbot *<texto>*_
+┊─•✧ _${usedPrefix}cleartpm_
+┊─•✧ _${usedPrefix}restart_
+┊─•✧ _${usedPrefix}update_
+┊─•✧ _${usedPrefix}banlist_
+┊─•✧ _${usedPrefix}addprem *<@tag> <tiempo>*_
+┊─•✧ _${usedPrefix}addprem2 *<@tag> <tiempo>*_
+┊─•✧ _${usedPrefix}addprem3 *<@tag> <tiempo>*_
+┊─•✧ _${usedPrefix}addprem4 *<@tag> <tiempo>*_
+┊─•✧ _${usedPrefix}delprem *<@tag>*_
+┊─•✧ _${usedPrefix}listcmd_
+┊─•✧ _${usedPrefix}setppbot *<responder a imagen>*_
+┊─•✧ _${usedPrefix}addcmd *<texto> <responder a sticker/imagen>*_
+┊─•✧ _${usedPrefix}delcmd *<responder a sticker/imagen con comando o texto asignado>*_
+┊─•✧ _${usedPrefix}saveimage
+┊─•✧ _${usedPrefix}viewimage
+┊─•✧ _${usedPrefix}dsowner_
+└─── •✧ᴊᴏᴛᴄʜᴜᴀ-ʙᴏᴛ ʙʏ ᴀʟꜱ✧•──┘`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -101,7 +103,7 @@ _${usedPrefix}dsowner_`.trim();
     conn.reply(m.chat, '*el menu tiene un error y no fue posible enviarlo*', m);
   }
 };
-handler.command = /^(menucreador|MenuCreador||)$/i;
+handler.command = /^(menucreador|MenuCreador)$/i;
 handler.exp = 50;
 handler.fail = null;
 export default handler;

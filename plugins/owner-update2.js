893eff36304faf9664e3a1e683c37ec25dd77e88
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-17 15:43:05 -0300

    Create owner-update2.js

---
+++ plugins/owner-update2.js
@@ -0,0 +1,15 @@
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+try {  
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)}
+} catch {
+var update = execSync('git remote set-url origin https://github.com/AleXD0009/Jotchua-Bot.git && git pull')
+await m.reply(update.toString())
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update2|actualizar2$/i
+handler.rowner = true
+export default handler

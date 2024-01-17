67fab902998b0decbed53231dda6bbfe6ee468ce
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-17 15:32:05 -0300

    Update nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -2,10 +2,10 @@ import { googleImage, pinterest } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!global.db.data.chats[m.chat].modohorny) return conn.reply(m.chat, `❎ En este grupo no esta permitido el contenido *+18*`, m, )
 
-if (!text) return conn.reply(m.chat, `*🚩 Ingresa un texto junto al comando.*`, m, )
-await conn.reply(m.chat, `*↻ Espera @${m.sender.split`@`[0]}, soy lento. . .*`,)
+if (!text) return conn.reply(m.chat, `*Ingresa un texto junto al comando.*`, m, )
+await conn.reply(m.chat, `*Espera @${m.sender.split`@`[0]}, én lo que envio tu rule pajero 🐶*`,)
 const res = await (await googleImage('rule34 ' + text)).getRandom()
-await conn.sendFile(m.chat, res, 'error.jpg', `*––––––『 NSFW RULE34 』––––––*\n\n*Resultado de ∙* ${text ? text.capitalize() : false}\n\nJotchua-Bot`, )
+await conn.sendFile(m.chat, res, 'error.jpg', `*––––––『 NSFW RULE34 』––––––*\n\n*Resultado de ∙* ${text ? text.capitalize() : false}\n\n𝐉𝐨𝐭𝐜𝐡𝐮𝐚-𝐁𝐨𝐭🐶`, )
 }
 handler.help = ['rule34 <texto>']
 handler.tags = ['nsfw', 'img']

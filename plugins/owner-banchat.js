3a19f6e7b60f9beeeb315cb70a22b0544b5d5762
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-17 18:12:34 -0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('*el chat fue baneado con excito*\n\n*el bot no respondera a ningun comando hasta desbanear el chat*');
+  m.reply('*el chat fue baneado con exito*\n\n*el bot no respondera a ningun comando hasta desbanear el chat*');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];

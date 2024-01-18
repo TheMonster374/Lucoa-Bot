440473f40e44706e90af23e2401ea63aa44cccbf
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 15:05:01 -0300

    Update owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -8,9 +8,9 @@ existsSync,
 readFileSync,
 watch
 } from 'fs'
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-conn.reply(m.chat, '*[ ✔ ] ARCHIVOS DE LA CARPETA TMP ELIMINADOS CON EXITO!!*', m)
+conn.reply(m.chat, `*⚠️ ARCHIVOS DE LA CARPETA TMP FUERON ELIMINADOS*`, m)
 
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
 const filename = []
@@ -18,9 +18,10 @@ tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(d
 return filename.map(file => {
 const stats = statSync(file)
 unlinkSync(file)
-})}
+})} //NO USAR ESTE COMANDO EN HEROKU | DO NOT USE THIS COMMAND ON HEROKU
 handler.help = ['cleartmp']
 handler.tags = ['owner']
-handler.command = /^(cleartmp|cleartemp)$/i
+handler.command = /^(cleartmp|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
+handler.exp = 500
 handler.rowner = true
 export default handler

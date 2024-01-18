dc8dbb5191e61ba7d92f089cfa4cc18a99871214
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 10:07:51 -0300

    Update rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,7 +1,7 @@
 const xpperlimit = 5;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
   if (global.db.data.users[m.sender].money >= xpperlimit * count) {
     global.db.data.users[m.sender].money -= xpperlimit * count;

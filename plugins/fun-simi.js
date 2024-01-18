b2ee10614d2fc63cd2d6728dbe3ee39ce34d96d7
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 04:28:41 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,12 +1,11 @@
-import translate from '@vitalets/google-translate-api';
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const handler = async (m, {text, command, args, usedPrefix}) => {
 if (!text) throw `𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Bot*`
 try {
-await conn.sendPresenceUpdate('composing', m.chat)
-const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+await m.chat.sendPresenceUpdate('composing')
+const api = await fetch('https://api.simsimi.net/v2/?text=' + encodeURIComponent(text) + '&lc=es');
 const resSimi = await api.json();
-m.reply(resSimi.success);
+m.reply(resSimi.success.toString());
 } catch {
 try {
 if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
@@ -14,18 +13,16 @@ if (text.includes('hola')) text = text.replace('hola', 'Hello');
 if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
 const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
 const resu = await reis.json();
-const nama = m.pushName || '1';
-const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+const nama = m.sender.pushname || '1';
+const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + encodeURIComponent(nama) + '&msg=' + encodeURIComponent(resu[0][0][0]));
 const res = await api.json();
 const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
 const resu2 = await reis2.json();
-m.reply(resu2[0][0][0]);
+m.reply(resu2[0][0][0].toString());
 } catch (e) {
-await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
+ }} catch {
+    throw '_*Ha habido un error*';    
 }}}
 handler.help = ['simsimi']
 handler.tags = ['General']
-handler.command = ['', '', ''] 
-export default handler
+handler.command = ['Test', 'test', ''] 

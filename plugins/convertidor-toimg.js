154801f5934f80a87e7815656e56e680affd434d
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 15:32:58 -0300

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,6 +1,6 @@
 import {webp2png} from '../lib/webp2mp4.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+  const notStickerMessage = `*responde al sticker que deseas convertir en imagen con el comando ${usedPrefix + command}*`;
   if (!m.quoted) throw notStickerMessage;
   const q = m.quoted || m;
   const mime = q.mediaType || '';

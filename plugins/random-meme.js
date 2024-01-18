b38813bbba5028fa93f0871f44d1b6b3e009fab0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 09:30:16 -0300

    Update random-meme.js

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -1,10 +1,9 @@
-/*   Creditos - ig : https://www.instagram.com/fg98._/    */
+
 import hispamemes from 'hispamemes';
 const handler = async (m, {command, conn}) => {
   const url = await hispamemes.meme();
-  conn.sendFile(m.chat, url, 'error.jpg', `_🤣 ${command} 🤣_`, m);
+  conn.sendFile(m.chat, url, 'error.jpg', `_${command}_`, m);
 };
-// conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
 handler.help = ['meme'];
 handler.tags = ['random'];
 handler.command = /^(meme|memes)$/i;

b7b959f6e4a7e0e93e1aebb9e260fa9e367b40f7
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 10:07:41 -0300

    Update and rename rpg-shop-coins.js to rpg-shop2.js

--- plugins/rpg-shop2.js
+++ plugins/rpg-shop2.js
@@ -1,10 +1,10 @@
-const xpperlimit = 350;
+const xplimit = 350;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xplimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
-    global.db.data.users[m.sender].exp -= xpperlimit * count;
+  if (global.db.data.users[m.sender].exp >= xplimit * count) {
+    global.db.data.users[m.sender].exp -= xplimit * count;
     global.db.data.users[m.sender].money += count;
     conn.reply(m.chat, `
 ┌─「 *NOTA DE PAGO* 」

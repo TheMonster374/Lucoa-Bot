c802ddca3432a9453d39cd1b43cf98defbe02404
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 15:15:57 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,27 +1,44 @@
-const fetch = require('node-fetch');
-const handler = async (m, {text, command, args, usedPrefix}) => {
-if (!text) throw `𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Bot*`
+import translate from '@vitalets/google-translate-api'
+import fetch from 'node-fetch'
+
+var handler = async (m, { text, command, args, usedPrefix }) => {
+
+if (!text) throw `*⚠️ INGRESE UN TEXTO PARA HABLAR CONMIGO*\n\n❕ EJEMPLO:\n*${usedPrefix + command}* Hola curiosity`
+m.react('🗣️') 
 try {
-await m.chat.sendPresenceUpdate('composing')
-const api = await fetch('https://api.simsimi.net/v2/?text=' + encodeURIComponent(text) + '&lc=es');
-const resSimi = await api.json();
-m.reply(resSimi.success.toString());
+let api = await fetch("https://api.simsimi.net/v2/?text=" + text + "&lc=es")
+let resSimi = await api.json()
+//m.reply(resSimi.success)      
+conn.reply(m.chat, resSimi.success, m, fake, )
 } catch {
 try {
-if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-if (text.includes('hola')) text = text.replace('hola', 'Hello');
-if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
-const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
-const resu = await reis.json();
-const nama = m.sender.pushname || '1';
-const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + encodeURIComponent(nama) + '&msg=' + encodeURIComponent(resu[0][0][0]));
-const res = await api.json();
-const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
-const resu2 = await reis2.json();
-m.reply(resu2[0][0][0].toString());
- }} catch {
-    throw '_*Ha habido un error*';    
-}}}
-handler.help = ['simsimi']
-handler.tags = ['General']
-handler.command = ['Test', 'test', ''] 
+if (text.includes('Hola')) text = text.replace('Hola', 'Hello')
+if (text.includes('hola')) text = text.replace('hola', 'Hello')
+if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO')    
+let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
+let resu = await reis.json()  
+let nama = m.pushName || '1'
+let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
+let res = await api.json()
+let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
+let resu2 = await reis2.json()
+//m.reply(resu2[0][0][0])      
+  conn.reply(m.chat, resu2[0][0][0], m, fake, )
+} catch {  
+let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
+let resuuu = await reisss.json()      
+let res222 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)  
+let json222 = await res222.json()
+let resulttt = json222.result
+let lolll = await translate(`${resulttt}`, { to: 'es', autoCorrect: true })
+//m.reply(lolll.text)      
+conn.reply(m.chat, lolll.text, m, fake, )
+}}
+
+}
+handler.help = ['simi']
+handler.tags = ['juegos']
+handler.command = /^((sim)?simi|jotchua|alexa|cortana|curio(sity)?)$/i
+handler.register = true
+
+export default handler

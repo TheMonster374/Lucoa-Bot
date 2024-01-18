61540b3535b94cc66c03deda3edde24c45b7af5f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-18 09:59:34 -0300

    Create RPG-semanal.js

---
+++ plugins/RPG-semanal.js
@@ -0,0 +1,19 @@
+import db from '../lib/database.js'
+
+const cooldown = 604800000 // 7 días 
+
+let handler = async (m) => {
+   let user = global.db.data.users[m.sender]
+   if (new Date - user.lastsemanal < cooldown) return conn.reply(m.chat, `🧭 Espera *${((user.lastsemanal + cooldown) - new Date()).toTimeString()}* para volver a Reclamar.`, m, )
+   let txt = `Felicidades 🎉, reclamaste *140 🪙 coins*.`
+   user.money += 140
+   user.lastsemanal = new Date * 1
+   await conn.reply(m.chat, txt, m, )
+}
+
+handler.help = ['semanal']
+handler.tags = ['rpg']
+handler.command = ['semanal', '.weekly']
+handler.cooldown = cooldown
+
+export default handler

de1f4f492ba780db758df141e3008c7f3bf44ec7
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 07:13:15 -0300

    Update econ-retirar.js

--- plugins/econ-retirar.js
+++ plugins/econ-retirar.js
@@ -1,18 +1,18 @@
-const xppercredit = 1
-let handler = async (m, { conn, command, args }) => {
+//const xppercredit = 1
+/let handler = async (m, { conn, command, args }) => {
     let count = command.replace(/^(wt|withdraw)/i, '')
     count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].bank / xppercredit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
     count = Math.max(1, count)
-    if (global.db.data.users[m.sender].bank >= xppercredit * count) {
-      global.db.data.users[m.sender].bank -= xppercredit * count
-      global.db.data.users[m.sender].money += count
-      conn.reply(m.chat, `transferiste 🪙 ${count} coins a tu billetera`, m)
-    } else conn.reply(m.chat, `🟥 *no tienes suficientes coins en tu banco*`, m)
+  //  if (global.db.data.users[m.sender].bank >= xppercredit * count) {
+      //global.db.data.users[m.sender].bank -= xppercredit * count
+      //global.db.data.users[m.sender].money += count
+    //  conn.reply(m.chat, `transferiste 🪙 ${count} coins a tu billetera`, m)
+  //  } else conn.reply(m.chat, `🟥 *no tienes suficientes coins en tu banco*`, m)/
   }
-  handler.help = ['withdraw']
-  handler.tags = ['economy']
-  handler.command = ['withdraw', 'with', 'withdrawall', 'withall', 'wt' , 'wtall'] 
+//  handler.help = ['withdraw']
+ // handler.tags = ['economy']
+ // handler.command = ['withdraw', 'with', 'withdrawall', 'withall', 'wt' , 'wtall'] 
   
-  handler.disabled = false
+//  handler.disabled = false
   
-  export default handler
+//  export default handler

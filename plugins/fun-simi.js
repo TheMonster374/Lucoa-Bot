ccd8db41d5a6da774c682d8367d366df603cf393
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 10:48:20 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -2,11 +2,9 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
- let lang = global.db.data.users[m.sender].language
   if (!text) throw `✳️ ${mssg.notext}`
   try { 
-  //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`)
-  let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
+    let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`

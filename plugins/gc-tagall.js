03d8c719a633ab0bf89542a37cd01c90e1cd1d6e
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 02:51:10 -0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -10,7 +10,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝙹𝚘𝚝𝚌𝚑𝚞𝚊-𝙱𝚘𝚝\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐁𝐲 𝙹𝚘𝚝𝚌𝚑𝚞𝚊-𝙱𝚘𝚝`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];

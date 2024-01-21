dc54212e5c42e9ffe5c5b5716371ace6e8d9c1ab
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 07:16:52 -0300

    Update RPG-semanal.js

--- plugins/RPG-semanal.js
+++ plugins/RPG-semanal.js
@@ -5,8 +5,8 @@ const cooldown = 604800000 // 7 días
 let handler = async (m) => {
    let user = global.db.data.users[m.sender]
    if (new Date - user.lastsemanal < cooldown) return conn.reply(m.chat, `🧭 Espera *${((user.lastsemanal + cooldown) - new Date()).toTimeString()}* para volver a Reclamar.`, m, )
-   let txt = `Felicidades 🎉, reclamaste *140 🪙 coins*.`
-   user.money += 140
+   let txt = `Felicidades 🎉, reclamaste *3000 XP*.`
+   user.exp += 3000
    user.lastsemanal = new Date * 1
    await conn.reply(m.chat, txt, m, )
 }

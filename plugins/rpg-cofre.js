cadc7e0baaca719c78fb3038e20462a9e2015655
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 07:19:30 -0300

    Update rpg-cofre.js

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -5,11 +5,9 @@ const handler = async (m, {isPrems, conn}) => {
   const img = 'https://img.freepik.com/vector-gratis/cofre-monedas-oro-piedras-preciosas-cristales-trofeo_107791-7769.jpg?w=2000';
   const dia = Math.floor(Math.random() * 30);
   const tok = Math.floor(Math.random() * 10);
-  const mystic = Math.floor(Math.random() * 4000);
   const expp = Math.floor(Math.random() * 5000);
 
   global.db.data.users[m.sender].limit += dia;
-  global.db.data.users[m.sender].money += mystic;
   global.db.data.users[m.sender].joincount += tok;
   global.db.data.users[m.sender].exp += expp;
 
@@ -17,10 +15,9 @@ const handler = async (m, {isPrems, conn}) => {
 ╔══════════⬣
 ║OBTIENES UN COFRE🎉
 ║┈┈┈┈┈┈┈┈┈┈┈┈┈
-║➢ *${dia} Diamantes* 💎
-║➢ *${tok} Tokens* 
-║➢ *${mystic} Coins* 🪙
-║➢ *${expp} Exp* ⚡
+║ *${dia} Diamantes* 💎
+║ *${tok} Tokens* 
+║ *${expp} Exp* ⚡
 ╚═════════════════⬣`;
 
   const fkontak = {

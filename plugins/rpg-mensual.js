2a52e52039a44218b20953fd4e0bbb296df05dd6
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-21 07:36:22 -0300

    Update rpg-mensual.js

--- plugins/rpg-mensual.js
+++ plugins/rpg-mensual.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { isPrems, conn }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" 
 }
-let grupos = [nna, nn, nnn, nnnt]
+let grupos = [mysticbot, md, md, md]
 let gata = [img5, img4, img3, img1, img2]
 let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🐈', body: 'support group' , sourceUrl: https://www.youtube.com/watch?v=CcF3rfC43Fk,  thumbnail: https://www.youtube.com/watch?v=CcF3rfC43Fk
 let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: https://www.youtube.com/watch?v=CcF3rfC43Fk, sourceUrl: yt }}}

ad84fbdf6ef26ed9c706ef9f0f1171c2df99290f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-22 09:55:57 -0300

    Update tEST.js

--- plugins/tEST.js
+++ plugins/tEST.js
@@ -2,89 +2,46 @@ const handler = async (m, {conn, usedPrefix}) => {
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `  _*ɪɴꜰᴏ ᴜꜱᴜᴀʀɪᴏ*_
-_*ɴᴏᴍʙʀᴇ​*_ : ${taguser}
- 
-
-╭─────────────┈⊷
-│「 _*ɪɴꜰᴏ ʙᴏᴛ*_ 」  
-╰────────────┈⊷  
-       
-
-🐶͘͜  _repo | repositorio_
-🐶͘͜  _estado_
-🐶͘͜  _grupos | gruposjt_
-🐶͘͜  _dash | dashboard_
-🐶͘͜  _speedtest_
-🐶͘͜  _instalarbot | infoinstalar_
-🐶͘͜  bot/sub bot: ${(conn.user.jid == global.conn.user.jid ? '' : `Jadibot de: https://wa.me/${global.conn.user.jid.split`@`[0]}`) || 'Este es el Bot oficial'}
-
-
-╭─────────────┈⊷
-│ 「 _*ᴍᴇɴᴜꜱ*_ 」
-╰────────────┈⊷            
-🐶͘͜  _MenuPorn | porn_
-🐶͘͜  _menuanimes | animes_
-🐶͘͜  _menuaudios | audios_
-🐶͘͜  _MenuCreador_
-🐶͘͜  _MenuDescargas | Descargas_
-🐶͘͜  _MenuRPG | RPG_
-
-╭─────────────┈⊷
-│ 「 _*ᴀᴄᴄɪᴏɴᴇꜱ*_ 」
-╰────────────┈⊷
-       
-🐶͘͜  _kiss_
-🐶͘͜  _slap_
-🐶͘͜  _pat_
-🐶͘͜  _dado_
-
-╭─────────────┈⊷
-│「 _*ɪᴀ/ʙᴏᴛ*_ 」
-╰────────────┈⊷
-
-🐶͘͜  _bot_
-🐶͘͜  _iaimagen
-
-
-╭─────────────┈⊷
-│「 _*ᴍᴇɴꜱᴀᴊᴇꜱ ᴇɴ ᴇꜱᴘᴇʀᴀ*_ 」
-╰────────────┈⊷
-
-🐶͘͜  _reporte *<texto>*_
-🐶͘͜  _fixmsgespera_
-⟼ Mensajes en espera
-
-
-╭─────────────┈⊷
-│「 _*ᴜɴᴇ ᴇʟ ʙᴏᴛ ᴀ ᴛᴜ ɢʀᴜᴘᴏ*_ 」
-╰────────────┈⊷
-
-🐶͘͜  _join *<enlace / link / url>*_
-
-
-╭─────────────┈⊷
-│「 _*ꜱᴇʀʙᴏᴛ - ᴊᴀᴅɪʙᴏᴛ*_ 」
-╰────────────┈⊷    
-
-🐶͘͜  _serbot_
-🐶͘͜  _serbot --code_
-🐶͘͜  _token_
-🐶͘͜  _bots_
-(si quiere sacar el subbot vaya 
-a dispositivos vinculados y cierre la sesion)
-
-
-╭─────────────┈⊷
-│「 _*ᴇɴᴀʙʟᴇ / ᴅɪꜱᴀʙʟᴇ*_ 」
-╰────────────┈⊷     
-
-🐶͘͜  _enable_
-🐶͘͜  _disable_
-
+  const text = ` 
 ╭─────────────┈⊷
 │「 _*ᴊᴜᴇɢᴏꜱ*_ 」
 ╰────────────┈⊷  
+
+🐶͘͜  _mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+🐶͘͜  _fake *<texto1> <@tag> <texto2>*_
+🐶͘͜  _ppt *<papel / tijera /piedra>*_
+🐶͘͜  _prostituto *<nombre / @tag>*_
+🐶͘͜  _prostituta *<nombre / @tag>*_
+🐶͘͜  _gay2 *<nombre / @tag>*_
+🐶͘͜  _lesbiana *<nombre / @tag>*_
+🐶͘͜  _pajero *<nombre / @tag>*_
+🐶͘͜  _pajera *<nombre / @tag>*_
+🐶͘͜  _puto *<nombre / @tag>*_
+🐶͘͜  _puta *<nombre / @tag>*_
+🐶͘͜  _manco *<nombre / @tag>*_
+🐶͘͜  _manca *<nombre / @tag>*_
+🐶͘͜  _rata *<nombre / @tag>*_
+🐶͘͜  _love *<nombre / @tag>*_
+🐶͜  _doxear *<nombre / @tag>*_
+🐶͘͜  _pregunta *<texto>*_
+🐶͘͜  suitpvp *<@tag>*_
+🐶͘͜  _slot *<apuesta>*_
+🐶͘͜  _ttt *<nombre sala>*_
+🐶͘͜  _delttt_
+🐶͘͜  _acertijo_
+🐶͘͜  _sorteo (nombre de lo que va a sortear)_
+🐶͘͜  _top *<texto>*_
+🐶͘͜  _topparejas_
+🐶͘͜  _amistad_
+🐶͘͜  _trivia_
+🐶͘͜  _topotakus_
+🐶͘͜  _formarpareja_
+🐶͘͜  _verdad_
+🐶͘͜  _reto_
+🐶͘͜  _cancion_
+🐶͘͜  _pista_
+🐶͘͜  _advpe_
+🐶͘͜  _palabra_
 
 `.trim();
   const buttonMessage= {
@@ -100,7 +57,7 @@ a dispositivos vinculados y cierre la sesion)
         'mediaUrl': 'https://github.com/AleXD0009/Jotchua-Bot',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': '𝐉𝐨𝐭𝐜𝐡𝐮𝐚 - 𝐁𝐨𝐭',
+        'title': '𝐌𝐄𝐍𝐔 𝐉𝐔𝐄𝐆𝐎𝐒',
         'body': wm,
         'thumbnail': imagen1,
         'sourceUrl': 'https://github.com/AleXD0009/Jotchua-Bot.git'}},
@@ -111,5 +68,5 @@ a dispositivos vinculados y cierre la sesion)
 };
 handler.help = ['owner', 'creator'];
 handler.tags = ['info'];
-handler.command = /^(prueba|creator|creador|propietario)$/i;
+handler.command = /^(MenuJuegos|Juegos|menuJuegos|menujuegos|juegos)$/i;
 export default handler;

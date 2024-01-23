f0e9c0b660253ad47578708411c3f2ce21c792d9
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-23 11:16:12 -0300

    Update fun-crimen.js

--- plugins/fun-crimen.js
+++ plugins/fun-crimen.js
@@ -4,18 +4,18 @@ const date = global.db.data.users[m.sender].crime + 10800000; //3600000 = 3 hs
 if (new Date - global.db.data.users[m.sender].crime < 10800000) return m.reply(`『🚓︎』𝙇𝘼 𝙋𝙊𝙇𝙄𝘾𝙄𝘼 𝙀𝙎𝙏𝘼 𝙑𝙄𝙂𝙄𝙇𝘼𝙉𝘿𝙊 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙈𝙊𝙈𝙀𝙉𝙏𝙊, 𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 : ${msToTime(date - new Date())}`)
 const exp = Math.floor(Math.random() * 10000)
 const diamond = Math.floor(Math.random() * 300)
-const exp = Math.floor(Math.random() * 10000)
+const money = Math.floor(Math.random() * 10000)
 let or = ['text', 'text2', 'text3', 'text4']; 
 let media = or[Math.floor(Math.random() * 4)]
 global.db.data.users[m.sender].crime = new Date * 1;
 if (media === 'text') return m.reply(`《💰》${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
 if (media === 'text2') return m.reply(`《🚓》${pickRandom(global.robmal)} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
-if (media === 'text3') return m.reply(`《💰》*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${exp} XP`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].exp += money)
-if (media === 'text4') return m.reply(`《🚓》${pickRandom(global.robmal)}\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${exp} XP`).catch(global.db.data.users[m.sender].limit -= diamond).catch(global.db.data.users[m.sender].exp -= money) 
+if (media === 'text3') return m.reply(`《💰》*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 👾 𝐌𝐲𝐬𝐭𝐢𝐜𝐂𝐨𝐢𝐧𝐬`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
+if (media === 'text4') return m.reply(`《🚓》${pickRandom(global.robmal)}\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 👾 𝐌𝐲𝐬𝐭𝐢𝐜𝐂𝐨𝐢𝐧𝐬`).catch(global.db.data.users[m.sender].limit -= diamond).catch(global.db.data.users[m.sender].money -= money) 
 }
 handler.help = ['robar'];
 handler.tags = ['xp'];
-handler.command = /^(crime|crimen|Crimen|RobarBanco|Crime)$/i
+handler.command = /^(crime|Crime)$/i
 handler.register = true
 export default handler;
 

29209b571c87c5759e1404888bfc199c99af8f96
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-23 06:53:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,121 @@
+let opcion
+if (methodCodeQR) {
+opcion = '1'
+}
+if (!methodCodeQR && !methodCode && !fs.existsSync(`./sessions/creds.json`)) {
+do {        
+let lineM = '┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅'
+opcion = await question(`┏${lineM}  
+┋ ${chalk.blueBright('┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┋ ${chalk.blueBright('┋')} ${chalk.blue.bgBlue.bold.cyan('MÉTODO DE VINCULACIÓN')}
+┋ ${chalk.blueBright('┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}   
+┋ ${chalk.blueBright('┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}     
+┋ ${chalk.blueBright('┋')} ${chalk.green.bgMagenta.bold.yellow('¿CÓMO DESEA CONECTARSE?')}
+┋ ${chalk.blueBright('┋')} ${chalk.bold.redBright('⇢  Opción 1:')} ${chalk.greenBright('Código QR.')}
+┋ ${chalk.blueBright('┋')} ${chalk.bold.redBright('⇢  Opción 2:')} ${chalk.greenBright('Código de 8 digitos.')}
+┋ ${chalk.blueBright('┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┋ ${chalk.blueBright('┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}     
+┋ ${chalk.blueBright('┋')} ${chalk.italic.magenta('Escriba sólo el número de')}
+┋ ${chalk.blueBright('┋')} ${chalk.italic.magenta('la opción para conectarse.')}
+┋ ${chalk.blueBright('┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┗${lineM}\n${chalk.bold.magentaBright('---> ')}`)
+if (!/^[1-2]$/.test(opcion)) {
+console.log(chalk.bold.redBright(`NO SE PERMITE NÚMEROS QUE NO SEAN ${chalk.bold.greenBright("1")} O ${chalk.bold.greenBright("2")}, TAMPOCO LETRAS O SÍMBOLOS ESPECIALES.\n${chalk.bold.yellowBright("CONSEJO: COPIE EL NÚMERO DE LA OPCIÓN Y PÉGUELO EN LA CONSOLA.")}`))
+}} while (opcion !== '1' && opcion !== '2' || fs.existsSync(`./sessions/creds.json`))
+}
+
+const sock = makeWASocket({
+logger: pino({ level: 'silent' }),
+printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
+mobile: MethodMobile, 
+browser: opcion == '1' ? ['NovaBot-MD', 'Safari', '1.0.0'] : methodCodeQR ? ['NovaBot-MD', 'Safari', '1.0.0'] : ['Chrome (Linux)', '', ''],
+auth: {
+creds: state.creds,
+keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
+},
+browser: ['Chrome (Linux)', '', ''], //
+markOnlineOnConnect: true, //establecer falso para fuera de línea
+generateHighQualityLinkPreview: true, //hacer enlace de vista previa alta
+getMessage: async (key) => {
+let jid = jidNormalizedUser(key.remoteJid)
+let msg = await store.loadMessage(jid, key.id)
+return msg?.message || ""
+},
+msgRetryCounterCache, //Resolver mensajes en espera
+defaultQueryTimeoutMs: undefined, //
+})
+
+store.bind(sock.ev)
+
+if (!fs.existsSync(`./sessions/creds.json`)) {
+if (opcion === '2' || methodCode) {
+opcion = '2'
+if (!sock.authState.creds.registered) {  
+let addNumber
+if (!!phoneNumber) {
+addNumber = phoneNumber.replace(/[^0-9]/g, '')
+if (!Object.keys(PHONENUMBER_MCC).some(v => addNumber.startsWith(v))) {
+console.log(chalk.bgBlack(chalk.bold.redBright("🟢 Comience con el código de país de su número de WhatsApp, ejemplo: +59178862672"))) 
+process.exit(0)
+}} else {
+while (true) {
+addNumber = await question(chalk.bgBlack(chalk.bold.greenBright(`🟢 Ingresa el número que sera bot\nPor ejemplo: +59178862672 `)))
+addNumber = addNumber.replace(/[^0-9]/g, '')
+  
+if (addNumber.match(/^\d+$/) && Object.keys(PHONENUMBER_MCC).some(v => addNumber.startsWith(v))) {
+break 
+} else {
+console.log(chalk.bold.redBright("❌ Asegúrese de agregar el código de país."))
+}}
+rl.close()  
+}
+
+setTimeout(async () => {
+let codeBot = await sock.requestPairingCode(addNumber)
+codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
+console.log(chalk.bold.white(chalk.bgMagenta(`👑 CÓDIGO DE VINCULACIÓN 👑: `)), chalk.bold.white(chalk.white(codeBot)))
+}, 2000)
+}}
+}
+
+sock.ev.on('messages.upsert', async chatUpdate => {
+//console.log(JSON.stringify(chatUpdate, undefined, 2))
+try {
+chatUpdate.messages.forEach(async (mek) => {
+try {
+mek = chatUpdate.messages[0]
+if (!mek.message) return
+mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+if (!sock.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
+if (mek.key.id.startsWith('FatihArridho_')) return
+global.numBot = sock.user.id.split(":")[0] + "@s.whatsapp.net"
+global.numBot2 = sock.user.id
+m = smsg(sock, mek)
+require("./main")(sock, m, chatUpdate, mek, store)
+} catch (e) {
+console.log(e)
+}})
+} catch (err) {
+console.log(err)
+}})
+
+sock.ev.on('messages.update', async chatUpdate => {
+for(const { key, update } of chatUpdate) {
+if(update.pollUpdates && key.fromMe) {
+const pollCreation = await getMessage(key)
+if(pollCreation) {
+const pollUpdate = await getAggregateVotesInPollMessage({
+message: pollCreation,
+pollUpdates: update.pollUpdates,
+})
+var toCmd = pollUpdate.filter(v => v.voters.length !== 0)[0]?.name
+if (toCmd == undefined) return
+var prefCmd = prefix+toCmd
+sock.appenTextMessage(prefCmd, chatUpdate)
+}}}})
+///TEST
 console.log('Iniciando...');
 import {join, dirname} from 'path';
 import {createRequire} from 'module';

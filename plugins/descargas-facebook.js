1dc1e66cc264699e0ff735f528cb197eeec698d1
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-23 10:24:50 -0300

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,9 +2,9 @@ async function descarga2(m, command, text, args, conn, lolkeysapi) {
 if (global.db.data.users[m.sender].registered < true) return m.reply(info.registra)
 if (global.db.data.users[m.sender].limit < 1) return m.reply(info.endLimit)
 if (command == 'facebook' || command == 'fb') { 
-if (!text) return m.reply(`${lenguaje.lengua.ejem}\n${prefix + command} https://fb.watch/ncowLHMp-x/?mibextid=rS40aB7S9Ucbxw6v`)
+if (!text) return m.reply(`usa el comando\n${prefix + command} https://fb.watch/ncowLHMp-x/?mibextid=rS40aB7S9Ucbxw6v`)
 if (!args[0].match(/www.facebook.com|fb.watch|web.facebook.com|business.facebook.com|video.fb.com/g)) return m.reply(`Error`) 
-conn.fakeReply(m.chat, `${lenguaje.lengua.espere}`, '0@s.whatsapp.net', 'No haga spam')
+conn.fakeReply(m.chat, `espera en lo que envio tu video`, '0@s.whatsapp.net', 'No haga spam')
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=${lolkeysapi}&url=${args[0]}`)
 let _json = await res.json()

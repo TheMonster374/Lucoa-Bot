710ff7c5ebe3b4e5f1b56dfeed7dce184cdcb104
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-23 04:04:09 -0300

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -13,13 +13,13 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
     const username = conn.getName(who);
     const prem = global.prems.includes(who.split `@` [0]);
     const sn = createHash('md5').update(who).digest('hex');
-    const str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
-*𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
-*𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
-*𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+    const str = `*Nombre:* ${username} ${registered ? '(' + name + ') ': ''}
+*Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*diamantes*:  ${user.limit}
+*​XP*: ${user.exp}
+*​Nivel​*: ${user.level}
+*Premium:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
+*Numero de serie:* 
 ${sn}`;
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
   }

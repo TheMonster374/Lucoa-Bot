f91dace725a870c7f45039639b2be2334ffb77b6
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-24 09:49:16 -0300

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -5,10 +5,20 @@ if (!text) {
  conn.reply(m.chat, `Hola *@${m.sender.split`@`[0]}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n*_📌 Ejemplo :_* *${usedPrefix + command}* Hola bot`, m, { mentions: [m.sender] })
  throw false
  }
-let res = await fetch(global.API('https://api.simsimi.net', '/v2/', ${encodeURIComponent(text)}`;
-let json = await res.json()
-if (json.success) m.reply(json.success)
-else throw json }
+    const API_URL = 'https://api.simsimi.net', '/v2/', ${encodeURIComponent(text)}`;
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    
+if (data.status && data.data) {
+      const respuestaAPI = data.data;
+      conn.reply(m.chat, respuestaAPI, m);
+    } else {
+      throw '_*𝐁𝐨𝐭 - 𝐈𝐀*_\n\n*No se pudo obtener una respuesta válida.*';
+    }
+  } catch (error) {
+    throw `_*𝐁𝐨𝐭 - 𝐈𝐀*_\n\n*Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
+  }
+};
 handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|alexa|cortana)$/i

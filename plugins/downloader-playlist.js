d83bc51ae6fcfa74a5b6553c9ee6e5c8adaec0fa
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-24 07:16:07 -0300

    Update downloader-playlist.js

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search';
 import fs from 'fs';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] Nombre de la canción faltante, por favor ingrese el comando más el nombre/titulo de una canción*\n\n*—◉ Ejemplo:*\n*${usedPrefix + command} Begin you*`;
+  if (!text) throw `*Nombre de la canción faltante, por favor ingrese el comando más el nombre/titulo de una canción*\n\n*Ejemplo:*\n*${usedPrefix + command} Nimstarr - Heart Racing*`;
   try {
     const vids_ = {
       from: m.sender,
@@ -14,26 +14,26 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
       global.videoList.splice(0, global.videoList.length);
     }
     const results = await yts(text);
-    const textoInfo = `*[❗] Puedes descargar el video que quieras de la siguiente forma:*
-◉ ${usedPrefix}audio <numero>
-◉ ${usedPrefix}video <numero> 
+    const textoInfo = `*Puedes descargar el video que quieras de la siguiente forma:*
+${usedPrefix}audio <numero>
+${usedPrefix}video <numero> 
 
-*—◉ Ejemplos:*
-*◉ ${usedPrefix}audio 5*
-*◉ ${usedPrefix}video 8*`.trim();
+*Ejemplos:*
+*${usedPrefix}audio 5*
+*${usedPrefix}video 8*`.trim();
     const teks = results.all.map((v, i) => {
       const link = v.url;
       vids_.urls.push(link);
       return `[${i + 1}] ${v.title}
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
-    }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+↳ *_Link :_* ${v.url}
+↳  *_Duración :_* ${v.timestamp}
+↳  *_Subido :_* ${v.ago}
+↳  *_Vistas :_* ${v.views}`;
+    }).join('\n\n────────────────────────────\n\n');
     conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
     global.videoList.push(vids_);
   } catch {
-    await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
+    await m.reply('*ERROR, intenta con el nombre de otra cancion*');
   }
 };
 handler.help = ['playlist *<texto>*'];

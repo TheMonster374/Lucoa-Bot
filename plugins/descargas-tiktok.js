55974a174f0552d5f1a75cf7442b12e4017c6152
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-24 05:50:04 -0300

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,46 +1,32 @@
-import fg from 'api-dylux'
-import { tiktokdl } from '@bochilteam/scraper'
+require('../main.js') 
+const fs = require("fs")
+const path = require("path")
+const chalk = require("chalk");
+const axios = require('axios')
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const yts = require('yt-search') 
+const ytdl = require('ytdl-core') 
+const {youtubedl, youtubedlv2} = require('@bochilteam/scraper') 
+const { smsg, fetchBuffer, getBuffer, buffergif, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getFile, getRandom, msToTime, downloadMediaMessage} = require('../libs/fuctions')
+const { ytmp4, ytmp3, ytplay, ytplayvid } = require('../libs/youtube') 
+const {sizeFormatter} = require('human-readable') 
+const formatSize = sizeFormatter({
+  std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
 
-var handler = async (m, { conn, text, args, usedPrefix, command}) => {
-
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok*\n\nEjemplo, !${command} https://vm.tiktok.com/ZMYG92bUh/`, m)
-if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🚩 *Verifica que el enlace sea correcto*`, m)
-
-const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waittt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitttt}`, edit: key})
-
-try {
-let p = await fg.tiktok(args[0])
-let te = `*Nombre:* ${p.nickname}
-*Usuario:* ${p.unique_id}
-*Duración:* ${p.duration}
-*Descripción:* ${p.description}`
-conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-} catch {
-
-try {
-
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-
-if (!url) return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, , )
-conn.sendFile(m.chat, url, 'fb.mp4', `*Nombre:* ${nickname}\n*Descripción:* ${description}`, m)
-m.react(done)
-} catch {
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, , )
-}}
+async function descarga(m, command, conn, text, command, args, fkontak, from, buffer, getFile, q, includes, lolkeysapi) {
+if (global.db.data.users[m.sender].registered < true) return m.reply(info.registra)
+if (global.db.data.users[m.sender].limit < 1) return m.reply(info.endLimit)
     
-}
-handler.help = ['tiktok']
-handler.tags = ['descargas']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
-
-
-export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+if (command == 'tiktok' || command == 'tt') {
+if (!text) return m.reply(`usa el comando\n${prefix + command} https://vm.tiktok.com/ZMjdrFCtg/`)
+if (!isUrl(args[0]) && !args[0].includes('tiktok')) return m.reply(`Link invalido!!`)
+conn.fakeReply(m.chat, `espera en lo que envio tu video`, '0@s.whatsapp.net', 'No haga spam')
+try {
+require('../libs/tiktok').Tiktok(args).then( data => {
+conn.sendMessage(m.chat, { video: { url: data.nowm }}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+conn.sendMessage(m.chat, { audio: { url: data.audio }, mimetype: 'audio/mp4' }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})})
+db.data.users[m.sender].limit -= 1
+m.reply('1 ' + info.limit)
+} catch {
+m.reply(info.error)}}

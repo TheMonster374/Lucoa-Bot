45d2c6d858da3fc0c2edb55fb151cbf0f36b85e9
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-24 07:11:56 -0300

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import axios from 'axios';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
- if (!text) throw `_*< DESCARGAS - SPOTIFY />*_\n\n*[ ℹ️ ] Hace falta el título de la canción de Spotify.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} Good Feeling - Flo Rida_`;
+ if (!text) throw `_*𝐒𝐏𝐎𝐓𝐈𝐅𝐘*_\n\n*Hace falta el título de la canción de Spotify.*\n\n*Ejemplo:* _${usedPrefix + command} Kanii, Riovaz, & Nimstarr - Heart Racing (Official Music Video)_`;
   try {
     const res = await fetch(global.API('CFROSAPI', '/api/spotifysearch?text=' + text))
     const data = await res.json()
@@ -16,17 +16,17 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     const info = await infos.json()
     const spty = info.spty.resultado
     const img = await (await fetch(`${spty.thumbnail}`)).buffer()  
-    let spotifyi = ` _*< DESCARGAS - SPOTIFY />*_\n\n`
-        spotifyi += ` ▢ *Título:* ${spty.title}\n\n`
-        spotifyi += ` ▢ *Artista:* ${spty.artist}\n\n`
-        spotifyi += ` ▢ *Álbum:* ${spty.album}\n\n`                 
-        spotifyi += ` ▢ *Publicado:* ${spty.year}\n\n`   
-        spotifyi += `*[ ℹ️ ] Se está enviando el audio. espere...*`
+    let spotifyi = ` _*𝐒𝐏𝐎𝐓𝐈𝐅𝐘*_\n\n`
+        spotifyi += ` 📚 *Título:* ${spty.title}\n\n`
+        spotifyi += ` 👻 *Artista:* ${spty.artist}\n\n`
+        spotifyi += ` 💽 *Álbum:* ${spty.album}\n\n`                 
+        spotifyi += ` 📅 *Publicado:* ${spty.year}\n\n`   
+        spotifyi += `*Se está enviando el audio. espere...*`
     await conn.sendMessage(m.chat, {text: spotifyi.trim(), contextInfo: {forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "thumbnailUrl": img, "mediaUrl": linkDL, "sourceUrl": linkDL}}}, {quoted: m});
     await conn.sendMessage(m.chat, {audio: music.data, fileName: `${spty.name}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
   } catch (error) {
     console.error(error);
-    throw '_*< DESCARGAS - SPOTIFY />*_\n\n[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*';
+    throw '_*𝐒𝐏𝐎𝐓𝐈𝐅𝐘*_\n\nOcurrió un error. Por favor, inténtalo de nuevo más tarde.*';
   }
 };
 handler.command = /^(spotify|music)$/i;

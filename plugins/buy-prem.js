a6b4f7a26bc2b772be2065f693fe413adb723793
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-24 13:31:33 -0300

    Create buy-prem.js

---
+++ plugins/buy-prem.js
@@ -0,0 +1,23 @@
+const xpperlimit = 100;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^buyprem/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  const prem = Math.floor(Math.random() * 3600000)
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].limit >= xpperlimit * count) {
+    global.db.data.users[m.sender].limit -= xpperlimit * count;
+    global.db.data.users[m.sender].premium += count;
+    conn.reply(m.chat, `
+┌─「 *NOTA DE PAGO* 」
+‣ *Compra nominal* : + ${count}💎 
+‣ *Gastado* : -${xpperlimit * count} XP
+└──────────────`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficiente *diantes* para comprar *${count}* premium`, m);
+};
+handler.help = ['Buy', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['buyprem', 'BuyPrem'];
+
+handler.disabled = false;
+
+export default handler;

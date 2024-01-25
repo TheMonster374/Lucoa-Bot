3c1f04ee6cb48d7acd4e98cf3d883799169c18b0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-25 09:23:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -55,6 +55,8 @@ global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Jotchua-Bot', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
+global.adsticker = `Jotchua-Bot` 
+
 global.mods = [];
 
 //* *******Tiempo***************

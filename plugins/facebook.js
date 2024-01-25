59ec91ef2572e35ac10fd4f3ce279c9bfc3b38c8
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-25 09:40:47 -0300

    Create facebook.js

---
+++ plugins/facebook.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+let handler = async (m, { conn, args, command }) => {
+	if (!args[0]) return conn.reply(m.chat, '*Escribe la URL de un video de Facebook que deseas descargar.*', m, adimagen)
+	try {
+	let config = {
+        'id': args[0],
+        'locale': 'id'
+      }
+    let { data, status } = await axios('https://getmyfb.com/process', {
+        method: 'POST',
+        data: new URLSearchParams(Object.entries(config)),
+        headers: {
+          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+          "cookie": "PHPSESSID=914a5et39uur28e84t9env0378; popCookie=1; prefetchAd_4301805=true"
+        }
+      })
+      let $ = cheerio.load(data)
+      let HD = $('div.container > div.results-download > ul > li:nth-child(1) > a').attr('href')
+      let SD = $('div.container > div.results-download > ul > li:nth-child(2) > a').attr('href')
+	await conn.sendMessage(m.chat, { video: { url: HD || SD }, caption: null }, { quoted: estilo})
+	} catch (e) {
+		conn.reply(m.chat, '*Ocurrió un error inesperado*', m, adimagen)
+	}
+}
+handler.help = ['facebook'].map(v => v + ' <url fb>')
+handler.tags = ['downloader']
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.limit = 2 
+export default handler

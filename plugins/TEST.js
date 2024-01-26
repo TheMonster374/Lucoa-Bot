3af6b2536a63635b3c6eec88439bdfcae723c9ea
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-26 15:53:47 -0300

    Update TEST.js

--- plugins/TEST.js
+++ plugins/TEST.js
@@ -7,172 +7,41 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   const {money, joincount} = global.db.data.users[m.sender];
   const {exp, limit, level, role} = global.db.data.users[m.sender];
   const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
-  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
-    await conn.reply(m.chat, '𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐌𝐞𝐧𝐮🐶.',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '𝐉𝐨𝐭𝐜𝐡𝐮𝐚 - 𝐁𝐨𝐭', body: 'BY als', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
+  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: catalogo ,sendEphemeral: true}}};
+    await conn.reply(m.chat, 'menu para la secu 😨.',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: 'ale te van a poner a chambear', body: 'menu para el ale', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
 //m.react('🐶');
-    await conn.sendMessage(m.chat, { react: { text: '🐶', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '📚', key: m.key } })
   let txt =`
 ╭─────────────┈
-│╭─────────────┈⊷
-││「 𝐈𝐍𝐅𝐎 𝐁𝐎𝐓 」  
-│╰────────────┈⊷  
-│ᴄᴏᴍᴀɴᴅᴏ ᴘᴀʀᴀ ꜱᴀʙᴇʀ ᴛᴏᴅᴏ ʟᴏ ɴᴜᴇᴠᴏ ᴅᴇʟ ʙᴏᴛ:
-│ _ComandosNuevos_
-│ _Nuevo_
-│
-│╭────────────────
-││𝙥𝙧𝙚𝙛𝙞𝙟𝙤:| . | / | # |
-│╰────────────────         
-│╭────────────────     
-││🐶͘͜  _owner | creador_
-││🐶͘͜  _repo | repositorio_
-││🐶͘͜  _estado_
-││🐶͘͜  _grupos | gruposjt_
-││🐶͘͜  _dash | dashboard_
-││🐶͘͜  _speedtest_
-││🐶͘͜  _instalarbot | infoinstalar_
-│╰────────────────  
-│ 
-│
-│╭─────────────┈⊷
-││ 「 𝐌𝐄𝐍𝐔𝐒 」
-│╰────────────┈⊷            
-│╭────────────────            
-││🐶͘͜  _MenuPorn | porn_
-││
-││🐶͘͜  _menuanimes | animes_
-││
-││🐶͘͜  _menuaudios | audios_
-││
-││🐶͘͜  _MenuCreador_
-││
-││🐶͘͜  _MenuDescargas | Descargas_
-││
-││🐶͘͜  _MenuRPG | RPG_
-│╰────────────────
-│ 
-│
-│╭─────────────┈⊷
-││ 「 𝐀𝐂𝐂𝐈𝐎𝐍𝐄𝐒」
-│╰────────────┈⊷
-│╭────────────────        
-││🐶͘͜  _kiss_
-││🐶͘͜  _slap_
-││🐶͘͜  _pat_
-││🐶͘͜  _dado_
-│╰────────────────
-│
-│╭─────────────┈⊷
-││「𝐈𝐀 / 𝐁𝐎𝐓/ 𝐒𝐈𝐌𝐒𝐈𝐌𝐈」
-│╰────────────┈⊷
-│╭──────────────── 
-││🐶͘͜  _bot_
-││🐶͘͜  _simi_
-││🐶͘͜  _iaimagen
-│╰────────────────
-│
-│╭─────────────┈⊷
-││「 𝐒𝐎𝐋𝐔𝐂𝐈𝐎𝐍 𝐀 𝐄𝐑𝐑𝐎𝐑𝐄𝐒 」
-│╰────────────┈⊷
-│╭──────────────── 
-││⟼ Mensajes en espera
-││🐶͘͜  _fixmsgespera_
-│╰────────────────
 │ 
 │╭─────────────┈⊷
-││「 𝐔𝐍𝐄 𝐄𝐋 𝐁𝐎𝐓 𝐀 𝐓𝐔 𝐆𝐑𝐔𝐏𝐎 」
-│╰────────────┈⊷
-│╭──────────────── 
-││🐶͘͜  _join *<enlace / link / url>*_
-│╰────────────────
-│ 
-│╭─────────────┈⊷
-││「 𝐒𝐄𝐑𝐁𝐎𝐓 - 𝐉𝐀𝐃𝐈𝐁𝐎𝐓 」
-│╰────────────┈⊷    
-│╭──────────────── 
-││🐶͘͜  _serbot_
-││🐶͘͜  _serbot --code_
-││🐶͘͜  _token_
-││🐶͘͜  _bots_
-││(si quiere sacar el subbot vaya 
-││a dispositivos vinculados y cierre la sesion)
-│╰────────────────
-│
-│╭─────────────┈⊷
-││「 𝐄𝐍𝐀𝐁𝐋𝐄 / 𝐃𝐈𝐒𝐀𝐁𝐋𝐄𝐒 𝐁𝐎𝐓 」
-│╰────────────┈⊷     
-│╭──────────────── 
-││🐶͘͜  _enable restrict_
-││🐶͘͜  _disable restrict_
-││🐶͘͜  _enable autoread_
-││🐶͘͜  _disable autoread_
-││🐶͘͜  _enable antispam_
-││🐶͘͜  _disable antispam_
-││🐶͘͜  _enable anticall_
-││🐶͘͜  _disable anticall_
-││🐶͘͜  _enable modoia_
-││🐶͘͜  _disable modoia_
-││🐶͘͜  _enable audios_bot_
-││🐶͘͜  _disable audios_bot_
-││🐶͘͜  _enable antiprivado_
-││🐶͘͜  _disable antiprivado_
-│╰────────────────
-│
-│╭─────────────┈⊷
-││「 𝐉𝐔𝐄𝐆𝐎𝐒 」
-│╰────────────┈⊷
-│╭──────────────── 
-││🐶͘͜  _mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-││🐶͘͜  _fake *<texto1> <@tag> <texto2>*_
-││🐶͘͜  _ppt *<papel / tijera /piedra>*_
-││🐶͘͜  _prostituto *<nombre / @tag>*_
-││🐶͘͜  _slot *<apuesta>*_
-││🐶͘͜  _ttt *<nombre sala>*_
-││🐶͘͜  _delttt_
-││🐶͘͜  _acertijo_
-││🐶͘͜  _sorteo (nombre de lo que va a sortear)_
-││🐶͘͜  _top *<texto>*_
-│╰────────────────
-│ 
-│╭─────────────┈⊷
-││「 𝐀𝐂𝐓𝐈𝐕𝐀𝐑 𝐎 𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐑 」
-│╰────────────┈⊷
-│╭────────────────     
-││🐶͘͜  _enable_
-││🐶͘͜  _disable_
-│╰────────────────
-│
-│╭─────────────┈⊷
-││「 𝐑𝐄𝐏𝐎𝐑𝐓𝐀𝐑 𝐄𝐑𝐑𝐎𝐑𝐄𝐒 」
-│╰────────────┈⊷
-│╭────────────────    
-││🐶͘͜  _reporte *<texto>*_
-│╰────────────────
-│ 
-│╭─────────────┈⊷
-││「 𝐁𝐔𝐒𝐂𝐀𝐃𝐎𝐑𝐄𝐒 」
+││𝐁𝐔𝐒𝐂𝐀𝐃𝐎𝐑𝐄𝐒 
 │╰────────────┈⊷     
 │╭────────────────      
-││🐶͘͜  _githubsearch *<texto>*_
-││🐶͘͜  _pelisplus *<texto>*_
-││🐶͘͜  _modapk *<texto>*_
-││🐶͘͜  _stickersearch *<texto>*_
-││🐶͘͜  _stickersearch2 *<texto>*_
-││🐶͘͜  _xnxxsearch *<texto>*_
-││🐶͘͜  _animeinfo *<texto>*_
-││🐶͘͜  _google *<texto>*_
-││🐶͘͜  _letra *<texto>*_
-││🐶͘͜  _wikipedia *<texto>*_
-││🐶͘͜  _ytsearch *<texto>*_
-││🐶͘͜  _playstore *<texto>*_
-│╰────────────────`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": '𝐉𝐨𝐭𝐜𝐡𝐮𝐚 - 𝐁𝐨𝐭🐶', "containsAutoReply": true, "mediaType": 1, "thumbnail": [imagen6,imagen1,imagen4].getRandom(), "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: fkon});
+││_githubsearch *<texto>*_
+││_pelisplus *<texto>*_
+││_modapk *<texto>*_
+││_stickersearch *<texto>*_
+││_stickersearch2 *<texto>*_
+││_animeinfo *<texto>*_
+││_google *<texto>*_
+││_letra *<texto>*_
+││_wikipedia *<texto>*_
+││_ytsearch *<texto>*_
+││_playstore *<texto>*_
+│╰────────────────
+│╭─────────────┈⊷
+││IMAGENES
+│╰────────────┈⊷     
+│
+╰────────────────`;
+    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": 'ale consigue chamba', "containsAutoReply": true, "mediaType": 1, "thumbnail": [catalogo,imagen1].getRandom(), "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: fkon});
  // m.react('🐶');
   } catch {
     conn.reply(m.chat, 'ha habido un error en enviar el menu.', m);
   }
 };
-handler.command = /^(test|Test)$/i;
+handler.command = /^(a|XD)$/i;
 handler.exp = 50;
 handler.fail = null;
 export default handler;

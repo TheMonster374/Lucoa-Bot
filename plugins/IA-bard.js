70e0c5795ef84596be2ccbfc9392c029f63f94d2
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-26 16:03:42 -0300

    Update and rename IA-Bot.js to IA-bard.js

---
+++ plugins/IA-bard.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `*Ingrese una petición*\n\nEjemplo, !bard Conoces Jotchua-Bot?`, m, estilo, )
+
+try {
+
+conn.sendPresenceUpdate('composing', m.chat)
+var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
+
+} catch (error) {
+console.error(error)
+return conn.reply(m.chat, `*Ocurrió un fallo*`, m, estilo, )
+}
+
+}
+handler.command = ['bard']
+handler.help = ['bard']
+handler.tags = ['ai']
+
+handler.premium = false
+
+export default handler

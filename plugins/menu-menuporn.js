8906315c78ef7160c997665a8a6916c5fa73d59e
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-27 14:49:38 -0300

    Update menu-menuporn.js

--- plugins/menu-menuporn.js
+++ plugins/menu-menuporn.js
@@ -49,7 +49,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┌┤_${usedPrefix}trapito_
 ┌┤_${usedPrefix}rule34_
 ┌┤_${usedPrefix}hentai_
-┌┤_${usedPrefix}xwaifu_
+┌┤_${usedPrefix}xwaifu2_
 ┌┤_${usedPrefix}hentai2_
 ┌┤_${usedPrefix}nsfwloli_
 ┌┤_${usedPrefix}nsfworgy_

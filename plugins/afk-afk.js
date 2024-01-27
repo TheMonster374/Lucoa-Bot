7462de9a96ffd13fd8abd627d6cec4c52a088ee4
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-27 03:56:48 -0300

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ const handler = async (m, {text}) => {
   const user = global.db.data.users[m.sender];
   user.afk = + new Date;
   user.afkReason = text;
-  m.reply(`*el usuario ${conn.getName(m.sender)} estara inactivo (afk), porfavor no lo etiqueten*\n\n*motivo de la inactividad (afk)${text ? ': ' + text : ''}*
+  m.reply(`*el usuario ${conn.getName(m.sender)} esta afk*\n\n*motivo:${text ? ': ' + text : ''}*
 `);
 };
 handler.help = ['afk [alasan]'];

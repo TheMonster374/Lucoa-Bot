288aeeaea6c1f795cda8f013f1efd061ba6d11db
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-27 04:03:50 -0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,7 +2,7 @@ export function before(m) {
   const user = global.db.data.users[m.sender];
   if (user.afk > -1) {
     m.reply(`
-  *DEJASTE DE ESTAR INACTIVO (AFK)${user.afkReason ? ' DESPUES DE ESTAR INACTIVO (AFK) POR EL MOTIVO: ' + user.afkReason : ''}*
+  *Dejaste de estar afk${user.afkReason ? 'afk motivo:' + user.afkReason : ''}*
   
   *TIEMPO DE INACTIVIDAD (AFK): ${(new Date - user.afk).toTimeString()}*
   `.trim());
@@ -22,9 +22,9 @@ export function before(m) {
     const reason = user.afkReason || '';
     m.reply(`*_NO LO ETIQUETES_*
 
-*_EL USUARIO QUE USTED ETIQUETO ESTA INACTIVO (AFK)_*      
-*${reason ? 'MOTIVO DE INACTIVIDAD (AFK): ' + reason : 'MOTIVO DE INACTIVIDAD (AFK): _𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙴𝚂𝙿𝙴𝙲𝙸𝙵𝙸𝙲𝙾 𝚄𝙽 𝙼𝙾𝚃𝙸𝚅𝙾_'}*
-*TIEMPO TRANSCURRIDO DE INACTIVIDAD (AFK): ${(new Date - afkTime).toTimeString()}*
+*_EL USUARIO QUE USTED ETIQUETO ESTA AFK_*      
+*${reason ? 'MOTIVO DE AFK: ' + reason : 'MOTIVO DE INACTIVIDAD (AFK): _𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙴𝚂𝙿𝙴𝙲𝙸𝙵𝙸𝙲𝙾 𝚄𝙽 𝙼𝙾𝚃𝙸𝚅𝙾_'}*
+*TIEMPO TRANSCURRIDO DE AFK: ${(new Date - afkTime).toTimeString()}*
   `.trim());
   }
   return true;

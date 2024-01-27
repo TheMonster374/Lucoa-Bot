1578056e96936cf19cb16b4ffbcb6f9e80bd5c31
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-27 14:59:38 -0300

    Update +18.js

--- plugins/+18.js
+++ plugins/+18.js
@@ -13,19 +13,3 @@ handler.tags = ['nsfw']
 handler.command = ['xwaifu']
 
 export default handler
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-   let res = await fetch('https://api.waifu.pics/nsfw/neko')
-   if (!res.ok) return
-   let json = await res.json()
-   if (!json.url) return
-   await conn.sendFile(m.chat, json.url, 'xneko.png', '*RANDOM NEKO*', m)
-}
-
-handler.help = ['xneko']
-handler.tags = ['nsfw']
-handler.command = ['xneko']
-
-export default handler

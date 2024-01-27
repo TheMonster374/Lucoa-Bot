f7cbfb9bc2be88e82516dfb71ed4e368987f3720
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-27 13:59:43 -0300

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, isPrems}) => {
   const hasil = Math.floor(Math.random() * 1000);
   const time = global.db.data.users[m.sender].lastmiming + 600000;
   if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*espera ${msToTime(time - new Date())} para volver a minar*`;
-  m.reply(`*minaste ${hasil} XP*`);
+  m.reply(`*minaste ${hasil} pesos 🪙*`);
   global.db.data.users[m.sender].lastmiming = new Date * 1;
 };
 handler.help = ['minar'];

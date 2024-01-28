ebd7cb45255183dce23b2646b34d4c26fb3a5984
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-28 17:00:39 -0300

    Update Menu-Descargas.js

--- plugins/Menu-Descargas.js
+++ plugins/Menu-Descargas.js
@@ -19,41 +19,41 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-           「 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 」
-
-🐶͘͜  _${usedPrefix}mediafire *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}gitclone *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}gdrive *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}tiktokimg *<enlace>*_
-🐶͘͜  _${usedPrefix}xnxxdl *<enlace>*_
-🐶͘͜  _${usedPrefix}xvideosdl *<enlace>*_
-🐶͘͜  _${usedPrefix}twitter *<enlace>*_ 
-🐶͘͜  _${usedPrefix}ytshort *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}ytmp3 *<enlace>*_
-🐶͘͜  _${usedPrefix}ytmp3doc *<enlace>*_
-🐶͘͜  _${usedPrefix}videodoc *<enlace>*_
-🐶͘͜  _${usedPrefix}dapk2 *<enlace>*_
-🐶͘͜  _${usedPrefix}stickerpack *<enlace>*_
-🐶͘͜  _${usedPrefix}facebook *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}tiktok *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}instagram *<enlace>*_ 『💎』
-🐶͘͜  _${usedPrefix}play *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}play2 *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}play.1 *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}play.2 *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}play5 *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}playlist *<texto>*_
-🐶͘͜  _${usedPrefix}spotify *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}ringtone *<texto>*_
-🐶͘͜  _${usedPrefix}soundcloud *<texto>*_
-🐶͘͜  _${usedPrefix}pinterest *<texto>*_ 『💎』
-🐶͘͜  _${usedPrefix}wallpaper *<texto>*_
-🐶͘͜  _${usedPrefix}pptiktok *<nombre de usuario>*_
-🐶͘͜  _${usedPrefix}igstalk *<nombre de usuario>*_
-🐶͘͜  _${usedPrefix}igstory *<nombre de usuario>*_
-🐶͘͜  _${usedPrefix}tiktokstalk *<username>*_
-    `.trim();
-    if (m.isGroup) {
+╭──────╯「 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 」╰────────╮
+┆
+┆🐶͘͜  _${usedPrefix}mediafire *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}gitclone *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}gdrive *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}tiktokimg *<enlace>*_
+┆🐶͘͜  _${usedPrefix}xnxxdl *<enlace>*_
+┆🐶͘͜  _${usedPrefix}xvideosdl *<enlace>*_
+┆🐶͘͜  _${usedPrefix}twitter *<enlace>*_ 
+┆🐶͘͜  _${usedPrefix}ytshort *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}ytmp3 *<enlace>*_
+┆🐶͘͜  _${usedPrefix}ytmp3doc *<enlace>*_
+┆🐶͘͜  _${usedPrefix}videodoc *<enlace>*_
+┆🐶͘͜  _${usedPrefix}dapk2 *<enlace>*_
+┆🐶͘͜  _${usedPrefix}stickerpack *<enlace>*_
+┆🐶͘͜  _${usedPrefix}facebook *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}tiktok *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}instagram *<enlace>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}play *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}play2 *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}play.1 *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}play.2 *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}play5 *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}playlist *<texto>*_
+┆🐶͘͜  _${usedPrefix}spotify *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}ringtone *<texto>*_
+┆🐶͘͜  _${usedPrefix}soundcloud *<texto>*_
+┆🐶͘͜  _${usedPrefix}pinterest *<texto>*_ 『💎』
+┆🐶͘͜  _${usedPrefix}wallpaper *<texto>*_
+┆🐶͘͜  _${usedPrefix}pptiktok *<nombre de usuario>*_
+┆🐶͘͜  _${usedPrefix}igstalk *<nombre de usuario>*_
+┆🐶͘͜  _${usedPrefix}igstory *<nombre de usuario>*_
+┆🐶͘͜  _${usedPrefix}tiktokstalk *<username>*_
+╰─────────────╮•╭────────────╯   `.trim();
+   if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});

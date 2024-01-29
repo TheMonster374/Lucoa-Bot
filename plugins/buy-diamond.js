959b806fbd65d6ff0610e59d7fbc5a02b8996b3f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-29 11:05:18 -0300

    Update buy-diamond.js

--- plugins/buy-diamond.js
+++ plugins/buy-diamond.js
@@ -1,21 +1,21 @@
-const xpperlimit = 200;
+const xpperlimit = 350;
 const handler = async (m, {conn, command, args}) => {
-  let count = command.replace(/^RentarBot/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].limit >= xpperlimit * count) {
-    global.db.data.users[m.sender].limit -= xpperlimit * count;
-    global.db.data.users[m.sender].exp += count;
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
     conn.reply(m.chat, `
 ┌─「 *NOTA DE PAGO* 」
-‣ *Compra nominal* : + ${count} hora el bot en tu grupo (mandar enlace al bot) 
-‣ *Gastado* : -${xpperlimit * count} Diamantes💎
+‣ *Compra nominal* : + ${count} Diamantes💎
+‣ *Gastado* : -${xpperlimit * count} pesos
 └──────────────`, m);
-  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficiente *Diamantes💎* para comprar *${count}* Hora el bot en tu grupo`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficiente *pesos* para comprar *${count}* Diamantes💎`, m);
 };
 handler.help = ['Buy', 'Buyall'];
 handler.tags = ['xp'];
-handler.command = ['rentbot', 'rentarbot'];
+handler.command = ['buy', 'buyall'];
 
 handler.disabled = false;
 

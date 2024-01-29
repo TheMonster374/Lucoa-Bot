fb3e04549d9dd6de6bb8f4c3d8c908705b0b7953
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-29 11:16:20 -0300

    Update rent-bot.js

--- plugins/rent-bot.js
+++ plugins/rent-bot.js
@@ -1,10 +1,10 @@
-const xpperlimit = 200;
+const limit = 200;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / limit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].limit >= xpperlimit * count) {
-    global.db.data.users[m.sender].limit -= xpperlimit * count;
+  if (global.db.data.users[m.sender].limit >= limit * count) {
+    global.db.data.users[m.sender].limit -= limit * count;
     global.db.data.users[m.sender].exp += count;
     conn.reply(m.chat, `
 ┌─「 *NOTA DE PAGO* 」

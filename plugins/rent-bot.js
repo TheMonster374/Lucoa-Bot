c6646459b2ad6e9035a00d0d285ec17ed82cbcf7
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-29 11:14:57 -0300

    Create rent-bot.js

---
+++ plugins/rent-bot.js
@@ -0,0 +1,22 @@
+const xpperlimit = 200;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].limit >= xpperlimit * count) {
+    global.db.data.users[m.sender].limit -= xpperlimit * count;
+    global.db.data.users[m.sender].exp += count;
+    conn.reply(m.chat, `
+┌─「 *NOTA DE PAGO* 」
+‣ *Compra nominal* : + ${count} hora de bot en tu grupo (pasar link)
+‣ *Gastado* : -${xpperlimit * count} 💎
+└──────────────`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficiente *Diamantes💎* para comprar *${count}* hora de bot en tu grupo`, m);
+};
+handler.help = ['rentar', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['rentar', 'buyall'];
+
+handler.disabled = false;
+
+export default handler;

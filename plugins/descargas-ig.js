5e903854fb4060c50a1c812aa30d3b961b425c66
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-29 15:15:57 -0300

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,61 +1,95 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import instagramGetUrl from 'instagram-url-direct'
-import { instagram } from '@xct007/frieren-scraper'
-import { instagramdl } from '@bochilteam/scraper'
- 
-var handler = async (m, {conn, args, command, usedPrefix}) => {
- 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de instagram*\n\nEjemplo ${usedPrefix + command} https://www.instagram.com/reel/CuqAzGRAbZa/?igshid=MzRlODBiNWFlZA==`, m, , )
-conn.reply(m.chat, `⏰ Espere un momento`, m, , )
-
-try {
-
-let apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`
-let responsel = await axios.get(apiUrll)
-let resultl = responsel.data
-for (const item of resultl.message) {
-let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text()
-let tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
-conn.sendFile(m.chat, item._url, null, tXXxt, fkontak, m)
-await new Promise((resolve) => setTimeout(resolve, 10000))
-} 
-} catch { 
-try { 
-let datTa = await instagram.v1(args[0])
-for (const urRRl of datTa) {
-let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
-conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, fkontak, m)
-await new Promise((resolve) => setTimeout(resolve, 10000))
-}
-} catch {
-try {
-let resultss = await instagramGetUrl(args[0]).url_list[0]
-let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt2 = `🍧 *Url:* ${shortUrl2}`.trim()
-await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
-} catch {
-try {
-let resultssss = await instagramdl(args[0])
-let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt4 = `🍧 *Url:* ${shortUrl3}`.trim()
-for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
-} catch {
-try {
-let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
-let json = await human.json()
-let videoig = json.result
-let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt1 = `🍧 *Url:* ${shortUrl1}`.trim()
-await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
-} catch {
-return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, , )
-}}}}} 
-
-}
-handler.help = ['ig']
-handler.tags = ['descargas']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
-
-export default handler
+import fetch from 'node-fetch';
+import axios from 'axios';
+import instagramGetUrl from 'instagram-url-direct';
+import {instagram} from '@xct007/frieren-scraper';
+import {instagramdl} from '@bochilteam/scraper';
+const handler = async (m, {conn, args, command, usedPrefix}) => {
+  if (!args[0]) {
+    let resp =  `*[❗𝐈𝐍𝐅𝐎❗] Ingrese un enlace de Instagram, ejemplo: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link`;
+    let txt = '';
+    let count = 0;
+    for (const c of resp) {
+        await new Promise(resolve => setTimeout(resolve, 15));
+        txt += c;
+        count++;
+    
+        if (count % 10 === 0) {
+            conn.sendPresenceUpdate('composing' , m.chat);
+        }
+    }
+        return conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
+    }
+  let resp = `*[❗] Descargando su video, aguarde un momento por favor, este proceso puede durar entre 2 y 10 minutos dependiendo de la duración del vídeo...*`
+  let txt = '';
+  let count = 0;
+  for (const c of resp) {
+      await new Promise(resolve => setTimeout(resolve, 15));
+      txt += c;
+      count++;
+  
+      if (count % 10 === 0) {
+          conn.sendPresenceUpdate('composing' , m.chat);
+      }
+  }
+      await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
+    try {
+    const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`;
+    const responsel = await axios.get(apiUrll);
+    const resultl = responsel.data;
+    for (const item of resultl.message) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text();
+      const tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim();
+      conn.sendFile(m.chat, item._url, null, tXXxt, m, true, {quoted: m, ephemeralExpiration: 2*60*1000});
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+  } catch {    
+  try {
+    const datTa = await instagram.v1(args[0]);
+    for (const urRRl of datTa) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+      const tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim();
+      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m, true, {quoted: m, ephemeralExpiration: 2*60*1000});
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+  } catch {
+      try {
+        const resultss = await instagramGetUrl(args[0]).url_list[0];
+        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+        const txt2 = `🔗 *Url:* ${shortUrl2}`.trim();
+        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m, true, {quoted: m, ephemeralExpiration: 2*60*1000});
+      } catch {
+        try {
+          const resultssss = await instagramdl(args[0]);
+          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+          const txt4 = `🔗 *Url:* ${shortUrl3}`.trim();
+          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m, true, {quoted: m, ephemeralExpiration: 2*60*1000});
+        } catch {
+          try {
+            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
+            const json = await human.json();
+            const videoig = json.result;
+            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+            const txt1 = `🔗 *Url:* ${shortUrl1}`.trim();
+            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m, true, {quoted: m, ephemeralExpiration: 2*60*1000});
+          } catch {
+            let resp = `*[❗𝐈𝐍𝐅𝐎❗] Error, por favor vuelva a intentarlo*`;
+            let txt = '';
+            let count = 0;
+            for (const c of resp) {
+                await new Promise(resolve => setTimeout(resolve, 5));
+                txt += c;
+                count++;
+            
+                if (count % 10 === 0) {
+                    conn.sendPresenceUpdate('composing' , m.chat);
+                }
+            }
+                return conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
+                      }
+        }
+      }
+    }
+  }
+};
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+export default handler;

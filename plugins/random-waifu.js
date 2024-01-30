f690e8e946fcd6c0154e8863fcb7126e8be5630b
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-29 22:57:18 -0300

    Update random-waifu.js

--- plugins/random-waifu.js
+++ plugins/random-waifu.js
@@ -4,8 +4,8 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   if (!res.ok) throw await res.text();
   const json = await res.json();
   if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, json.url, 'error.jpg', `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, m);
-// conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+  conn.sendFile(m.chat, json.url, 'error.jpg', `Jotchua - BOT`, m);
+// conn.sendButton(m.chat, `XD`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 };
 handler.help = ['waifu'];
 handler.tags = ['anime'];

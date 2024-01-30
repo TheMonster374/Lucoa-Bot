c7b8796cb685218fd03415db7f8cdd695742903d
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-30 19:44:48 -0300

    Create nsfw-2.js

---
+++ plugins/nsfw-2.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import fetch from 'node-fetch';
+const handler = async (m, {command, conn}) => {
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*𝐋𝐎𝐒 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 +𝟏𝟖 𝐄𝐒𝐓𝐀𝐍 𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐃𝐎𝐒 𝐄𝐍 𝐄𝐒𝐓𝐄 𝐆𝐑𝐔𝐏𝐎, 𝐒𝐈 𝐄𝐒 𝐀𝐃𝐌𝐈𝐍 𝐘 𝐃𝐄𝐒𝐄𝐀 𝐔𝐒𝐀𝐑𝐋𝐎𝐒 𝐔𝐒𝐄 𝐄𝐋 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 #enable modohorny*';
+
+  if (command == 'ahegao') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/kai0071/A17/master/HostMedia/nsfw/ahegao.json`)).data;
+    const haha = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
+};
+handler.help = ['ahegao'];
+handler.command = ['ahegao'];
+handler.tags = ['nsfw'];
+export default handler;

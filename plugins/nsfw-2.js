73758d8d910b043ad82dc13acde3de8129d6ab86
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-30 19:55:49 -0300

    Update nsfw-2.js

--- plugins/nsfw-2.js
+++ plugins/nsfw-2.js
@@ -1,16 +1,29 @@
-  case 'ahegao':
-        if (isBan) return reply(mess.banned);
-        if (isBanChat) return reply(mess.bangc);
-        if (!m.isGroup) return reply(mess.grouponly);
-        if (!AntiNsfw) return reply(mess.nonsfw);
-        A17.sendMessage(from, { react: { text: "🥵", key: m.key } })
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	
+	 let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+    
+    let user = global.db.data.users[who]
+    let name = conn.getName(who) 
+   let name2 = conn.getName(m.sender) 
 
-        var nsfwdata = JSON.parse(fs.readFileSync('./HostMedia/nsfw/agegao.json'))
-        var kairesult = pickRandom(nsfwdata)
-        A17.sendMessage(m.chat, { caption: mess.success, image: { url: kairesult.url } }, { quoted: m })
-        break;
+  let rki = await fetch(`/HostMedia/nsfw/pussy.json`)
+    if (!rki.ok) throw await rki.text()
+   let jkis = await rki.json()
+   let { url } = jkis
+   let stiker = await sticker(null, url, `(${name2}) mató a`, `${name}`)
+   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+   m.react('🗡️') 
+   
+}
 
-handler.help = ['ahegao'];
-handler.command = ['ahegao'];
-handler.tags = ['nsfw'];
-export default handler;
+handler.help = ['kill @tag']
+handler.tags = ['rnime']
+handler.command = /^(ahegao|matar)$/i
+
+export default handler

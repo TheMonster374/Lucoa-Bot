d0520b99dc7de033f2eff32df145f748b5b54471
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-30 19:58:43 -0300

    Update nsfw-2.js

--- plugins/nsfw-2.js
+++ plugins/nsfw-2.js
@@ -8,17 +8,14 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     else who = m.chat
     if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
     
-    let user = global.db.data.users[who]
-    let name = conn.getName(who) 
-   let name2 = conn.getName(m.sender) 
+
 
   let rki = await fetch(`/HostMedia/nsfw/pussy.json`)
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) mató a`, `${name}`)
-   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-   m.react('🗡️') 
+   let stiker = await imagen(null, url, `(${name2}) mató a`, `${name}`)
+   conn.sendFile(m.chat, imagen, null, { imagen: true }, m)
    
 }
 

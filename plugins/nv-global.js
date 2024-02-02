2f9a2cf809b2fc8d2864b33ae0a96c3aa4977669
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-02 07:35:43 -0300

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -4,8 +4,8 @@ handler.all = async function(m, {conn}) {
   const chat = global.db.data.chats[m.chat];
 
   if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Abre este enlace')) && !m.isBaileys && !m.isGroup && !chat.isBanned && !m.fromMe) {
-    const join = `*< UNE UN BOT A TU GRUPO />*\n\n*Hola @${m.sender.split('@')[0]}, si deseas solicitar un Bot para tu grupo usa el comando #join mas el enlace de tu grupo.*\n\n*Ejemplo:*\n*#join*https://chat.whatsapp.com/Cd90JzMCZ5o0ZX6gXBDwd4`.trim();
-    this.sendMessage(m.chat, {text: join.trim(), mentions: [...join.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...join.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": global.imagen6, "mediaUrl": `https://chat.whatsapp.com/Cd90JzMCZ5o0ZX6gXBDwd4`, "sourceUrl": `https://chat.whatsapp.com/Cd90JzMCZ5o0ZX6gXBDwd4`}}}, {quoted: m});
+    const join = `*< UNE UN BOT A TU GRUPO />*\n\n*Hola @${m.sender.split('@')[0]}, si deseas solicitar un Bot para tu grupo usa el comando #join mas el enlace de tu grupo.*\n\n*Ejemplo:*\n*#join*https://chat.whatsapp.com/LpZl6HC1iD3KfoYfDkyjbJ`.trim();
+    this.sendMessage(m.chat, {text: join.trim(), mentions: [...join.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...join.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": global.imagen6, "mediaUrl": `https://chat.whatsapp.com/LpZl6HC1iD3KfoYfDkyjbJ`, "sourceUrl": `https://chat.whatsapp.com/LpZl6HC1iD3KfoYfDkyjbJ`}}}, {quoted: m});
   }
 
   if (/^hola$/i.test(m.text) && !chat.isBanned) {

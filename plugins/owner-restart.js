be40f5f5bfb01bfb44811e6fb6f6ad6e77e1964a
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-03 09:29:05 -0300

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,11 +1,16 @@
-const handler = async (m, {conn, isROwner, text}) => {
-  if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
-    // conn.readMessages([m.key])
-    await m.reply('*Reiniciando el Bot...*\n\n*Espere un momento para volver a usar el Bot, puede tomar unos minutos.*');
-    process.send('reset');
-};
-handler.help = ['restart'];
-handler.tags = ['owner'];
-handler.command = ['restart', 'reiniciar'];
-handler.rowner = true;
-export default handler;
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply(`🔄 reiniciando jotchua, espera un momento`)
+    process.send('reset')
+  } else throw 'eh'
+}
+
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = ['restart','reiniciar'] 
+
+handler.rowner = true
+
+export default handler

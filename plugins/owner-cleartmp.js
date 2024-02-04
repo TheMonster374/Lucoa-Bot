067c914ed7a0d1fa13d0e32ddd699910e2214afe
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-04 03:58:31 -0300

    Update owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,27 +1,35 @@
-import { tmpdir } from 'os'
-import path, { join } from 'path'
-import {
-readdirSync,
-statSync,
-unlinkSync,
-existsSync,
-readFileSync,
-watch
-} from 'fs'
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-conn.reply(m.chat, `*⚠️ ARCHIVOS DE LA CARPETA TMP FUERON ELIMINADOS*`, m)
+import { tmpdir } from 'os';
+import path, { join } from 'path';
+import fs from 'fs';
+import { readdirSync, unlinkSync, rmSync } from 'fs';
 
-const tmp = [tmpdir(), join(__dirname, '../tmp')]
-const filename = []
-tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-return filename.map(file => {
-const stats = statSync(file)
-unlinkSync(file)
-})} //NO USAR ESTE COMANDO EN HEROKU | DO NOT USE THIS COMMAND ON HEROKU
-handler.help = ['cleartmp']
-handler.tags = ['owner']
-handler.command = /^(cleartmp|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
-handler.exp = 500
-handler.rowner = true
-export default handler
+let handler = async (m, { conn, __dirname, args }) => {
+
+  m.reply(`Se limpió la carpeta *tmp + sessions*`);
+  m.react(done);
+  // -- eliminar archivos temporales ---
+  const tmpDirs = [tmpdir(), join(__dirname, '../tmp')];
+  const tmpFiles = [];
+  tmpDirs.forEach((dir) => readdirSync(dir).forEach((file) => tmpFiles.push(join(dir, file))));
+  tmpFiles.forEach((file) => unlinkSync(file));
+
+  // -- eliminar sesiones del bot ---
+  const Sessions = "./sessions";
+  readdirSync(Sessions).forEach((file) => {
+    if (file !== 'creds.json') {
+      unlinkSync(`${Sessions}/${file}`, { recursive: true, force: true });
+    }
+  });
+
+
+
+//--
+};
+handler.help = ['cleartmp'];
+handler.tags = ['owner'];
+handler.command = /^(cleartmp)$/i;
+
+handler.rowner = true;
+
+export default handler;

27891d2edf8574a99a3a5c6566842869aea6647a
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-07 20:57:45 -0300

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;ALS⁩;;\nFN:ALS⁩\nORG:ALS\nTITLE:\nitem1.TEL;waid=5491144775561:5491144775561\nitem1.X-ABLabel:ALS⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:ALS\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩⁩;;\nFN:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩⁩\nORG:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩\nTITLE:\nitem1.TEL;waid=5491144775561:5491144775561\nitem1.X-ABLabel:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: '𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']

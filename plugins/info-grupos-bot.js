60d48e3ec006a096a4c1d2f61d6eb9245dac3c77
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-08 10:11:53 -0300

    Create info-grupos-bot.js

---
+++ plugins/info-grupos-bot.js
@@ -0,0 +1,39 @@
+const handler = async (m, {conn, usedPrefix}) => {
+  const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const text = `*Hola Usuario, te invito a unirte a los grupos de jotchua - bot - mini para convivir con la comunidad :D*
+
+                 ɢʀᴜᴘᴏꜱ ᴅᴇ ᴊᴏᴛᴄʜᴜᴀ-ʙᴏᴛ-ᴍɪɴɪ
+       
+⎯⎯  \\\ 𝕭𝖔𝖙𝖘 /// ⎯⎯ https://chat.whatsapp.com/GR9pMnqYI8DB9HoJnl2HkB ⎯⎯  \\\ 𝕭𝖔𝖙𝖘 /// ⎯⎯
+
+
+ɢʀᴜᴘᴏꜱ ᴄᴏʟᴀʙ ᴊᴏᴛᴄʜᴜᴀ-ʙᴏᴛ-ᴍɪɴɪ
+
+`.trim();
+  const buttonMessage= {
+    'document': {url: `https://github.com/AleXD0009`},
+    'mimetype': `application/${document}`,
+    'fileName': `ᴊᴏᴛᴄʜᴜᴀ - ʙᴏᴛ - ᴍɪɴɪ`,
+    'fileLength': 99999999999999,
+    'pageCount': 200,
+    'contextInfo': {
+      'forwardingScore': 200,
+      'isForwarded': true,
+      'externalAdReply': {
+        'mediaUrl': 'https://github.com/AleXD0009',
+        'mediaType': 2,
+        'previewType': 'pdf',
+        'title': 'ᴊᴏᴛᴄʜᴜᴀ - ʙᴏᴛ - ᴍɪɴɪ⁩',
+        'body': wm,
+        'thumbnail': imagen1,
+        'sourceUrl': 'https://chat.whatsapp.com/GR9pMnqYI8DB9HoJnl2HkB'}},
+    'caption': text,
+    'footer': wm,
+    'headerType': 6};
+  conn.sendMessage(m.chat, buttonMessage, {quoted: m});
+};
+handler.help = ['xwaifu']
+handler.tags = ['main'];
+handler.command = ['linkgc', 'grupos', 'gruposjt'];
+export default handler;

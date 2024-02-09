082580c6597da804e48c34e7016db473cb15b1dc
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-09 04:33:29 -0300

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,11 +3,11 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*responde a una imagen o video el cual sera convertido a enlace*';
+  if (!mime) throw '*ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ᴏ ᴠɪᴅᴇᴏ ᴇʟ ᴄᴜᴀʟ ꜱᴇʀᴀ ᴄᴏɴᴠᴇʀᴛɪᴅᴏ ᴀ ᴇɴʟᴀᴄᴇ*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*Enlace a su archivo:* ${link}`);
+  m.reply(`*ᴇɴʟᴀᴄᴇ ᴀ ꜱᴜ ᴀʀᴄʜɪᴠᴏ:* ${link}`);
 };
 handler.help = ['tourl <reply image>'];
 handler.tags = ['tools'];

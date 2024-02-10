45395d4b400b21f7639e0fa47347b907820bb3b4
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-10 05:39:29 -0300

    Update nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -2,7 +2,7 @@ import { googleImage, pinterest } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!global.db.data.chats[m.chat].modohorny) return conn.reply(m.chat, `❎ En este grupo no esta permitido el contenido *+18*`, m, )
 
-if (!text) return conn.reply(m.chat, `*Ingresa un texto junto al comando.*`, m, )
+if (!text) return conn.reply(m.chat, `*ɪɴɢʀᴇꜱᴀ ᴜɴ ᴛᴇxᴛᴏ ᴊᴜɴᴛᴏ ᴀʟ ᴄᴏᴍᴀɴᴅᴏ\n\n[🐶] ᴇᴊᴇᴍᴘʟᴏ: ${usedPrefix + command} gawr gura.*`, m, )
 const res = await (await googleImage('rule34 ' + text)).getRandom()
 await conn.sendFile(m.chat, res, 'error.jpg', `*ʀᴜʟᴇ34*\n\n*Resultado de ∙* ${text ? text.capitalize() : false}\n\nᴊᴏᴛᴄʜᴜᴀ - ʙᴏᴛ - ᴍɪɴɪ`, )
 }

3b3da2f711db9e9524be193d48c97228a4d34e43
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 18:09:02 -0300

    Create downloader-facebook.js

---
+++ plugins/downloader-facebook.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args }) => {
+    if (!args[0]) throw `ᴘᴏʀ ꜰᴀᴠᴏʀ, ɪɴɢʀᴇꜱᴇ ᴜɴ ᴇɴʟᴀᴄᴇ ᴅᴇ ꜰᴀᴄᴇʙᴏᴏᴋ.`;
+
+    try {
+        const apiUrl = `${apikasu}/api/dowloader/fbdown?url=${args[0]}&apikey=${apikeykasu}`;
+        const response = await fetch(apiUrl);
+
+        if (response.ok) {
+
+            const data = await response.json();
+            const videoUrl = data.result.Normal_video;
+
+            const fileName = "fb.mp4";
+
+            const videoResponse = await fetch(videoUrl);
+            const fileBuffer = await videoResponse.buffer();
+
+            conn.sendFile(m.chat, fileBuffer, fileName, "", m);
+
+        } else {
+            throw `
+> Sin respuesta
+
+No se pudo obtener el contenido de Facebook.`;
+        }
+    } catch (error) {
+        console.error(error);
+        throw `
+> Sin respuesta
+
+Ocurrió un error al descargar el video de Facebook: ${error.message}`;
+    }
+};
+
+handler.help = ['facebook'];
+handler.tags = ['downloader'];
+handler.command = ['facebook', 'fb'];
+
+export default handler;

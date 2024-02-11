108daca2a531af44f2395768f6c47c48c13367a3
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 01:35:30 -0300

    Update menuX.js

--- plugins/menuX.js
+++ plugins/menuX.js
@@ -6,8 +6,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
   const {money, joincount} = global.db.data.users[m.sender];
   const {exp, limit, level, role} = global.db.data.users[m.sender];
-  const m = { 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
-  let txt =`╭
+  let txt =`  
 ┋ 
 ┋ 
 ┋ 
@@ -15,7 +14,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┋ 
 ╰◆━━━━━━◆❃◆━━━━━━◆
 `;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": 'Jotchua-Bot', "containsAutoReply": true, "mediaType": 1, "thumbnail": [imagen3,imagen1,imagen4].getRandom(), "mediaUrl": md, "sourceUrl": md}}}, {quoted: m});
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}, mentionedJid: "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": 'Jotchua-Bot', "containsAutoReply": true, "mediaType": 1, "thumbnail": [imagen3,imagen1,imagen4].getRandom(), ;
  // m.react('🔥');
   } catch {
     conn.reply(m.chat, '⚠️ 𝑬el menu tiene un error.', m);

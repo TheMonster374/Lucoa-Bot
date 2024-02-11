e43e7b026cf3e7635cb4f3eee6745d00c2163649
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 18:49:36 -0300

    Create downloader-ig.js

---
+++ plugins/downloader-ig.js
@@ -0,0 +1,30 @@
+import { instagramdl } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+
+var handler = async (m, { args, conn, usedPrefix, command }) => {
+    if (!args[0]) throw `Ex:\n${usedPrefix}${command} https://www.instagram.com/reel/C0EEgMNSSHw/?igshid=MzY1NDJmNzMyNQ==`;
+
+    m.reply('enviando video')
+    
+    try {
+        let res = await bochil.snapsave(args[0]);
+        let media = await res[0].url;
+      
+        const sender = m.sender.split(`@`)[0];
+
+        if (!res) throw 'Can\'t download the post';
+      
+        await conn.sendMessage(m.chat, { video: { url: media }, caption: `aqui tienes tu video @${sender}`, mentions: [m.sender]}, m);
+
+    } catch (e) {
+      conn.reply(m.chat, 'Gagal mengunduh video', m);
+    }
+}
+
+handler.help = ['ig'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(ig(dl)?)$/i
+
+
+export default handler

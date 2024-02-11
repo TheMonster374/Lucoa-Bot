61323dfef803d4a544674495c3fe430242389321
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 17:55:20 -0300

    Update and rename downloader-ig. to downloader-ig.js

---
+++ plugins/downloader-ig.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args }) => {
+    if (!args[0]) {
+        throw `Por favor, ingresa un enlace de Instagram.`;
+    }
+
+    try {
+        const apiUrl = `${apikasu}/api/dowloader/instagram?url=${args[0]}&apikey=${apikeykasu}`
+        const response = await fetch(apiUrl);
+        const responseData = await response.json();
+
+        m.react(rwait);
+
+        if (responseData.status && responseData.result.length > 0) {
+            for (const media of responseData.result) {
+                m.react(done);
+                await conn.sendFile(m.chat, media.link, media.ext === 'mp4' ? 'video.mp4' : 'imagen.jpg', '', m);
+            }
+        } else {
+            throw `
+> Sin respuesta
+
+No se pudo obtener el contenido de Instagram.`;
+        }
+    } catch (error) {
+        console.error(error);
+        throw `
+> Sin respuesta
+
+Ocurrió un error al procesar la solicitud: ${error.message};`
+    }
+};
+
+handler.help = ['instagram'];
+handler.tags = ['dl'];
+handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
+
+export default handler;

50c39e2cc7e41001b19406731239006d76a70d5a
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 17:38:47 -0300

    Create applemusicsearch.js

---
+++ plugins/applemusicsearch.js
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+  if (!text) {
+    throw 'Ingrese el nombre de la canción';
+  }
+
+  try {
+    const apiUrl = `${apikasu}/api/search/applemusic?text=${encodeURIComponent(text)}&apikey=${apikeykasu}`;
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`Error al buscar la canción en Apple Music`);
+    }
+
+    const json = await response.json();
+
+    m.react(rwait);
+
+    const songInfo =`
+> Informacion
+*Nombre:* ${json.result.name}\n
+*Artista:* ${json.result.artist}\n
+*Álbum:* ${json.result.album}\n
+*Fecha de lanzamiento:* ${json.result.release_date}\n
+*Precio:* ${json.result.price}\n
+*Duración:* ${json.result.length}\n
+*Género:* ${json.result.genre}\n
+*Enlace:* ${json.result.url}`
+
+    if (json.result.thumbnail) {
+      m.react(done);
+      await conn.sendFile(m.chat, json.result.thumbnail, 'thumbnail.jpg', songInfo, m);
+    } else {
+      m.reply(songInfo);
+    }
+
+  } catch (error) {
+    console.error(error);
+    throw `
+> Sin respuesta
+Ocurrió un error al procesar la solicitud: ${error.message}`;
+  }
+};
+
+handler.help = ['applemusicsearch'];
+handler.tags = ['dl'];
+handler.command = /^(applemusicsearch)$/i;
+
+export default handler;

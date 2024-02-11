159c8342aa53d64818f5c8fe37a1dfc7ee8c4898
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-11 14:27:05 -0300

    Create IA-bingimg.js

---
+++ plugins/IA-bingimg.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw '*ɪɴɢʀᴇꜱᴀ ᴜɴ ᴛᴇxᴛᴏ ᴅᴇ ʟᴀ ɪᴍᴀɢᴇɴ Qᴜᴇ Qᴜɪᴇʀᴇꜱ* 🐶'
+    let msg = encodeURIComponent(text)
+    let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
+    let data = await res.json()
+    console.log(data)
+    let buffer = data.result
+    conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m)
+}
+
+handler.help = ['bingimg <query>']
+handler.tags = ['AI']
+handler.command = /^bingimg$/i
+
+export default handler

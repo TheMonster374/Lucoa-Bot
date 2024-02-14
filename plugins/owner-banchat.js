d5d7b02b9da621e4f313a8b826e0752ae100c774
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-14 04:57:26 -0300

    Create owner-banchat.js

---
+++ plugins/owner-banchat.js
@@ -0,0 +1,9 @@
+const handler = async (m) => {
+  global.db.data.chats[m.chat].isBanned = true;
+  m.reply('*[✅] 𝑪𝒉𝒂𝒕 𝑩𝒂𝒏𝒆𝒂𝒅𝒐*\n\n*𝑬𝒍  𝑩𝒐𝒕 𝑵𝒐 𝒓𝒆𝒔𝒑𝒐𝒏𝒅𝒆𝒓𝒂 𝒂 𝒏𝒊𝒏𝒈𝒖𝒏 𝒄𝒐𝒎𝒂𝒏𝒅𝒐*');
+};
+handler.help = ['banchat'];
+handler.tags = ['owner'];
+handler.command = /^(banchat|banearbot)$/i;
+handler.rowner = true;
+export default handler;

1bf85e2bc00a25b08522cd72530fa36f81ffa9fa
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-14 04:58:48 -0300

    Create owner-unbanchat.js

---
+++ plugins/owner-unbanchat.js
@@ -0,0 +1,9 @@
+const handler = async (m) => {
+  global.db.data.chats[m.chat].isBanned = false;
+  m.reply('*[✅] 𝑪𝒉𝒂𝒕 𝑫𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐*');
+};
+handler.help = ['unbanchat'];
+handler.tags = ['owner'];
+handler.command = /^(unbanchat|desbanearchat)$/i;
+handler.rowner = true;
+export default handler;

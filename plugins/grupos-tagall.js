16fa75ff418c431652eda3c94d0f59da46f71238
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-14 05:30:06 -0300

    Create grupos-tagall.js

---
+++ plugins/grupos-tagall.js
@@ -0,0 +1,21 @@
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `*𝑴𝒆𝒏𝒔𝒂𝒋𝒆:* ${pesan}`;
+  let teks = `*⺀𝑬𝑻𝑰𝑸𝑼𝑬𝑻𝑨𝑵𝑫𝑶 𝑮𝑹𝑼𝑷𝑶𝙾⺀*\n\n ${oi}\n\n *𝑷𝑬𝑹𝑺𝑶𝑵𝑨𝑺:*\n`;
+  for (const mem of participants) {
+    teks += `┣ @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `*└* 𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕-𝑴𝑰𝑵𝑰*`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;

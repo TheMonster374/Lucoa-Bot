bcc4c90c8d397dbfbd85cb3b9576e9e3750edbce
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-14 02:19:30 -0300

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,6 +1,6 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `⚠️️ *_Responde a un sticker con :_*\n\n *${usedPrefix + command}*`
+    const notStickerMessage = `⚠️️ *_𝑹𝒆𝒔𝒑𝒐𝒏𝒅𝒆 𝒂 𝒖𝒏 𝒔𝒕𝒊𝒄𝒌𝒆𝒓 𝒄𝒐𝒏 :_*\n\n *${usedPrefix + command}*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     await conn.sendNyanCat(m.chat, global.wait, m)
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ Aquí tienes⚘*', m)
+    await conn.sendFile(m.chat, out, 'out.png', '*✅ 𝒂𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔*', m)
 }
 handler.help = ['toimg *<sticker>*']
 handler.tags = ['sticker']

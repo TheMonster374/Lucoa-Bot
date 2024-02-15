fe284c0e05011786b201c84cb814b607c0812010
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-14 22:28:50 -0300

    Update test1.js

--- plugins/test1.js
+++ plugins/test1.js
@@ -9,14 +9,19 @@ const handler = async (m, {command, conn}) => {
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
-  
+  if (command == 'tentaculos') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/KazukoGans/database/main/nsfw/tentacles.json`)).data;
+    const haha = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
+
   if (command == 'manga') {
     const res = (await axios.get(`https://raw.githubusercontent.com/KazukoGans/database/main/nsfw/manga.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 };
-handler.help = ['ahegao', 'manga'];
-handler.command = ['ahegao', 'manga'];
+handler.help = ['ahegao', 'manga', 'tentaculos'];
+handler.command = ['ahegao', 'manga', 'tentaculos'];
 handler.tags = ['nsfw'];
 export default handler;

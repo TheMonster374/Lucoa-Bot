14b32d4eb418e7681fd04dc9be9c4f4b9af40d0e
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 00:40:07 -0300

    Create puta.js

---
+++ plugins/puta.js
@@ -0,0 +1,20 @@
+case 'creategc': case 'creategroup': {
+if (!args.join(" ")) return m.reply(`Uso: ${prefix+command} grupo nopor`)
+try {
+let cret = await sms.groupCreate(args.join(" "), [])
+let response = await sms.groupInviteCode(cret.id)
+const teksop = `     「 Grupo - Creado 」
+▸ Nombre : ${cret.subject}
+▸ Dueño : @${cret.owner.split("@")[0]}
+▸ Creacion : ${moment(cret.creation * 1000).tz("America/Lima'").format("DD/MM/YYYY HH:mm:ss")}
+https://chat.whatsapp.com/${response}`
+sms.sendMessage(m.chat, { text:teksop, mentions: await sms.parseMention(teksop)}, {quoted:m})
+} catch {
+m.reply(Error)
+}}
+
+handler.help = ['creargc'];
+handler.tags = ['owner'];
+handler.command = /^(creargcX)$/i;
+handler.rowner = true;
+export default handler;

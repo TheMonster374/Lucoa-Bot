16a68ca6ef9944ec9d97b3915393649d457be0d5
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 01:20:42 -0300

    Update grupo-crear.js

--- plugins/grupo-crear.js
+++ plugins/grupo-crear.js
@@ -1,23 +1,17 @@
-let handler = async (m, { text, args }) => {
-  if (!args.join(" ")) throw `[🐶] pon un nombre perra`
+case 'creategc': case 'creategroup': {
+if (!Owner) return ('> Este comando solo puede ser utilizado por mi creador >')
+if (!args.join(" ")) return m.reply(`Uso: ${prefix+command} grupo nopor`)
 try {
 let cret = await sms.groupCreate(args.join(" "), [])
 let response = await sms.groupInviteCode(cret.id)
 const teksop = `     「 Grupo - Creado 」
-
 ▸ Nombre : ${cret.subject}
 ▸ Dueño : @${cret.owner.split("@")[0]}
 ▸ Creacion : ${moment(cret.creation * 1000).tz("America/Lima'").format("DD/MM/YYYY HH:mm:ss")}
-
 https://chat.whatsapp.com/${response}`
 sms.sendMessage(m.chat, { text:teksop, mentions: await sms.parseMention(teksop)}, {quoted:m})
 } catch {
 m.reply(Error)
 }}
-
-handler.help = ['creargc']
-handler.tags = ['owner'];
-handler.command = /^(creargc|grupocrear)$/i;
-handler.rowner = true;
-
+break
 export default handler;

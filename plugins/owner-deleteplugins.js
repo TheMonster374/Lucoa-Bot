ad3c160287c5e970eaf294ce650923ce90e01d44
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 06:32:45 -0300

    Update owner-deleteplugins.js

--- plugins/owner-deleteplugins.js
+++ plugins/owner-deleteplugins.js
@@ -12,11 +12,11 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
 
 let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `📌 *_Ejemplo de uso:_*\n*#deleteplugin Menu▪oficial*`
-    if (!ar1.includes(args[0])) return m.reply(`*🗃️ NO ENCONTRADO!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!text) throw `📌 *_𝑬𝒋𝒆𝒎𝒑𝒍𝒐 𝒅𝒆 𝒖𝒔𝒐:_*\n*#deleteplugin Menu▪oficial*`
+    if (!ar1.includes(args[0])) return m.reply(`*[ ❌ ] 𝑵𝑶 𝑬𝑵𝑪𝑶𝑵𝑻𝑹𝑨𝑫𝑶!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
 const file = join(__dirname, '../plugins/' + args[0] + '.js')
 unlinkSync(file)
-conn.reply(m.chat, `⚠️ *_El plugin "plugins/${args[0]}.js" se ha eliminado con éxito._*`, m)
+conn.reply(m.chat, `*_El plugin "plugins/${args[0]}.js" se ha eliminado con éxito._* [ ✅ ]`, m)
     
 }
 handler.help = ['deleteplugin *<nombre>*']

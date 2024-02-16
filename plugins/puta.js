4c3429c85ae0da35f9ffd4a0e73825a4adf3f17b
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 01:33:14 -0300

    Update puta.js

--- plugins/puta.js
+++ plugins/puta.js
@@ -1,25 +1,19 @@
 let handler = async (m, { text, args }) => {
-if (!args.join(" ")) return m.reply(`Uso: ${prefix+command} grupo nopor`)
+  if (!args.join(" ")) throw `[🐶] pon un nombre perra`
 try {
 let cret = await sms.groupCreate(args.join(" "), [])
 let response = await sms.groupInviteCode(cret.id)
 const teksop = `     「 Grupo - Creado 」
-
 ▸ Nombre : ${cret.subject}
 ▸ Dueño : @${cret.owner.split("@")[0]}
 ▸ Creacion : ${moment(cret.creation * 1000).tz("America/Lima'").format("DD/MM/YYYY HH:mm:ss")}
-
 https://chat.whatsapp.com/${response}`
 sms.sendMessage(m.chat, { text:teksop, mentions: await sms.parseMention(teksop)}, {quoted:m})
 } catch {
 m.reply(Error)
 }}
-
-
 handler.help = ['creargc']
 handler.tags = ['owner'];
-handler.command = /^(creargcX|grupocrear)$/i;
-
+handler.command = /^(GC|grupocrear)$/i;
 handler.rowner = true;
-
 export default handler;

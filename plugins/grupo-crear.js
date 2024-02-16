935765e6112dd3fab2aaff234d74791d0273766a
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 06:35:34 -0300

    Update grupo-crear.js

--- plugins/grupo-crear.js
+++ plugins/grupo-crear.js
@@ -1,16 +1,16 @@
 //import { bold } from "chalk"
 let handler = async (m, { conn, text }) => {
    
-   if (!text) return m.reply('⚠️ _Escribe el nombre del grupo!_')
+   if (!text) return m.reply('[ ⚠ ] _𝑬𝒔𝒄𝒓𝒊𝒃𝒆 𝒆𝒍 𝒏𝒐𝒎𝒃𝒓𝒆 𝒅𝒆𝒍 𝒈𝒓𝒖𝒑𝒐!_')
    try{
     m.reply(wait)
     let group = await conn.groupCreate(text, [m.sender])
     let link = await conn.groupInviteCode(group.gid)
     let url = 'https://chat.whatsapp.com/' + link;
  /// console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nViolet'))
-    m.reply('_Grupo creado correctamente *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url)
+    m.reply('_𝑮𝒓𝒖𝒑𝒐 𝒄𝒓𝒆𝒂𝒅𝒐 𝒄𝒐𝒓𝒓𝒆𝒄𝒕𝒂𝒎𝒆𝒏𝒕𝒆 *' + text + '*_\n\n*𝑵𝒐𝒎𝒃𝒓𝒆:* ' + text + '\n*𝑰𝑫:* ' + group.gid + '\n*𝑳𝒊𝒏𝒌:* ' + url)
        } catch (e) {
-    m.reply(`❌ *Error.*`)
+    m.reply(`[❌] 𝑬𝑹𝑹𝑶𝑹 [❌]`)
   }
 }
 handler.help = ['creategroup']

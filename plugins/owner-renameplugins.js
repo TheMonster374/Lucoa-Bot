0ace95ccab6c21636c3901a373c53a10c6e7c510
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 06:31:19 -0300

    Update owner-renameplugins.js

--- plugins/owner-renameplugins.js
+++ plugins/owner-renameplugins.js
@@ -2,15 +2,15 @@ import { tmpdir } from 'os'
 import { join } from 'path'
 import fs from 'fs'
 let handler = async (m, { args, text, usedPrefix, command }) => {
-	let info = `${usedPrefix + command} <Antiguo nombre> | <Nuevo nombre>
+	let info = `${usedPrefix + command} 𝑨𝒏𝒕𝒊𝒈𝒖𝒐 𝒏𝒐𝒎𝒃𝒓𝒆 | 𝑵𝒖𝒆𝒗𝒐 𝒏𝒐𝒎𝒃𝒓𝒆
 
-*📌_ • Ejemplo:_*
+*📌_ • 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:_*
 ➞ ${usedPrefix + command} inv | rpg-inv
 
-*_🗒️ • Nota:_*
-no use la palabra .js al final de la oracion y trate que la palabra no lleve espacios "rpg- inv"`
+*_🗒️ • 𝑵𝒐𝒕𝒂:_*
+𝒏𝒐 𝒖𝒔𝒆 𝒍𝒂 𝒑𝒂𝒍𝒂𝒃𝒓𝒂 .𝒋𝒔 𝒂𝒍 𝒇𝒊𝒏𝒂𝒍 𝒅𝒆 𝒍𝒂 𝒐𝒓𝒂𝒄𝒊𝒐𝒏 𝒚 𝒕𝒓𝒂𝒕𝒆 𝒒𝒖𝒆 𝒍𝒂 𝒑𝒂𝒍𝒂𝒃𝒓𝒂 𝒏𝒐 𝒍𝒍𝒆𝒗𝒆 𝒆𝒔𝒑𝒂𝒄𝒊𝒐𝒔 "𝒓𝒑𝒈- 𝒊𝒏𝒗"`
 if (!args[0]) throw info
-if (!args[1] == "|") throw `• *📌_ • Ejemplo:_*:
+if (!args[1] == "|") throw `• *📌_ • 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:_*:
 ➞ ${usedPrefix + command} inv | rpg-inv`
 if (!args[2]) throw `• example:
 ➞ ${usedPrefix + command} inv | rpg-inv`
@@ -20,7 +20,7 @@ let to = args[2]
 
 let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!ar1.includes(args[0])) return m.reply(`*🗃️ NO ENCONTRADO!*\n==================================\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!ar1.includes(args[0])) return m.reply(`*[ ❌ ] 𝑵𝑶 𝑬𝑵𝑪𝑶𝑵𝑻𝑹𝑨𝑫𝑶!*\n==================================\n\n${ar1.map(v => ' ' + v).join`\n`}`)
 await fs.renameSync(`./plugins/${from}.js`, `./plugins/${to}.js`)
 conn.reply(m.chat, `Succes changes "plugins/${from}.js" to "plugins/${to}.js"`, m)
     

5d025ffc0bf040517ad76680eefdc9cd2525ccb0
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 05:44:35 -0300

    Update downloader-play2.js

--- plugins/downloader-play2.js
+++ plugins/downloader-play2.js
@@ -1,80 +1,36 @@
-import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import fetch from 'node-fetch';
+import youtube from "@yimura/scraper";
+const yt = new youtube.default();
 
-let handler = async (m.reply, { conn, text, args, usedPrefix, command }) => {
-  if (!text) throw `*Formato incorrecto*\ningresa el nombre de la canción\nEjemplo:\n${usedPrefix + command} Mi corazón encantado`;
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  if (!text) throw "Masukan Query !";
+  let res = await yt.search(text);
+  let vidio = res.videos[0];
+  let {
+    description,
+    duration,
+    duration_raw,
+    uploaded,
+    views,
+    channel,
+    id,
+    link,
+    thumbnail,
+    title,
+    shareLink,
+  } = vidio;
+  let imgg = await conn.resize(thumbnail, 300, 150);
+  let playy = `${title}
 
-  let ytse = await yts(args.join(" "));
-  let vid = ytse.all.find(v => v.type === "video");
-  let play = `
-• Título: ${vid.title}
-• Duración: ${convertDuration(vid.timestamp)}
-• Vistas: ${numberWithCommas(vid.views)}
-• Subido: ${vid.ago}
-• Link: ${vid.url}
-`.trim();
-  conn.sendMessage(m.chat, {
-    text: play,
-    contextInfo: {
-      externalAdReply: {
-        title: 'Descarga de Canciones',
-        thumbnailUrl: 'https://i.ibb.co/2jycYt8/images-1-6.jpg',
-        sourceUrl: 'http://youtube.com/@Kriz_Chaan',
-        mediaType: 1,
-      }
-    }
-  }, { quoted: m });
-
-  try {
-    let infoo = await ytdl.getInfo('https://youtu.be/' + vid.videoId);
-    let ress = ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' });
-    let thumbnail = vid.thumbnail;
-    conn.sendMessage(m.chat, { audio: { url: ress.url }, mimetype: "audio/mpeg", fileName: vid.title + '.mp3', quoted: m, contextInfo: {
-      forwardingScore: 200,
-      isForwarded: false,
-      externalAdReply: {
-        showAdAttribution: true,
-        title: vid.title,
-        body: null,
-        mediaType: 2,
-        sourceUrl: 'https://youtube.com/@KrizDavid_Fdz',
-        thumbnail: await (await fetch(thumbnail)).buffer(),
-      }
-    } });
-  } catch (error) {
-  
-  }
+📌 *ID :* ${id}
+⏲️ *Duration :* ${duration}
+⌚ *Duration Raw :* ${duration_raw}
+⏰ *Uploaded :* ${uploaded}
+👁️ *Views :* ${views}
+🔗 *URL:* ${link}`;
+  conn.sendFile(m.chat, thumbnail, "err.jpg", playy, m);
 };
-handler.help = ["play"].map(v => v + " <búsqueda>");
-handler.tags = ["descargador"];
+handler.help = ["play", "play2"].map((v) => v + " <pencarian>");
+handler.tags = ["downloader"];
 handler.command = /^play2?$/i;
+
 export default handler;
-
-function convertDuration(duration) {
-  if (!duration || duration.toLowerCase() === 'nan') return 'Desconocida';
-  let seconds = duration.split(':').reverse().map((value, index) => parseInt(value) * Math.pow(60, index)).reduce((a, b) => a + b, 0);
-  let hours = Math.floor(seconds / 3600);
-  let minutes = Math.floor((seconds % 3600) / 60);
-  let secs = seconds % 60;
-  return ${hours > 0 ? hours + ' horas ' : ''}${minutes > 0 ? minutes + ' minutos ' : ''}${secs > 0 ? secs + ' segundos' : ''};
-}
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function timeSince(date) {
-  let seconds = Math.floor((new Date() - new Date(date)) / 1000);
-  let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) return interval + " años";
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) return interval + " meses";
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) return interval + " días";
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) return interval + " horas";
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) return interval + " minutos";
-  return Math.floor(seconds) + " segundos";
-}*/

5d75365fc69257bb5c9b7f100135546d0530e2e2
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-16 00:18:59 -0300

    Create grupo-crear.js

---
+++ plugins/grupo-crear.js
@@ -0,0 +1,18 @@
+case 'creategc': case 'creategroup': {
+if (!Owner) return ('> Este comando solo puede ser utilizado por mi creador >')
+if (!args.join(" ")) return m.reply(`Uso: ${prefix+command} grupo nopor`)
+try {
+let cret = await sms.groupCreate(args.join(" "), [])
+let response = await sms.groupInviteCode(cret.id)
+const teksop = `     「 Grupo - Creado 」
+
+▸ Nombre : ${cret.subject}
+▸ Dueño : @${cret.owner.split("@")[0]}
+▸ Creacion : ${moment(cret.creation * 1000).tz("America/Lima'").format("DD/MM/YYYY HH:mm:ss")}
+
+https://chat.whatsapp.com/${response}`
+sms.sendMessage(m.chat, { text:teksop, mentions: await sms.parseMention(teksop)}, {quoted:m})
+} catch {
+m.reply(Error)
+}}
+break

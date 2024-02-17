7cf03b3cfd9c506977cb2f7e95fd98905a24c67b
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 01:51:09 -0300

    Update game-quiensoyi.js

--- plugins/game-quiensoyi.js
+++ plugins/game-quiensoyi.js
@@ -1,28 +1,51 @@
-let fetch = require('node-fetch')
-async function handler(m) {
-    this.game = this.game ? this.game : {}
-    let id = 'family100_' + m.chat
-    if (id in this.game) {
-        this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
-        throw false
-    }
-    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `
-*Soal:* ${json.soal}
+const { Brainly } = require('brainly-scraper-v2')
+const brainly = new Brainly('id')
 
-Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
-(beberapa jawaban terdapat spasi)
-`: ''}`.trim()
-    this.game[id] = {
-        id,
-        msg: await this.reply(m.chat, caption, m),
-        ...json,
-        terjawab: Array.from(json.jawaban, () => false)
-    }
+
+let handler = async function (m, { text }) {
+  if (!text) throw 'Soalnya?'
+  let res = await brainly.search('id', text)
+  let answer = res.map(({ question, answers }, i) => `
++ *Pertanyaan Ke ${i + 1}*
+${formatTags(question.content)}${answers.map((v, i) => `
+*Jawaban Ke ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Terpilih)' : ''}
+${formatTags(v.content)}`).join``}`).join(`
+
+•──┈┈──•
+
+`)
+  m.reply(answer)
 }
-handler.help = ['family100']
-handler.tags = ['game']
-handler.command = /^(family100)$/i
+handler.help = ['brainly'].map(j=>j+` <soal>`)
+handler.tags = ['search']
+
+handler.command = /^(brainly)$/i
 
 module.exports = handler
+
+function formatTags(str) {
+  let tagRegex = /<(.+?)>((?:.|\n)*?)<\/\1>/gi
+  let replacer = (_, tag, inner) => {
+    let a = inner.replace(tagRegex, replacer)
+    let b = ''
+    switch (tag) {
+      case 'p':
+        a += '\n'
+        break
+      case 'i':
+        b = '_'
+      case 'strikethrough':
+        b = '~'
+      case 'strong':
+        b = '*'
+        a = a.split('\n').map(a => a ? b + a + b : a).join('\n')
+        break
+    }
+    return a
+  }
+  
+  return str
+    .replace(/<br *?\/?>/gi, '\n')
+    .replace(tagRegex, replacer)
+    .trim()
+}

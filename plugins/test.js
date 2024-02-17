ff2773db0fbe753184e2d557495e42d4d93b1e0f
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 11:13:00 -0300

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,21 @@
+const maker = require('mumaker')
+
+module.exports = {
+    name: "pornhub",
+    alias: ["ph"],
+    desc: "Make text logo.",
+    react: "👹",
+    category: "Logo Maker",
+    handler: {
+        command: ['ph']
+    },
+    start: async(Yaka, m, { pushName, prefix, text }) => {
+        if (!text.includes("|")) return m.reply(`Example: *${prefix}pornhub Yaka MD | Yakashi*`);
+        const teks1 = text.split("|")[0];
+        const teks2 = text.split("|")[1];
+        
+        maker.textpro("https://textpro.me/pornhub-style-logo-online-generator-free-977.html", [`${teks1}`, `${teks2}`])
+            .then((data) => Yaka.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+            .catch((err) => m.reply('An Error occued!'));
+    }
+}

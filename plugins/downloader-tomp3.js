232b0cc995ece050b791ad7c0297341df661ea49
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 02:00:47 -0300

    Update downloader-tomp3.js

--- plugins/downloader-tomp3.js
+++ plugins/downloader-tomp3.js
@@ -1,17 +1,32 @@
-import { toAudio } from '../lib/converter.js'
+const { toAudio, toPTT } = require('../lib/converter')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
-   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
-    let media = await q.download?.()
-    if (!media) throw '❎ Failed to download media'
+  let q = m.quoted ? m.quoted : m
+  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+  if (/mp3|a(udio)?$/i.test(command)) {
+    if (!/video|audio/.test(mime))
+      throw `Reply audio/video dengan perintah *${usedPrefix + command}*`
+    let media = await q.download()
+    if (!media) throw 'Gagal mendownload media'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error converting'
-    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
+    if (!audio.data) throw 'Gagal menkonversi.'
+    await conn.sendMedia(m.chat, audio.data, m, {
+      fileName: Func.filename('mp3'),
+      mentions: [m.sender],
+    })
+  }
+  if (/vn|ptt$/i.test(command)) {
+    if (!/video|audio/.test(mime))
+      throw `Reply vn dengan perintah *${usedPrefix + command}*`
+    let media = await q.download()
+    if (!media) throw 'Gagal mendownload media.'
+    let audio = await toPTT(media, 'mp4')
+    if (!audio.data) throw 'Gagal menkonversi.'
+    await conn.sendFile(m.chat, audio.data, 'vn.opus', '', m)
+  }
 }
-handler.help = ['tomp3']
-handler.tags = ['fun']
-handler.command = /^to(mp3|a(udio)?)$/i
+handler.help = ['tomp3', 'tovn']
+handler.tags = ['tools']
+handler.command = ['tomp3', 'toaudio', 'tovn', 'toppt']
 
-export default handler
+module.exports = handler

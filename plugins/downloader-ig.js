328d2763e0bf3c5dc38bd6f1bfa15ffeaee9362d
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 08:35:42 -0300

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args }) => {
     if (!args[0]) {
-        throw `Por favor, ingresa un enlace de Instagram. ${JT.smsEjemplo} `;
+        throw `${JT.smsIG}. ${JT.smsEjemplo} `;
     }
 
     try {

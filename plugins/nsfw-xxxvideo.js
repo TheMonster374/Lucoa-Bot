a61c3e06b13f99cad15767409a957a8dce793fed
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 12:03:03 -0300

    Update nsfw-xxxvideo.js

--- plugins/nsfw-xxxvideo.js
+++ plugins/nsfw-xxxvideo.js
@@ -3,11 +3,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let name = await conn.getName(m.sender);
   if (!global.db.data.chats[m.chat].nsfw && m.isGroup)
     throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`;
-  var { age } = global.db.data.users[m.sender];
-  if (age < 17) {
-    m.reply(`❎ Eres menor de edad! vuelve cuando tengas más de 18 años`);
-    throw false;
-  }
   let res = await fetch(
     "https://api.zahwazein.xyz/randomasupan/discord18?apikey=zenzkey_048b3b850d51"
   );

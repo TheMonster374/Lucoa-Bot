6755ad1f4a25a17677470d17274911d65fd56e7a
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 08:18:00 -0300

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,12 +2,12 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m});
+    const { key } = await conn.sendMessage(m.chat, {text: `🕳🕳`}, {quoted: m});
 await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
+await conn.sendMessage(m.chat, {text: `🕳🕳🕳🕳`, edit: key});
 await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key});
-await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩`, edit: key});
+await conn.sendMessage(m.chat, {text: `🕳🕳🕳🕳🕳🕳🕳`, edit: key});
+await conn.sendMessage(m.chat, {text: `𝑹𝒆𝒊𝒏𝒊𝒄𝒊𝒂𝒏𝒅𝒐`, edit: key});
     process.send('reset')
   } else throw 'eh'
 }

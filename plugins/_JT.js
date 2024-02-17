c8da335e381b37d2fc551e3745f1ad5d57ac9239
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 07:54:39 -0300

    Update _JT.js

--- plugins/_JT.js
+++ plugins/_JT.js
@@ -1,7 +1,7 @@
 import { es } from '../lib/idiomas/_default.js' 
 
 export async function before(m,{ conn }) {
-let idioma  = global.db.data.users[m.sender].Language
+let idioma  = global.db.data.users[m.sender].JTLanguage
 let JT
   
 if (idioma == "es") {
@@ -9,6 +9,6 @@ JT = es
 }  {
 JT = mid || es
 }
-global.mid = JT
+global.JT = JT
 
 }

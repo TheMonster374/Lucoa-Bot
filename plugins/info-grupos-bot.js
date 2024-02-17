b8b1bdf89070513df9d639935cf55f17f34829b4
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-02-17 09:27:24 -0300

    Update info-grupos-bot.js

--- plugins/info-grupos-bot.js
+++ plugins/info-grupos-bot.js
@@ -1,27 +1,27 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*Hola Usuario, te invito a unirte a los grupos de jotchua - bot - mini para convivir con la comunidad :D*
+  const text = `𝑯𝒐𝒍𝒂 𝑼𝒔𝒖𝒂𝒓𝒊𝒐, 𝒕𝒆 𝒊𝒏𝒗𝒊𝒕𝒐 𝒂 𝒖𝒏𝒊𝒓𝒕𝒆 𝒂 𝒍𝒐𝒔 𝒈𝒓𝒖𝒑𝒐𝒔 𝒅𝒆 𝒋𝒐𝒕𝒄𝒉𝒖𝒂 - 𝒃𝒐𝒕 - 𝒎𝒊𝒏𝒊 𝒑𝒂𝒓𝒂 𝒄𝒐𝒏𝒗𝒊𝒗𝒊𝒓 𝒄𝒐𝒏 𝒍𝒂 𝒄𝒐𝒎𝒖𝒏𝒊𝒅𝒂𝒅
 ${JT.smsGruposBot}
 
 `.trim();
   const buttonMessage= {
     'document': {url: `https://github.com/AleXD0009`},
     'mimetype': `application/${document}`,
-    'fileName': `ᴊᴏᴛᴄʜᴜᴀ - ʙᴏᴛ - ᴍɪɴɪ`,
+    'fileName': `𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕-𝑴𝒊𝒏𝒊`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/AleXD0009',
+        'mediaUrl': '𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕-𝑴𝒊𝒏𝒊',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': 'ᴊᴏᴛᴄʜᴜᴀ - ʙᴏᴛ - ᴍɪɴɪ⁩',
+        'title': '𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕-𝑴𝒊𝒏𝒊⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://chat.whatsapp.com/GR9pMnqYI8DB9HoJnl2HkB'}},
+        'sourceUrl': 'nn'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};

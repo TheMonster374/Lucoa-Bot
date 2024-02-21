7cc89cba4d5cbe69fece2f5da61156f412010950
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-21 12:42:01 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,38 +1,137 @@
-//▪CÓDIGO BY ALBERTO9883 PRROS XD▪
-//▪NyanCatBot - MD▪
-
-import MessageType from '@whiskeysockets/baileys'
-import util from 'util'
-import fs from 'fs'
-import path from 'path'
-import fetch from 'node-fetch'
-
-let handler = m => m
-
-handler.before = async function (m, { conn, command, MessageType, text, usedPrefix, isAdmin }) {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let chat = global.db.data.chats[m.chat]
-let user = global.db.data.users[who]
-let username = conn.getName(who)
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-
-if ((m.isBaileys && m.fromMe) || m.fromMe ) return true
-
-if (chat.chatbot) {
-//▪AQUÍ AGREGA LOS COMANDOS▪
-let sticker1A = /Xd|xd|xD|XD/i
-let sticker1B = sticker1A.exec(m.text)
-
-
-//▪AQUI AGREGA LOS DIRECTORIOS DEL STICKER A MANDAR▪
-if (sticker1B) {
-	let vn = './src/welgc.webp'
-	conn.sendFile(m.chat, vn, 'welgc.webp', null, m, true, { type: 'webpMessage', ptt: true, contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${wm}`, body: `h`, mediaType: 2, sourceUrl: `null`, thumbnailUrl: md}}}, { quoted: m })
-	} else
+import puppeteer from "puppeteer"
+import fetch from "node-fetch"
+let handler = async (m, {
+    conn,
+    isOwner,
+    usedPrefix,
+    command,
+    args
+}) => {
+    let query = "Format: *.carbon Kode*"
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw query
+    await m.reply(wait)
+    try {
+        let result = await Carbonify(text)
+        await conn.sendFile(m.chat, result, "", "*From:* " + m.name, m)
+    } catch (e) {
+        try {
+            let result = await CarbonifyV2(text)
+            await conn.sendFile(m.chat, result, "", "*From:* " + m.name, m)
+        } catch (e) {
+            try {
+                let result = await CarbonifyV3(text)
+                await conn.sendFile(m.chat, result, "", "*From:* " + m.name, m)
+            } catch (e) {
+                throw eror
+            }
+        }
+    }
 }
-return !0
-}
-
+handler.help = ["carbon"]
+handler.tags = ["ai"]
+handler.command = /^carbon(ify)?$/i
+handler.limit = 1
 export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+
+const config = {
+    bg: "rgba(0, 0, 0, 1)",
+    t: "3024-night",
+    wt: "sharp",
+    l: "auto",
+    // width: 680,
+    ds: true,
+    dsyoff: "20px",
+    dsblur: "68px",
+    wc: true,
+    wa: true,
+    pv: "0px",
+    ph: "0px",
+    //ln: false,
+    // fl: 1,
+    fm: "Droid Sans Mono",
+    fs: "14px",
+    // lh: "133%",
+    si: false,
+    es: "2x",
+    wm: false
+};
+
+function convertToParams(myData) {
+    var out = [];
+    for (var key in myData) {
+        if (myData.hasOwnProperty(key)) {
+            out.push(key + "=" + encodeURIComponent(myData[key]));
+        }
+    }
+    return out.join("&");
+};
+
+async function Carbonify(teks) {
+    const snippets = [{
+        name: "Carbonify",
+        code: teks
+    }]
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 800,
+        height: 800,
+        deviceScaleFactor: 2
+    });
+    let index = 1;
+    for (const snippet of snippets) {
+        console.log(`Carbonifying snippet ${index} of ${snippets.length}`);
+        await page.goto(
+            `https://carbon.now.sh?${convertToParams(config)}&code=${encodeURI(
+        snippet.code
+      )}`
+        );
+
+        const codeContainer = await page.$("#export-container");
+        await page.addStyleTag({
+            content: ".CodeMirror-sizer{min-height: 0!important}"
+        });
+        return await codeContainer.screenshot({
+            path: `./sticker/${snippet.name.split(".")[0]}.png`,
+        });
+        index++;
+    }
+    await browser.close();
+}
+
+async function CarbonifyV3(input) {
+    let Blobs = await fetch("https://carbon-api.vercel.app/api", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "code": input
+            })
+        })
+        .then(response => response.blob())
+    let arrayBuffer = await Blobs.arrayBuffer();
+    let buffer = Buffer.from(arrayBuffer);
+    return buffer
+}
+
+async function CarbonifyV2(input) {
+    let Blobs = await fetch("https://carbonara.solopov.dev/api/cook", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "code": input
+            })
+        })
+        .then(response => response.blob())
+    let arrayBuffer = await Blobs.arrayBuffer();
+    let buffer = Buffer.from(arrayBuffer);
+    return buffer
+}

9cce15818b26b6c1935d307100d51aca1dac1e2f
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-21 12:25:00 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,160 +1,197 @@
+//▪CÓDIGO BY ALBERTO9883 PRROS XD▪
+//▪NyanCatBot - MD▪
 
-let { MessageType } = require("@adiwajshing/baileys");
-let util = require('util')
-let path = require('path')
-let fs = require("fs")
-let fetch = require('node-fetch')
+import MessageType from '@whiskeysockets/baileys'
+import util from 'util'
+import fs from 'fs'
+import path from 'path'
+import fetch from 'node-fetch'
 
 let handler = m => m
 
-handler.before = async function (m, { conn, command }) {
+handler.before = async function (m, { conn, command, MessageType, text, usedPrefix, isAdmin }) {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[who]
+let username = conn.getName(who)
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+
 if ((m.isBaileys && m.fromMe) || m.fromMe ) return true
 
-let sticker1A = /ola/i
-let sticker1B = audio1A.exec(m.text)
+if (chat.chatbot) {
+//▪AQUÍ AGREGA LOS COMANDOS▪
+let sticker1A = /Xd|xd|xD|XD/i
+let sticker1B = sticker1A.exec(m.text)
 
-let audio2A = /buenos dias|Buenos dias|buenos días|Buenos días/i
-let audio2B = audio2A.exec(m.text)
+let sticker2A = /nyan|cat|Nyan|Cat/i
+let sticker2B = sticker2A.exec(m.text)
 
-let audio3A = /Hentai|HENTAI|hentai|antojen|hentay|Hentay|Antojen/i 
-let audio3B = audio3A.exec(m.text)
+let sticker3A = /Gracias|gracias/i
+let sticker3B = sticker3A.exec(m.text)
 
-let audio4A = /fiesta del admin|Fiesta del admin|atención grupo|atencion grupo|aviso importante|fiestadeladmin|fiesta en casa del admin/i
-let audio4B = audio4A.exec(m.text)
+let sticker4A = /PEDO/i
+let sticker4B = sticker4A.exec(m.text)
 
-let audio5A = /fiesta del admin2|fiesta del admin 2|fiestadeladmin2|Fiesta del admin 2|Fiesta del admin2/i
-let audio5B = audio5A.exec(m.text)
+let sticker5A = /nay/i
+let sticker5B = sticker5A.exec(m.text)
 
-let audio6A = /fiesta viernes|viernes|Viernes|viernes fiesta/i
-let audio6B = audio6A.exec(m.text)
+let sticker6A = /nay/i
+let sticker6B = sticker6A.exec(m.text)
 
-let audio7A = /Me olvide|ME OLVIDE|me olvide|Me olvidé|me olvidé|ME OLVIDÉ/i
-let audio7B = audio7A.exec(m.text)
+let sticker7A = /nay/i
+let sticker7B = sticker7A.exec(m.text)
 
-let audio8A = /baneado|Baneado|baneada|Baneada/i
-let audio8B = audio8A.exec(m.text)
+let sticker8A = /nay/i
+let sticker8B = sticker8A.exec(m.text)
 
-let audio9A = /error/i
-let audio9B = audio9A.exec(m.text)
+let sticker9A = /nay/i
+let sticker9B = sticker9A.exec(m.text)
 
-let audio10A = /estoy aburrid|ando aburrid|que aburrid/i
-let audio10B = audio10A.exec(m.text)
+let sticker10A = /nay/i
+let sticker10B = sticker10A.exec(m.text)
 
-let audio11A = /leche/i
-let audio11B = audio11A.exec(m.text)
+let sticker11A = /nay/i
+let sticker11B = sticker11A.exec(m.text)
 
-let audio12A = /jajajaja/i
-let audio12B = audio12A.exec(m.text)
+let sticker12A = /nay/i
+let sticker12B = sticker12A.exec(m.text)
 
-let audio13A = /suicidar/i
-let audio13B = audio13A.exec(m.text)
+let sticker13A = /nay/i
+let sticker13B = sticker13A.exec(m.text)
 
-let audio14A = /puto bot|bot puto|pvto bot|bot pvto/i
-let audio14B = audio14A.exec(m.text)
+let sticker14A = /nay/i
+let sticker14B = sticker14A.exec(m.text)
 
-let audio15A = /^(A|ª|ᵃ|a)$/i
-let audio15B = audio15A.exec(m.text)
+let sticker15A = /nay/i
+let sticker15B = sticker15A.exec(m.text)
 
-let audio16A = /Bot|bot/i
-let audio16B = audio16A.exec(m.text)
+let sticker16A = /nay/i
+let sticker16B = sticker16A.exec(m.text)
 
+let sticker17A = /nay/i
+let sticker17B = sticker17A.exec(m.text)
 
-//Mensaje new
-//let texto1A = /No tengo hermana|No tengo ermana/i
-//let texto1B = texto1A.exec(m.text)
+let sticker18A = /nay/i
+let sticker18B = sticker18A.exec(m.text)
 
-if (sticler1B) {
-	let vn  = './media/Noche.mp3'
-	conn.sendFile(m.chat, vn, 'Noche.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+let sticker19A = /nay/i
+let sticker19B = sticker19A.exec(m.text)
+
+let sticker20A = /nay/i
+let sticker20B = sticker20A.exec(m.text)
+
+let sticker21A = /nay/i
+let sticker21B = sticker21A.exec(m.text)
+
+//▪AQUI AGREGA LOS DIRECTORIOS DEL STICKER A MANDAR▪
+if (sticker1B) {
+	let vn = './storage/stickers/xd.webp'
+	conn.sendFile(m.chat, vn, 'xd.webp', null, m, true, { type: 'webpMessage', ptt: true, contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${username}`, body: ``, mediaType: 2, sourceUrl: ``, thumbnailUrl: pp}}}, { quoted: m })
 	} else
 
-if (audio2B) {
-	let vn = './media/Buenos-dias-2.mp3'
-	conn.sendFile(m.chat, vn, 'Buenos-dias-2.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker2B) {
+	let emot = pickRandom(["🐢", "🌹", "🌺", "🍂"])
+conn.sendMessage(m.chat, { react: { text: emot, key: m.key }})
 	} else
 
-if (audio3B) {
-	let vn = './media/hentai.mp3'
-	conn.sendFile(m.chat, vn, 'hentai.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker3B) {
+	let vn = './storage/stickers/gracias.webp'
+	conn.sendFile(m.chat, vn, 'gracias.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio4B) {
-	let vn = './media/Fiesta1.mp3'
-	conn.sendFile(m.chat, vn, 'Fiesta1.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker4B) {
+	let vn = './src/webp/hola.webp'
+	conn.sendFile(m.chat, vn, 'hola.webp', null, fkowner, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio5B) {
-	let vn = './media/fiesta.mp3'
-	conn.sendFile(m.chat, vn, 'fiesta.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker5B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio6B) {
-	let vn = './media/viernes.mp3'
-	conn.sendFile(m.chat, vn, 'viernes.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker6B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio7B) {
-	let vn = './media/chica lgante.mp3'
-	conn.sendFile(m.chat, vn, 'chica lgante.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker7B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio8B) {
-	let vn = './media/baneado.mp3'
-	conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker8B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio9B) {
-	let vn = './storage/audio/Me_cago_en_la_concha.mp3'
-	conn.sendFile(m.chat, vn, 'Me_cago_en_la_concha.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker9B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio10B) {
-	let vn = './storage/audio/Juguemos_al_ascensor.mp3'
-	conn.sendFile(m.chat, vn, 'Juguemos_al_ascensor.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker10B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio11B) {
-	let vn = './storage/audio/Esto_no_es_leche.mp3'
-	conn.sendFile(m.chat, vn, 'Esto_no_es_leche.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker11B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio12B) {
-        let tt = ["./storage/audio/Eres_muy_chistoso.mp3", "./storage/audio/Gracioso_te_crees.mp3"]
-	let vn = tt[Math.floor(Math.random() * tt.length)]
-	conn.sendFile(m.chat, vn, 'Gracioso.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker12B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	} else
 
-if (audio13B) {
-	let vn = './storage/audio/El_suicidio_no_es_una_opcion.mp3'
-	conn.sendFile(m.chat, vn, 'El_suicidio_no_es_una_opcion.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+if (sticker13B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+
+if (sticker14B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+
+if (sticker15B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+
+if (sticker16B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+	
+if (sticker17B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+	
+if (sticker18B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+	
+if (sticker19B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+	
+if (sticker20B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
+	} else
+	
+if (sticker21B) {
+	let vn = './src/webp/bot.webp'
+	conn.sendFile(m.chat, vn, 'bot.webp', null, m, true, { type: 'webpMessage', ptt: true })
 	}
+}
+return !0
+}
 
-if (audio14B) {
-	let vn = './storage/audio/Deje_de_insultarme.mp3'
-	conn.sendFile(m.chat, vn, 'Deje_de_insultarme.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
-
-if (audio15B) {
-	let vn = './storage/audio/A.mp3'
-	conn.sendFile(m.chat, vn, 'A.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
-/*
-if (audio16B) {
-	let vn = './storage/sticker/Bot.webp'
-        let pp = await conn.getProfilePicture(m.sender) 
-        let ppp = await(await fetch(pp)).buffer()
-        conn.sendMessage(m.chat, fs.readFileSync(vn), MessageType.sticker, { quoted: m, contextInfo: { externalAdReply: {title: conn.getName(m.sender), body:"© lolibot", previewType: "PHOTO", thumbnail: ppp, sourceUrl:``}}})
-        }
-
-
-
-//Mensaje new
-if (texto1B) {
-        conn.sendMessage(m.chat, 'En ese caso el tuyo 7w7', MessageType.text, { sendEphemeral: true, quoted: m })
-        }
-
-
-
-} */ 
-/*
-module.exports = handler
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}

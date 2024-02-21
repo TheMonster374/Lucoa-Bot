39269cbcfff0692dbcc88d034b29655443295430
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-21 12:24:23 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,83 +1,160 @@
-import { randomBytes } from "crypto";
-let link = /chat.whatsapp.com/;
-let handler = async (m, { conn, text, groupMetadata }) => {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (!m.isGroup) return !1;
-  if (!text)
-    throw "*_⚠ • ️Ingrese un -texto- para enviar un mensaje a todos los grupos._*";
-  try {
-    const linkThisGroup = `${link}`;
-    if (m.text.includes(linkThisGroup))
-      return conn.reply(
-        m.chat,
-        "❌ *_No puedes espamear enlaces a otros grupos._*",
-        m
-      );
-    let time = global.db.data.users[m.sender].msgwait + 300000;
-    if (new Date() - db.data.users[m.sender].msgwait < 300000)
-      throw `*_⚠️ • Tienes que esperar ${msToTime(
-        time - new Date()
-      )} para volver a enviar un mensaje._*`;
-    let who =
-      m.mentionedJid && m.mentionedJid[0]
-        ? m.mentionedJid[0]
-        : m.fromMe
-        ? conn.user.jid
-        : m.sender;
-    let name = await conn.getName(m.sender);
-    let groups = Object.entries(conn.chats)
-      .filter(
-        ([jid, chat]) =>
-          jid.endsWith("@g.us") &&
-          chat.isChats &&
-          !chat.metadata?.read_only &&
-          !chat.metadata?.announce
-      )
-      .map((v) => v[0]);
-    let fakegif = {
-      key: {
-        participant: `0@s.whatsapp.net`,
-        ...("6289643739077-1613049930@g.us"
-          ? { remoteJid: "6289643739077-1613049930@g.us" }
-          : {}),
-      },
-      message: {
-        videoMessage: {
-          title: "null",
-          h: `Hmm`,
-          seconds: "99999",
-          gifPlayback: "true",
-          caption: "null",
-          jpegThumbnail: catalogo,
-        },
-      },
-    };
-    let teks = `*🌺 • Grupo:* ${groupMetadata.subject}\n*🍀 • De:* @${
-      m.sender.split`@`[0]
-    }\n*🍁 • Número:* wa.me/${who.split`@`[0]}\n*📧 • Mensaje:* ${text}`;
-    for (let id of groups) {
-      await conn.reply(id, teks, m, { mentions: conn.parseMention(teks) });
-      global.db.data.users[m.sender].msgwait = new Date() * 1;
-    }
-  } catch {}
-};
-handler.tags = ["fun"];
-handler.command = handler.help = ["msg"];
-handler.owner = false;
-export default handler;
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  return minutes + " m " + seconds + " s ";
-}
-const more = String.fromCharCode(8206);
-const readMore = more.repeat(4001);
-const randomID = (length) =>
-  randomBytes(Math.ceil(length * 0.5))
-    .toString("hex")
-    .slice(0, length);
+
+let { MessageType } = require("@adiwajshing/baileys");
+let util = require('util')
+let path = require('path')
+let fs = require("fs")
+let fetch = require('node-fetch')
+
+let handler = m => m
+
+handler.before = async function (m, { conn, command }) {
+if ((m.isBaileys && m.fromMe) || m.fromMe ) return true
+
+let sticker1A = /ola/i
+let sticker1B = audio1A.exec(m.text)
+
+let audio2A = /buenos dias|Buenos dias|buenos días|Buenos días/i
+let audio2B = audio2A.exec(m.text)
+
+let audio3A = /Hentai|HENTAI|hentai|antojen|hentay|Hentay|Antojen/i 
+let audio3B = audio3A.exec(m.text)
+
+let audio4A = /fiesta del admin|Fiesta del admin|atención grupo|atencion grupo|aviso importante|fiestadeladmin|fiesta en casa del admin/i
+let audio4B = audio4A.exec(m.text)
+
+let audio5A = /fiesta del admin2|fiesta del admin 2|fiestadeladmin2|Fiesta del admin 2|Fiesta del admin2/i
+let audio5B = audio5A.exec(m.text)
+
+let audio6A = /fiesta viernes|viernes|Viernes|viernes fiesta/i
+let audio6B = audio6A.exec(m.text)
+
+let audio7A = /Me olvide|ME OLVIDE|me olvide|Me olvidé|me olvidé|ME OLVIDÉ/i
+let audio7B = audio7A.exec(m.text)
+
+let audio8A = /baneado|Baneado|baneada|Baneada/i
+let audio8B = audio8A.exec(m.text)
+
+let audio9A = /error/i
+let audio9B = audio9A.exec(m.text)
+
+let audio10A = /estoy aburrid|ando aburrid|que aburrid/i
+let audio10B = audio10A.exec(m.text)
+
+let audio11A = /leche/i
+let audio11B = audio11A.exec(m.text)
+
+let audio12A = /jajajaja/i
+let audio12B = audio12A.exec(m.text)
+
+let audio13A = /suicidar/i
+let audio13B = audio13A.exec(m.text)
+
+let audio14A = /puto bot|bot puto|pvto bot|bot pvto/i
+let audio14B = audio14A.exec(m.text)
+
+let audio15A = /^(A|ª|ᵃ|a)$/i
+let audio15B = audio15A.exec(m.text)
+
+let audio16A = /Bot|bot/i
+let audio16B = audio16A.exec(m.text)
+
+
+//Mensaje new
+//let texto1A = /No tengo hermana|No tengo ermana/i
+//let texto1B = texto1A.exec(m.text)
+
+if (sticler1B) {
+	let vn  = './media/Noche.mp3'
+	conn.sendFile(m.chat, vn, 'Noche.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio2B) {
+	let vn = './media/Buenos-dias-2.mp3'
+	conn.sendFile(m.chat, vn, 'Buenos-dias-2.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio3B) {
+	let vn = './media/hentai.mp3'
+	conn.sendFile(m.chat, vn, 'hentai.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio4B) {
+	let vn = './media/Fiesta1.mp3'
+	conn.sendFile(m.chat, vn, 'Fiesta1.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio5B) {
+	let vn = './media/fiesta.mp3'
+	conn.sendFile(m.chat, vn, 'fiesta.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio6B) {
+	let vn = './media/viernes.mp3'
+	conn.sendFile(m.chat, vn, 'viernes.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio7B) {
+	let vn = './media/chica lgante.mp3'
+	conn.sendFile(m.chat, vn, 'chica lgante.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio8B) {
+	let vn = './media/baneado.mp3'
+	conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio9B) {
+	let vn = './storage/audio/Me_cago_en_la_concha.mp3'
+	conn.sendFile(m.chat, vn, 'Me_cago_en_la_concha.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio10B) {
+	let vn = './storage/audio/Juguemos_al_ascensor.mp3'
+	conn.sendFile(m.chat, vn, 'Juguemos_al_ascensor.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio11B) {
+	let vn = './storage/audio/Esto_no_es_leche.mp3'
+	conn.sendFile(m.chat, vn, 'Esto_no_es_leche.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio12B) {
+        let tt = ["./storage/audio/Eres_muy_chistoso.mp3", "./storage/audio/Gracioso_te_crees.mp3"]
+	let vn = tt[Math.floor(Math.random() * tt.length)]
+	conn.sendFile(m.chat, vn, 'Gracioso.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	} else
+
+if (audio13B) {
+	let vn = './storage/audio/El_suicidio_no_es_una_opcion.mp3'
+	conn.sendFile(m.chat, vn, 'El_suicidio_no_es_una_opcion.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	}
+
+if (audio14B) {
+	let vn = './storage/audio/Deje_de_insultarme.mp3'
+	conn.sendFile(m.chat, vn, 'Deje_de_insultarme.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	}
+
+if (audio15B) {
+	let vn = './storage/audio/A.mp3'
+	conn.sendFile(m.chat, vn, 'A.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+	}
+/*
+if (audio16B) {
+	let vn = './storage/sticker/Bot.webp'
+        let pp = await conn.getProfilePicture(m.sender) 
+        let ppp = await(await fetch(pp)).buffer()
+        conn.sendMessage(m.chat, fs.readFileSync(vn), MessageType.sticker, { quoted: m, contextInfo: { externalAdReply: {title: conn.getName(m.sender), body:"© lolibot", previewType: "PHOTO", thumbnail: ppp, sourceUrl:``}}})
+        }
+
+
+
+//Mensaje new
+if (texto1B) {
+        conn.sendMessage(m.chat, 'En ese caso el tuyo 7w7', MessageType.text, { sendEphemeral: true, quoted: m })
+        }
+
+
+
+} */ 
+/*
+module.exports = handler

923fe0aac8d5e8b5922587f00501a3c5a69fb303
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-23 08:33:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -44,6 +44,7 @@ global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
 global.imagen5 = fs.readFileSync('./src/+18.jpg');
 global.imagen6 = fs.readFileSync('./Menu3.png');
+global.imagen7 = fs.readFileSync('./storage/img/perfil.png')
 //━━━━━━IMAGENES 2━━━━━━━━━━ 
 
 global.imgmenu = fs.readFileSync('./Menu3.png') 

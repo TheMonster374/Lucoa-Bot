d7962f592904861cc802b9d62f8af569f4aeb2d0
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-24 08:59:23 -0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[✅] 𝑪𝒉𝒂𝒕 𝑫𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐*');
+  m.reply('*[🔓] 𝑪𝒉𝒂𝒕 𝑫𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];

3b207b3754a93499b8f81ae612dcb98ed60b194c
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-24 14:06:31 -0300

    Update fakeReply.js

--- plugins/fakeReply.js
+++ plugins/fakeReply.js
@@ -16,40 +16,4 @@ export async function before(m, { conn }) {
 				}
 			}
 		}
-		
-		// Respuesta de descargas de YouTube
- global.adYT = {
-			contextInfo: {
-			mentionedJid: [m.sender],
-				forwardingScore: 9999,
-				externalAdReply: {
-				showAdAttribution: true,
-					title: adyoutube,
-					body: namebot,
-					mediaUrl: null,
-					description: null,
-					previewType: "PHOTO",
-					thumbnail: miniurl,
-					sourceUrl: null
-				}
-			}
-		}
-		
-		  // Respuesta de Stickers 
- global.adSticker = {
-			contextInfo: {
-			mentionedJid: [m.sender],
-				forwardingScore: 9999,
-				externalAdReply: {
-				showAdAttribution: true,
-					title: adsticker,
-					body: namebot,
-					mediaUrl: null,
-					description: null,
-					previewType: "PHOTO",
-					thumbnail: miniurl,
-					sourceUrl: null
-				}
-			}
-		}
 }

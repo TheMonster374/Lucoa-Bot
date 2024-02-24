e32abc5611ae25859d2df86f67e081848a9da5a4
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-24 14:13:10 -0300

    Update rpg-claim.js

--- plugins/rpg-claim.js
+++ plugins/rpg-claim.js
@@ -5,10 +5,10 @@ const cooldown = 86400000 // 24 Horas
 let handler = async (m) => {
    let user = global.db.data.users[m.sender]
    if (new Date - user.lastclaim < cooldown) return conn.reply(m.chat, `🧭 𝑬𝒔𝒑𝒆𝒓𝒂 *${((user.lastclaim + cooldown) - new Date()).toTimeString()}* 𝒑𝒂𝒓𝒂 𝒗𝒐𝒍𝒗𝒆𝒓 𝒂 𝑹𝒆𝒄𝒍𝒂𝒎𝒂𝒓.`, m adReply)
-   let txt = `𝑭𝒆𝒍𝒊𝒄𝒊𝒅𝒂𝒅𝒆𝒔 🎉, 𝒓𝒆𝒄𝒍𝒂𝒎𝒂𝒔𝒕𝒆 *500* 🪙 𝑷𝒆𝒔𝒐𝒔 *.` adReply
+   let txt = `𝑭𝒆𝒍𝒊𝒄𝒊𝒅𝒂𝒅𝒆𝒔 🎉, 𝒓𝒆𝒄𝒍𝒂𝒎𝒂𝒔𝒕𝒆 *500* 🪙 𝑷𝒆𝒔𝒐𝒔 *.` 
    user.exp += 500
    user.lastclaim = new Date * 1
-   await conn.reply(m.chat, txt, m)
+   await conn.reply(m.chat, txt, m adReply)
 }
 
 handler.help = ['claim']

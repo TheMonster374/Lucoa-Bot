b85921222a2b7b33fb844abd9d24edc42b3c1859
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-25 01:47:22 -0300

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,29 +1,25 @@
-import db from '../lib/database.js'
-
-let handler = async (m, { conn }) => {
-
-  let stars = Math.floor(Math.random() * 20)
-  let time = global.db.data.users[m.sender].lastmiming + 14400000
-  if (new Date - global.db.data.users[m.sender].lastmiming < 14400000) return conn.reply(m.chat, `Espera *${msToTime(time - new Date())}* para regresar a la Mina`, m, adReply)
-  global.db.data.users[m.sender].exp += amount
-  conn.reply(m.chat, `🚩 Genial! minaste *${amount} ⭐ Estrellas*`, m, adReply)
-  global.db.data.users[m.sender].lastmiming = new Date * 1
-}
-handler.help = ['minar']
-handler.tags = ['rpg']
-handler.command = ['minar', 'miming', 'mine'] 
-handler.register = true 
-export default handler
+const handler = async (m, {conn, isPrems}) => {
+  const hasil = Math.floor(Math.random() * 1000);
+  const time = global.db.data.users[m.sender].lastmiming + 600000;
+  if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*[ ⏲️ ] espera ${msToTime(time - new Date())} para volver a conseguir pesos`;
+  m.reply(`*[ 🎉 ] Conseguiste ${hasil} Pesos*` , m adReply);
+  global.db.data.users[m.sender].lastmiming = new Date * 1;
+};
+handler.help = ['work2'];
+handler.tags = ['rpg'];
+handler.command = ['work2', 'Work2', 'trabajar2'];
+handler.fail = null;
+export default handler;
 
 function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+  const milliseconds = parseInt((duration % 1000) / 100);
+  let seconds = Math.floor((duration / 1000) % 60);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
 
-  return hours + " hora(s) " + minutes + " minuto(s) " + seconds + " segundo(s)" 
+  return minutes + ' m y ' + seconds + ' s ';
 }

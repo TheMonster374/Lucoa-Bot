db781b32efbd492924f7bb270c54d8b15ba221bc
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-25 02:11:26 -0300

    Create rpg-buy.js

---
+++ plugins/rpg-buy.js
@@ -0,0 +1,22 @@
+const xpperlimit = 350;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
+    conn.reply(m.chat, `
+┌─「 *𝗡𝗢𝗧𝗔 𝗗𝗘 𝗣𝗔𝗚𝗢* 」
+‣ *𝗖𝗼𝗺𝗽𝗿𝗮 𝗻𝗼𝗺𝗶𝗻𝗮𝗹* : + ${count}💎 
+‣ *𝗚𝗮𝘀𝘁𝗮𝗱𝗼* : -${xpperlimit * count} Pesos 🪙
+└──────────────`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *Pesos* para comprar *${count}* Diamantes💎`, m);
+};
+handler.help = ['Buy <cantidad>', 'Buyall'];
+handler.tags = ['rpg'];
+handler.command = ['buy', 'buyall'];
+
+handler.disabled = false;
+
+export default handler;

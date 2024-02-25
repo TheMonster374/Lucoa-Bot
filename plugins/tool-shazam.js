23d57b9ae1cd12997812a7ba70a4106655d2fe8e
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-25 15:29:41 -0300

    Update tool-shazam.js

--- plugins/tool-shazam.js
+++ plugins/tool-shazam.js
@@ -1,45 +1,37 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import request from 'request';
+import fs from 'fs';
 import acrcloud from 'acrcloud';
-
-let acr = new acrcloud({
+const acr = new acrcloud({
   host: 'identify-eu-west-1.acrcloud.com',
   access_key: 'c33c767d683f78bd17d4bd4991955d81',
-  access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
-})
+  access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu',
+});
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-  try {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (/video|audio/.test(mime)) {
-      let buffer = await q.download()
-      let { status, metadata } = await acr.identify(buffer)
-      if (status.code !== 0) {
-        buffer = await q.download() 
-        let retry = await acr.identify(buffer)
-        if (retry.status.code !== 0) {
-          throw retry.status.msg; 
-        }
-        metadata = retry.metadata;
-      }
-      let { title, artists, album, genres, release_date } = metadata.music[0]
-      let txt = `🌿⁀➷ *Titulo* : ${title}${artists ? `\n🐢⁀➷ *Artista* : ${artists.map(v => v.name).join(', ')}` : ''}`
-      txt += `${album ? `\n🌿⁀➷ *Album* : ${album.name}` : ''}${genres ? `\n🐢⁀➷ *Genero* : ${genres.map(v => v.name).join(', ')}` : ''}\n`
-      txt += `🌿⁀➷ *Fecha de lanzamiento* : ${release_date}`
-    // await conn.reply(m.chat, txt, m)
-      let img = await (await fetch(`https://i.postimg.cc/B6CDnZG3/wonder-egg-priority-icons.jpg`)).buffer()
-      await conn.sendMessage(m.chat, { text: txt, contextInfo: { mentionedJid: [m.sender], forwardingScore: 9, externalAdReply: { title: 'Jotchua-Mini', body: 'Wʜᴀᴛꜱᴀᴘᴘ Bᴏᴛ - Mᴜʟᴛɪ Dᴇᴠɪᴄᴇ', thumbnail: img, thumbnailUrl: img, sourceUrl: 'https://www.instagram.com/Ale2_0_2/', mediaType: 1 }}}, { quoted: m })
-    } else {
-      return await conn.sendMessage(m.chat, { text: "*Responde a un audio o video*", contextInfo: {   mentionedJid: [m.sender], forwardingScore: 9, externalAdReply: { title: 'Jotchua-Mini', body: 'Wʜᴀᴛꜱᴀᴘᴘ Bᴏᴛ - Mᴜʟᴛɪ Dᴇᴠɪᴄᴇ', thumbnail: img, thumbnailUrl: img, sourceUrl: 'https://www.instagram.com/Ale2_0_2/', mediaType: 1 }}}, { quoted: m })
-    }
-  } catch (error) {
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (/audio|video/.test(mime)) {
+    if ((q.msg || q).seconds > 20) return m.reply('Archivo demasiado grande, intente con uno menos pesado.');
+    const media = await q.download();
+    const ext = mime.split('/')[1];
+    fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media);
+    const res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`));
+    const {code, msg} = res.status;
+    if (code !== 0) throw msg;
+    const {title, artists, album, genres, release_date} = res.metadata.music[0];
+    const txt = `*Resultados*
 
-  }
-}
-handler.help = ['whatmusic *<audio/video>*']
+*Titulo:* ${title}
+*Artista:* ${artists !== undefined ? artists.map((v) => v.name).join(', ') : 'No encontrado'}
+*Album:* ${album.name || 'No encontrado'}
+*Genero:* ${genres !== undefined ? genres.map((v) => v.name).join(', ') : 'No encontrado'}
+*Fecha de lanzamiento:* ${release_date || 'No encontrado'}
+`.trim();
+    fs.unlinkSync(`./tmp/${m.sender}.${ext}`);
+    m.reply(txt);
+  } else throw 'error';
+};
+
+handler.help = ['shazam (audio)']
 handler.tags = ['tools']
-handler.command = /^(whatmusic|shazam)$/i
-handler.limit = 2
-export default handler
+handler.command = /^shazam|quemusicaes|whatmusic$/i;
+export default handler;

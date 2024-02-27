b74f6a01aee111959c6df8b8967002c85d6302fd
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-27 06:58:12 -0300

    Update search-animeinfo.js

--- plugins/search-animeinfo.js
+++ plugins/search-animeinfo.js
@@ -3,10 +3,10 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { conn, text }) => {
-if (!text) throw `*Masukan Judul Manga Yang Ingin Kamu Cari !*`
-conn.reply(m.chat, 'Sedang mencari manga... Silahkan tunggu', m)
+if (!text) throw `*¡Ingresa el título del manga que deseas buscar!*`
+conn.reply(m.chat, 'Buscando manga... Por favor espera', m)
 let res = await fetch('https://api.jikan.moe/v4/manga?q=' + text)
-if (!res.ok) throw 'Tidak Ditemukan'
+if (!res.ok) throw 'error'
 let json = await res.json()
 let { chapters, url, type, score, scored, scored_by, rank, popularity, members, background, status, volumes, synopsis, favorites } = json.data[0]
 let judul = json.data[0].titles.map(jud => `${jud.title} [${jud.type}]`).join('\n');
@@ -18,18 +18,11 @@ let animeingfo = `📚 titulo: ${judul}
 ✉️ transmisión: ${type}
 🗂 Estado: ${status}
 😎 Género: ${genrenya}
-🗃 volúmen: ${volumes}
 🌟 favoritos: ${favorites}
 🧮 puntaje: ${score}
-🧮 Anotado: ${scored}
-🧮 Anotado por: ${scored_by}
 🌟 Rank: ${rank}
-🤩 popularidad: ${popularity}
-👥 Miembros: ${members}
 ⛓️ Url: ${url}
 👨‍🔬 Autor: ${xnuvers007}
-📝 Fondo: ${background}
-💬 Sinopsis: ${synopsis}
 `
 conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'manga.jpg', `*MANGA INFO*\n` + animeingfo, m)
 }

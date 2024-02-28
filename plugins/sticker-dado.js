1ad36bcbb68a734beb439ae4347077f5cca948b9
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 11:03:41 -0300

    Update sticker-dado.js

--- plugins/sticker-dado.js
+++ plugins/sticker-dado.js
@@ -1,5 +1,7 @@
 const handler = async (m, {conn}) => {
   conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/sticker/dadu?apikey=${lolkeysapi}`, 'error.webp', '', m, {asSticker: true});
 };
+handler.tags = ['sticker'];
 handler.command = ['dado', 'dados', 'dadu'];
+handler.help = ['dado'];
 export default handler;

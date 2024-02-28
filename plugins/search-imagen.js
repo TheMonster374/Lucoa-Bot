21ed52627bc902ba678353b1af4d55179d0ac6c8
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 07:31:30 -0300

    Update search-imagen.js

--- plugins/search-imagen.js
+++ plugins/search-imagen.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
     const res = await googleImage(text);
   const image = await res.getRandom();
   const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐 𝒅𝒆: ${text}`, m);
+  conn.sendFile(m.chat, link, 'error.jpg', `𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐 𝒅𝒆: ${text}`, m, adReply);
 };
 handler.help = ['imagen <texto>'];
 handler.tags = ['search'];

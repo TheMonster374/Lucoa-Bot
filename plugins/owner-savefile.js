8e6b51803ebfc32cbc83b4a765d73bd1ccf35dee
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 11:32:38 -0300

    Update and rename logo-rem.js to owner-savefile.js

---
+++ plugins/owner-savefile.js
@@ -0,0 +1,15 @@
+//creditos a StarLigthsTeam
+import fs from 'fs'
+let handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) return conn.reply(m.chat, `Que nombre le pongo al archivo?`, m, adReply)
+    if (!m.quoted.text) return conn.reply(m.chat, `Responder al mensaje!`, m, adReply)
+    let path = `${text}.js`
+    await fs.writeFileSync(path, m.quoted.text)
+    await conn.reply(m.chat, `Guardado en ${path}`, m, adReply)
+}
+handler.help = ['savefile'].map(v => v + ' <nombre>')
+handler.tags = ['owner']
+handler.command = ["savefile"]
+
+handler.rowner = true
+export default handler

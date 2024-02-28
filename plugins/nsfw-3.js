127ba91d62e0dd5c174f0795019fe3255464cd69
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 14:45:17 -0300

    Update nsfw-3.js

--- plugins/nsfw-3.js
+++ plugins/nsfw-3.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (global.db.data.chats[m.chat].game == false && m.isGroup) return conn.reply(m.chat, 'Nsfw Tidak Aktif Di Chat Ini\n\nSilahkan Ketik .on nsfw\nUntuk Mengaktifkan Game ', fkontak)
-let hentai = JSON.parse(fs.readFileSync('./json/hentai.json'))
+let hentai = JSON.parse(fs.readFileSync('src/JSON/hentaxi.json'))
 let res =  hentai[Math.floor(Math.random() * hentai.length)]
 conn.sendButtonImg(m.chat, res, '❏  *H E N T A I*\n\nTch, Dasar Sange', author, 'Next', usedPrefix + 'hentai', m)
 }

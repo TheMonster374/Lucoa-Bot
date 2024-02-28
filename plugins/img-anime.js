4f53e363e60d162b945991e3b671786486926469
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 01:53:31 -0300

    Update img-anime.js

--- plugins/img-anime.js
+++ plugins/img-anime.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/AleXD0009/jotchua-mini/master/src/JSON/anime-${command}.json`)).data;
+  const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-${command}.json`)).data;
   const haha = await res[Math.floor(res.length * Math.random())];
   conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m);
 };

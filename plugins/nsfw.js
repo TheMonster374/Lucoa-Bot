6524e0c185b6e8601bbfb81ca5b1ea35d108f69e
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 14:27:54 -0300

    Update nsfw.js

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,17 +1,20 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!global.db.data.chats[m.chat].nsfw) throw `🚫 en este grupo no esta permtido contenido nsfw \n\n si eres admin y quieres activarlo usa el siguiente comando: \n*${usedPrefix}enable* nsfw`
+
+ m.react(rwait)
 
 let res = await fetch(`https://fantox-apis.vercel.app/${command}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.url) throw '❎ Error'
 conn.sendFile(m.chat, json.url, 'img.jpg', `✅ Random ${command}`, m)
+m.react(dwait)
 }
-handler.help = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
+handler.help = ['18menu']
 handler.command = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
-handler.tags = ['nsfw'] 
+handler.tags = ['nsfw', 'premium'] 
+
 
 
 export default handler

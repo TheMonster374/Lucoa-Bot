847c8293ec17d0155afea75aa131125c8bf8a833
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-28 14:30:43 -0300

    Update nsfw.js

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,22 +1,19 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-let res = await fetch(`https://fantox-apis.vercel.app/${command}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.url) throw '❎ Error'
-conn.sendFile(m.chat, json.url, 'img.jpg', `✅ Random ${command}`, m)
-m.react(dwait)
-}
-handler.help = ['18menu']
-handler.command = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
-handler.tags = ['nsfw', 'premium'] 
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
 
 
+  if (!args[0]) throw `select tag:\nblowjob\nneko\ntrap\nwaifu`;
+
+  let res = await fetch(`https://api.waifu.pics/nsfw/${text}`);
+  if (!res.ok) throw await res.text();
+
+  let json = await res.json();
+  if (!json.url) throw 'Error!';
+
+  conn.sendFile(m.chat, json.url, '', global.wm, m);
+};
+
+handler.command = /^(nsfw)$/i
 
 export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}

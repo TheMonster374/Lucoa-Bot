1927cf0be9bdb1b591cda7d24c18ad56e10bf110
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-29 10:30:02 -0300

    Update rg-mysn.js

--- plugins/rg-mysn.js
+++ plugins/rg-mysn.js
@@ -4,8 +4,8 @@ const handler = async function(m, {conn, text, usedPrefix}) {
   m.reply(`*Tu Numero de serie es:*  ${sn}
 `.trim());
 };
-const fkontak2 = { key: { participant: '0@s.whatsapp.net' }, message: { contactMessage: { displayName: packname, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${author},;;;\nFN:${author},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, jpegThumbnail: fs.readFileSync('/Menu1.png'), thumbnail: fs.readFileSync('/Menu1.png'), sendEphemeral: true }}}
-   conn.sendMessage(m.chat, {image: imagen1, caption: sn.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: imagen1, caption: reply.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
 handler.help = ['myns'];
 handler.tags = ['rg'];
 handler.command = /^(myns|ceksn)$/i;

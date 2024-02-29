de33831ddef6603b21e8b234db0c7941a80ad174
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-29 10:31:07 -0300

    Update rg-mysn.js

--- plugins/rg-mysn.js
+++ plugins/rg-mysn.js
@@ -1,11 +1,12 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {conn, text, usedPrefix}) {
   const sn = createHash('md5').update(m.sender).digest('hex');
-  m.reply(`*Tu Numero de serie es:*  ${sn}
+  const str =
+ /* m.reply*/(`*Tu Numero de serie es:*  ${sn}
 `.trim());
 };
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: imagen1, caption: reply.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      conn.sendMessage(m.chat, {image: imagen1, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
 handler.help = ['myns'];
 handler.tags = ['rg'];
 handler.command = /^(myns|ceksn)$/i;

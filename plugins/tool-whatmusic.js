a7fe1769eed4bb8d3a757bc7c746a61429c6354d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-02-29 11:23:37 -0300

    Update tool-whatmusic.js

--- plugins/tool-whatmusic.js
+++ plugins/tool-whatmusic.js
@@ -1,37 +1,27 @@
-import fs from 'fs';
-import acrcloud from 'acrcloud';
-const acr = new acrcloud({
+import acrcloud from 'acrcloud'
+
+let acr = new acrcloud({
   host: 'identify-eu-west-1.acrcloud.com',
   access_key: 'c33c767d683f78bd17d4bd4991955d81',
-  access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu',
-});
-
-const handler = async (m) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || '';
-  if (/audio|video/.test(mime)) {
-    if ((q.msg || q).seconds > 20) return m.reply('Archivo demasiado grande, intente con uno menos pesado.');
-    const media = await q.download();
-    const ext = mime.split('/')[1];
-    fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media);
-    const res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`));
-    const {code, msg} = res.status;
-    if (code !== 0) throw msg;
-    const {title, artists, album, genres, release_date} = res.metadata.music[0];
-    const txt = `*Resultados*
-
-*Titulo:* ${title}
-*Artista:* ${artists !== undefined ? artists.map((v) => v.name).join(', ') : 'No encontrado'}
-*Album:* ${album.name || 'No encontrado'}
-*Genero:* ${genres !== undefined ? genres.map((v) => v.name).join(', ') : 'No encontrado'}
-*Fecha de lanzamiento:* ${release_date || 'No encontrado'}
-`.trim(adReply);
-    fs.unlinkSync(`./tmp/${m.sender}.${ext}`);
-    m.reply(txt, m, adReply);
-  } else throw 'error';
-};
-
-handler.help = ['whatmusic (audio)']
+  access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
+})
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ''
+  if (/video|audio/.test(mime)) {
+  let buffer = await q.download()
+  let { status, metadata } = await acr.identify(buffer)
+  if (status.code !== 0) throw status.msg 
+  let { title, artists, album, genres, release_date } = metadata.music[0]
+  let txt = `*• Titulo:* ${title}${artists ? `\n*• Artists:* ${artists.map(v => v.name).join(', ')}` : ''}`
+  txt += `${album ? `\n*• Album:* ${album.name}` : ''}${genres ? `\n*• Genero:* ${genres.map(v => v.name).join(', ')}` : ''}\n`
+  txt += `*• Fecha de lanzamiento:* ${release_date}`
+  await conn.reply(m.chat, txt, m, adReply)
+  } else return conn.reply(m.chat, `*🚩 Responde a un audio o video.*`, m, adReply)
+}
+handler.help = ['whatmusic <audio/video>']
 handler.tags = ['tools']
-handler.command = /^shazam|quemusicaes|whatmusic$/i;
-export default handler;
+handler.command = /^(whatmusic|shazam)$/i
+handler.star = 2
+handler.register = true 
+export default handler

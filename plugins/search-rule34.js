4cbb84b66184ca1bdf0591c805e6412558273f3d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-01 14:44:33 -0300

    Update search-rule34.js

--- plugins/search-rule34.js
+++ plugins/search-rule34.js
@@ -4,8 +4,9 @@ if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `❎ En este g
 
 if (!text) return conn.reply(m.chat, `[📚] 𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒖𝒏 𝒕𝒆𝒙𝒕𝒐 𝒋𝒖𝒏𝒕𝒐 𝒂𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐\n\n[💡] 𝑬𝒋𝒆𝒎𝒑𝒍𝒐: ${usedPrefix + command} *gawr gura.*`, m, )
 const res = await (await googleImage('rule34 ' + text)).getRandom()
-await conn.sendFile(m.chat, res, 'error.jpg', `*Resultado de ∙* ${text ? text.capitalize() : false}`, estilo)
+await conn.sendFile(m.chat, res, 'error.jpg', `*Resultado de ∙* ${text ? text.capitalize() : false}`, )
 }
+ if (googleImage) await conn.sendFile(m.chat, googleImage, 'rule34.jpg', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: true, title: packname, body: `Simple whatsapp bot`, mediaType: 2, sourceUrl: group, thumbnail: catalogo}}}, { quoted: m })
 handler.help = ['rule34']
 handler.tags = ['search', 'nsfw']
 handler.command = ['rule34', 'r34']

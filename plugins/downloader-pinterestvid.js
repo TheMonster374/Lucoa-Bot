2aaf8eea6cf5695198e1f512aeb89cf29e690ade
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-02 11:43:27 -0300

    Update downloader-pinterestvid.js

--- plugins/downloader-pinterestvid.js
+++ plugins/downloader-pinterestvid.js
@@ -13,17 +13,3 @@ handler.tags = ['downloader']
 handler.command = /^(vpinterest|vidpin|pintvid)$/i
 
 export default handler
-
-async function pinterest(query) {
-		let $ = cheerio.load(await res.text())
-		let data = $('table[class="table table-condensed table-striped table-bordered"]').find('a').attr('href')
-		if (!data) throw 'Can\'t download post :/'
-		return data
-	} else {
-		let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${query}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${query}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`)
-		let json = await res.json()
-		let data = json.resource_response.data.results
-		if (!data.length) throw `Query "${query}" not found :/`
-		return data[~~(Math.random() * (data.length))].images.orig.url
-	}
-

532f9022b905d1d0fdbcb9fb9dd6e8cf31574176
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-02 11:39:39 -0300

    Update downloader-pinterestvid.js

--- plugins/downloader-pinterestvid.js
+++ plugins/downloader-pinterestvid.js
@@ -8,9 +8,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	let res = await pinterest(text)
 	// if (!res) throw res
 	let mime = await lookup(res)
-	text.match(URL_REGEX) ?
 		await conn.sendMessage(m.chat, { [mime.split('/')[0]]: { url: res }, caption: `Succes Download: ${await shortUrl(res)}` }, { quoted: m }) :
-	await conn.sendButton(m.chat, `Result From: ${text.capitalize()}`, await shortUrl(res), res, [['Next', `${usedPrefix + command} ${text}`]], m)
+	await conn.sendButton(m.chat, `Result From: `, await shortUrl(res), res, [['Next', `${usedPrefix + command} ${text}`]], m)
 }
 handler.help = ['pinterest']
 handler.tags = ['downloader']

0ad971c4734403839c962038768daeacce8d18fd
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-02 10:26:22 -0300

    Update search-imagen.js

--- plugins/search-imagen.js
+++ plugins/search-imagen.js
@@ -4,9 +4,12 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
     const res = await googleImage(text);
   const image = await res.getRandom();
   const link = image;
-if (image) conn.sendFile(m.chat, image, 'imagen.jpg', '𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐 𝒅𝒆: ${text}',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `Whatsapp Bot Multi Device`, mediaType: 2, sourceUrl: md, thumbnail: imagen1}}}, { quoted: m })
+if (image) conn.sendFile(m.chat, image, 'imagen.jpg', '𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐 𝒅𝒆:' ${text}',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `Whatsapp Bot Multi Device`, mediaType: 2, sourceUrl: md, thumbnail: imagen1}}}, { quoted: m })
 };
 handler.help = ['imagen <texto>'];
 handler.tags = ['search'];
 handler.command = /^(gimage|image|imagen)$/i;
 export default handler;
+
+
+  /*  conn.sendFile(m.chat, link, 'error.jpg', `𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐 𝒅𝒆: ${text}`, m);*/

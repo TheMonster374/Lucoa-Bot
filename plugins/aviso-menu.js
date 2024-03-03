c132296fb800b86aae0c4d46e9cee39cde6358ab
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-03 11:19:52 -0300

    Create aviso-menu.js

---
+++ plugins/aviso-menu.js
@@ -0,0 +1,25 @@
+/*import { xpRange } from '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'*/
+let handler = async (m, { conn, usedPrefix, command}) => {
+  
+
+let Reglas = ` 
+`.trim()
+await m.react('❌')  
+await conn.sendFile(m.chat, imagen1, 'gata.mp4', Reglas)
+}
+handler.customPrefix = /menu|menubot|menu|uso, usobot|uso del bot/i
+handler.command = new RegExp
+handler.register = true
+handler.exp = 70
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}

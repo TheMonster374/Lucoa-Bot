b5bc1d4b09d15dd5512927381d4d8dc6ee8f7b2e
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 14:34:27 -0300

    🐶

--- plugins/downloader-historiaig.js
+++ plugins/downloader-historiaig.js
@@ -1,21 +1,21 @@
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-  if (!args[0]) throw `*Ingresa el nombre de un usuario*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Illenium*`;
+  if (!args[0]) throw `[📚] 𝐼𝑛𝑔𝑟𝑒𝑠𝑎 𝑒𝑙 𝑛𝑜𝑚𝑏𝑟𝑒 𝑑𝑒 𝑢𝑛 𝑢𝑠𝑢𝑎𝑟𝑖𝑜\n\n*[💡] 𝐸𝑗𝑒𝑚𝑝𝑙𝑜:*\n*${usedPrefix + command} Luisitocomunica*`;
   await m.reply(global.wait);
   const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`);
   const anu = await res.json();
   const anuku = anu.result;
-  if (anuku == '') return m.reply('*[❗] 𝑼𝒔𝒖𝒂𝒓𝒊𝒐 𝑰𝒏𝒗𝒂𝒍𝒊𝒅𝒐 𝒐 𝑺𝒊𝒏 𝑯𝒊𝒔𝒕𝒐𝒓𝒊𝒂𝒔*');
+  if (anuku == '') return m.reply('[❗] 𝑈𝑠𝑢𝑎𝑟𝑖𝑜 𝑖𝑛𝑣𝑎𝑙𝑖𝑑𝑜 𝑜 𝑠𝑖𝑛 ℎ𝑖𝑠𝑡𝑜𝑟𝑖𝑎𝑠');
   for (const i of anuku) {
     const res = await axios.head(i);
     const mime = res.headers['content-type'];
     if (/image/.test(mime)) {
       await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => {
-        return m.reply('*[❗] 𝑼𝒔𝒖𝒂𝒓𝒊𝒐 𝑰𝒏𝒗𝒂𝒍𝒊𝒅𝒐 𝒐 𝑺𝒊𝒏 𝑯𝒊𝒔𝒕𝒐𝒓𝒊𝒂𝒔*');
+        return m.reply('[❗] 𝑈𝑠𝑢𝑎𝑟𝑖𝑜 𝑖𝑛𝑣𝑎𝑙𝑖𝑑𝑜 𝑜 𝑠𝑖𝑛 ℎ𝑖𝑠𝑡𝑜𝑟𝑖𝑎𝑠');
       });
     }
     if (/video/.test(mime)) {
       await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => {
-        return m.reply('*[❗] 𝑼𝒔𝒖𝒂𝒓𝒊𝒐 𝑰𝒏𝒗𝒂𝒍𝒊𝒅𝒐 𝒐 𝑺𝒊𝒏 𝑯𝒊𝒔𝒕𝒐𝒓𝒊𝒂𝒔*');
+        return m.reply('[❗] 𝑈𝑠𝑢𝑎𝑟𝑖𝑜 𝑖𝑛𝑣𝑎𝑙𝑖𝑑𝑜 𝑜 𝑠𝑖𝑛 ℎ𝑖𝑠𝑡𝑜𝑟𝑖𝑎𝑠');
       });
     }
   }

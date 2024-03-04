8519b2f76e97113b05a5c915ad081ccab3de46b2
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 06:07:36 -0300

    Update aviso-menu.js

--- plugins/aviso-menu.js
+++ plugins/aviso-menu.js
@@ -11,7 +11,7 @@ let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.cha
   
 let Reglas = `⚜ 𝐼𝑁𝐹𝑂 𝐷𝐸𝐿 𝑀𝐸𝑁𝑈 ⚜\n\n𝐸𝑙 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑚𝑒𝑛𝑢 𝑐𝑎𝑚𝑏𝑖𝑜 𝑎ℎ𝑜𝑟𝑎 𝑒𝑠\n --> .comandos 
 `.trim()
-await conn.sendFile(m.chat, catalogo, reglas, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: group, thumbnail: await (await fetch(pp)).buffer() }}})
+await conn.sendFile(m.chat, catalogo, Reglas, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: group, thumbnail: await (await fetch(pp)).buffer() }}})
 }
 handler.customPrefix = /menu|menubot|menu|uso, usobot|uso del bot/i
 handler.command = new RegExp

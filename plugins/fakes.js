45f1f850be07dce823e8591215c6a30bd1b26130
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 05:13:36 -0300

    Update fakes.js

--- plugins/fakes.js
+++ plugins/fakes.js
@@ -113,7 +113,7 @@ global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', s
 
 global.nombre = conn.getName(m.sender)
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: saludo + ` ${m.name}`, body: dev, sourceUrl: enlaces, thumbnailUrl: fotos }}}
+global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: saludo + ` ${m.name}`, body: dev, sourceUrl: md, thumbnailUrl: imagen1 }}}
 }
 
 export default handler

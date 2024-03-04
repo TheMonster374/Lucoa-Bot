a8e9aba80ecc95227feb9533f7b10b4d43e29d80
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 14:32:35 -0300

    🐶

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
 const handler = async (m, {args, usedPrefix, command}) => {
-  if (!args[0]) throw `[📚] 𝑰𝒏𝒈𝒓𝒆𝒔𝒆 𝒖𝒏 𝒆𝒏𝒍𝒂𝒄𝒆 𝒅𝒆 𝑮𝒊𝒕𝑯𝒖𝒃.\n\n*[💡] 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:* _${usedPrefix + command} https://github.com/AleXD0009/Jotchua-mini_`;
-  if (!regex.test(args[0])) throw '[ ❌ ] El enlace que proporcionó es incorrecto.*';
+  if (!args[0]) throw `[📚] 𝐼𝑛𝑔𝑟𝑒𝑠𝑎 𝑢𝑛 𝑒𝑛𝑙𝑎𝑐𝑒 𝑑𝑒 𝑔𝑖𝑡ℎ𝑢𝑏.\n\n*[💡] 𝐸𝑗𝑒𝑚𝑝𝑙𝑜:* _${usedPrefix + command} https://github.com/AleXD0009_`;
+  if (!regex.test(args[0])) throw '[ ❌ ] 𝐸𝑙 𝑒𝑛𝑙𝑎𝑐𝑒 𝑞𝑢𝑒 𝑝𝑟𝑜𝑝𝑜𝑟𝑐𝑖𝑜𝑛ó 𝑒𝑠 𝑖𝑛𝑐𝑜𝑟𝑟𝑒𝑐𝑡𝑜.';
   let [_, user, repo] = args[0].match(regex) || [];
   repo = repo.replace(/.git$/, '');
   const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
   const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  m.reply(`*[ ⏳ ] 𝑺𝒆 𝒆𝒔𝒕á 𝒆𝒏𝒗𝒊𝒂𝒏𝒅𝒐 𝒆𝒍 𝒂𝒓𝒄𝒉𝒊𝒗𝒐. 𝒆𝒔𝒑𝒆𝒓𝒆...*\n\n*[ ⚠ ] 𝑺𝒊 𝒏𝒐 𝒔𝒆 𝒆𝒏𝒗í𝒂, 𝒑𝒐𝒅𝒓í𝒂 𝒔𝒆𝒓 𝒑𝒐𝒓𝒒𝒖𝒆 𝒔𝒖𝒑𝒆𝒓𝒂 𝒆𝒍 𝒍í𝒎𝒊𝒕𝒆 𝒅𝒆 𝒕𝒂𝒎𝒂ñ𝒐.*`);
+  m.reply(`[ ⏳ ] 𝑆𝑒 𝑒𝑠𝑡𝑎 𝑒𝑛𝑣𝑖𝑎𝑛𝑑𝑜 𝑒𝑙 𝑎𝑟𝑐ℎ𝑖𝑣𝑜, 𝑒𝑠𝑝𝑒𝑟𝑎...\n\n[ ⚠ ] 𝑆𝑖 𝑛𝑜 𝑠𝑒 𝑒𝑛𝑣𝑖𝑎 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑝𝑜𝑟𝑞𝑢𝑒 𝑠𝑢𝑝𝑒𝑟𝑎 𝑒𝑙 𝑙𝑖𝑚𝑖𝑡𝑒 𝑑𝑒 𝑡𝑎𝑚𝑎ñ𝑜`);
   conn.sendFile(m.chat, url, filename, null, m);
 };
 handler.help = ['gitclone']

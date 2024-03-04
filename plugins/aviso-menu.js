a431cdae56a434db07516bedd1444996e278f37d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 06:07:17 -0300

    Update aviso-menu.js

--- plugins/aviso-menu.js
+++ plugins/aviso-menu.js
@@ -3,12 +3,15 @@ import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
 import { join } from 'path'*/
 let handler = async (m, { conn, usedPrefix, command}) => {
+let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}}
+
   
     await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } })
   
 let Reglas = `⚜ 𝐼𝑁𝐹𝑂 𝐷𝐸𝐿 𝑀𝐸𝑁𝑈 ⚜\n\n𝐸𝑙 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑚𝑒𝑛𝑢 𝑐𝑎𝑚𝑏𝑖𝑜 𝑎ℎ𝑜𝑟𝑎 𝑒𝑠\n --> .comandos 
 `.trim()
-await conn.sendFile(m.chat, fake, catalogo, 'gata.mp4', Reglas)
+await conn.sendFile(m.chat, catalogo, reglas, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: group, thumbnail: await (await fetch(pp)).buffer() }}})
 }
 handler.customPrefix = /menu|menubot|menu|uso, usobot|uso del bot/i
 handler.command = new RegExp

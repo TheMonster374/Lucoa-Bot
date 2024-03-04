808a2a242b2e37fd8e5df82f8e43cbe17f50d1ba
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-04 15:21:16 -0300

    Update xd.js

--- plugins/xd.js
+++ plugins/xd.js
@@ -59,3 +59,33 @@ handler.help = ["play"].map(v => v + " <búsqueda>");
 handler.tags = ["descargador"];
 handler.command = /^A?$/i;
 export default handler;
+
+async function getlink(dl_url) {
+try {
+const baseURL = "https://drive.google.com/uc?export=download&id=";
+const match = /\/d\/(.+?)\//.exec(dl_url);
+const fileId = match[1];
+const googleDriveLink = `${baseURL}${fileId}`;
+return googleDriveLink;
+} catch (error) {
+return dl_url;
+}}
+
+async function getBuffer(url, options){
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (err) {
+		return err
+	}
+}

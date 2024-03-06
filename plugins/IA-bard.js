3001c6d829491f3457be57d37640c99427072eae
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-06 01:54:35 -0300

    🐶

--- plugins/IA-bard.js
+++ plugins/IA-bard.js
@@ -4,12 +4,6 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 
-      const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m});
-
-    
-    await conn.sendMessage(m.chat, {text: `Enviado`, edit: key}, {quoted: m});
-
-
 if (!text) return conn.reply(m.chat, `[📚] 𝐼𝑛𝑔𝑟𝑒𝑠𝑎 𝑢𝑛 𝑡𝑒𝑥𝑡𝑜\n\n*[💡] 𝐸𝑗𝑒𝑚𝑝𝑙𝑜:*, !bard conoces la omniprecencia?`,  fkontak )
 
 try {

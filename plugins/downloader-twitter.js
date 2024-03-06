ec04d06600a1748adeab4ec3e937782bc14dae63
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-05 22:56:26 -0300

    🐶

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -7,7 +7,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 const twitterUrlRegex = /^https?:\/\/(www\.)?twitter\.com\/(\w+)\/status\/(\d+)$/i
-if (!text) return conn.reply(m.chat,`uso incorrecto del comando, uso correcto ejemplo:ا \n*.twitter*  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, fkontak)
+if (!text) return conn.reply(m.chat,`[📚] 𝐼𝑛𝑔𝑟𝑒𝑠𝑎 𝑙𝑎 𝑢𝑟𝑙 𝑑𝑒 𝑢𝑛 𝑣𝑖𝑑𝑒𝑜 𝑑𝑒 𝑡𝑤𝑖𝑡𝑡𝑒𝑟 𝑞𝑢𝑒 𝑑𝑒𝑠𝑒𝑎𝑠 𝑑𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑟 \n\n[💡] 𝐸𝑗𝑒𝑚𝑝𝑙𝑜:*.twitter*  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, fkontak)
 try{ 
 const apiUrl = `https://api.lolhuman.xyz/api/twitter?apikey=GataDios&url=${encodeURIComponent(text)}`
 const response = await fetch(apiUrl)

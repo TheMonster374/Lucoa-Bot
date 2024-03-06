aefea7aca6f3919f498a1ef949b15ea1cb0a0ea4
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-06 17:31:23 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1544,14 +1544,14 @@ global.dfail = (type, m, conn) => {
   const msg = {
     rowner: '📌 Hola  %taguser *este comando solo puede ser utilizado por el propietario del bot*',
     owner: '📌 Hola @${participant.split`@`[0]} *este comando solo puede ser utilizado por el propietario del bot*',
-    mods: '📌 Hola @${participant.split`@`[0]} *este comando solo puede ser utilizado por moderadores del bot y el propietario del bot',
-    premium: '📌 Hola @${participant.split`@`[0]} *este comando solo puede ser utilizado por premiums y el propietario del bot*',
-    group: '📌 Hola ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')} *este comando solo puede ser utilizado en grupos*',
-    private: '📌 Hola @${participant.split`@`[0]} *este comando solo puede ser utilizado por chat privado del bot*',
-    admin: '📌 Hola @${participant.split`@`[0]} *este comando solo puede ser utilizado por admins del grupo*',
-    botAdmin: '📌 Hola @${participant.split`@`[0]} *para utilizar este comando es necesario que el bot sea admin del grupo*',
-    unreg: '📌 Hola @${participant.split`@`[0]} para utilizar este comando necesitas estar registrado\n\n*utiliza el comando:* _/reg nombre.edad_ *para registrarte*',
-    restrict: '📌 Hola @${participant.split`@`[0]} este comando fue desactivado por el propietario del bot',
+    mods: '📌 Hola *este comando solo puede ser utilizado por moderadores del bot y el propietario del bot',
+    premium: '📌 Hola *este comando solo puede ser utilizado por premiums y el propietario del bot*',
+    group: '📌 Hola @user *este comando solo puede ser utilizado en grupos*',
+    private: '📌 Hola  *este comando solo puede ser utilizado por chat privado del bot*',
+    admin: '📌 Hola  *este comando solo puede ser utilizado por admins del grupo*',
+    botAdmin: '📌 Hola  *para utilizar este comando es necesario que el bot sea admin del grupo*',
+    unreg: '📌 Hola  para utilizar este comando necesitas estar registrado\n\n*utiliza el comando:* _/reg nombre.edad_ *para registrarte*',
+    restrict: '📌 Hola  este comando fue desactivado por el propietario del bot',
   }[type];
   const aa = {quoted: m, userJid: conn.user.jid};
   const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕 | 𝑴𝑰𝑵𝑰', body: 'Whatsapp bot Multi device By als', thumbnail: catalogo, sourceUrl: 'https://atom.bio/als'}}}}, aa);

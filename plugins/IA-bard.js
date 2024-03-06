a5fbe17bf0a1b42dd4c7e3f45a92a4137aae2dcc
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-06 01:53:19 -0300

    Update IA-bard.js

--- plugins/IA-bard.js
+++ plugins/IA-bard.js
@@ -4,6 +4,9 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 
+      const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m});
+
+    
     await conn.sendMessage(m.chat, {text: `Enviado`, edit: key}, {quoted: m});
 
 

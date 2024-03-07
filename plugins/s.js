e07cc40c2ae0dde1836689a64ebb53c3fc3b98f0
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:36:47 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,28 +1,16 @@
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  // const b = text.split('|');
-//  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
- // if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;*/
-if (!text) return conn.sendPoll(m.chat, `xd\n*/grupo abrir*\n*grupo cerrar*\nSelecione una de esta opción`, ['owner','grupo cerrar'])
-  const caption = `Menu`.trim();
-  const options = 
-    if (args[0] === 'owner') {
-m.reply('Mi creador es')
-} else if (args[0] === 'cerrar') {
+if (!m.isGroup) return m.reply(info.group);  
+if (!isBotAdmins) return m.reply(info.botAdmin);  
+if (!isGroupAdmins) return m.reply(info.admin)
+if (!text) return conn.sendPoll(m.chat, `xd\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
+//m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
+if (args[0] === 'abrir') {
 m.reply('xd')
+await conn.groupSettingUpdate(m.chat, 'not_announcement')
+} else if (args[0] === 'cerrar') {
+m.reply('a')
 await conn.groupSettingUpdate(m.chat, 'announcement')
 }}
-  const sendPollMessage = {
-    messageContextInfo: {
-        messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="
-    },
-    pollCreationMessage: {
-        name: caption,
-        options: options,
-        selectableOptionsCount: 1,
-    }
-  };
-conn.relayMessage(m.chat, sendPollMessage, {quoted: m});
-};
+
 handler.help = ['encuesta question|option|option'];
 handler.tags = ['group'];
 handler.command = ['poll', 'encuesta'];

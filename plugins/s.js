7437b7f21ec64875656d982d3b4bb6a48fe77195
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:58:11 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -4,15 +4,15 @@ if (command == 'grupo') {
 if (!m.isGroup) return m.reply(info.group);  
 if (!isBotAdmins) return m.reply(info.botAdmin);  
 if (!isGroupAdmins) return m.reply(info.admin)
-if (!text) return conn.sendPoll(m.chat, `${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
+if (!text) return conn.sendPoll(m.chat, `hola\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
 if (args[0] === 'abrir') {
-m.reply(lenguaje.grupos.text1)
+m.reply('a')
 await conn.groupSettingUpdate(m.chat, 'not_announcement')
 } else if (args[0] === 'cerrar') {
-m.reply(lenguaje.grupos.text2)
+m.reply('xd')
 await conn.groupSettingUpdate(m.chat, 'announcement')
 }}
 
-  handler.command = ['Grupo', 'grupo'];
+handler.command = ['Grupo'];
 export default handler;

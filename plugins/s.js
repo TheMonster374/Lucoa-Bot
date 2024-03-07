7b24c037145335e17985665e699a6ed990089652
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:34:10 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,11 +1,16 @@
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  let name = await conn.getName(m.sender);
-  if (name == 'undefined') name = 'Indefinido';
-  const b = text.split('|');
-  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
-  if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+  // const b = text.split('|');
+//  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+ // if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;*/
+if (!text) return conn.sendPoll(m.chat, `${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['owner','grupo cerrar'])
   const caption = `Menu`.trim();
-  const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
+  const options = 
+    if (args[0] === 'owner') {
+m.reply('Mi creador es')
+} else if (args[0] === 'cerrar') {
+m.reply('xd')
+await conn.groupSettingUpdate(m.chat, 'announcement')
+}}
   const sendPollMessage = {
     messageContextInfo: {
         messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="

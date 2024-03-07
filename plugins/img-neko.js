00f55f7204f6a25f5f5f4b362ef6355a1e79d731
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 18:24:06 -0300

    🐶

---
+++ plugins/img-neko.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+   let res = await fetch('https://api.waifu.pics/sfw/neko')
+   if (!res.ok) return 
+   let json = await res.json()
+   if (!json.url) return 
+   await conn.sendFile(m.chat, json.url, 'neko.png', '*NEKO*', m)
+}
+
+handler.help = ['neko']
+handler.tags = ['img']
+handler.command = ['neko']
+
+export default handler

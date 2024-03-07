87c1b0c76ec29bfd8ffd13af50a46d21f578451e
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:20:15 -0300

    Update and rename .js to s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,13 +1,3 @@
-const fs = require("fs")
-const path = require("path")
-const chalk = require("chalk");
-const moment = require('moment-timezone') 
-const gradient = require('gradient-string') 
-const fetch = require('node-fetch') 
-const axios = require('axios')
-const cheerio = require('cheerio')
-const Jimp = require('jimp')
-const os = require('os')
 
 const handler = async (m, {conn, isAdmin, isOwner, args, Prefix, command}) => {
 

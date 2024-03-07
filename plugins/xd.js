843e6af10f4c8ffcbedc751cc4417bb85a2fd33e
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 18:19:04 -0300

    Update xd.js

--- plugins/xd.js
+++ plugins/xd.js
@@ -1,46 +1,17 @@
-import { tmpdir } from 'os'
-import path, { join } from 'path'
-import { readdirSync, statSync, unlinkSync, existsSync } from 'fs'
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-
-  const tmp = [tmpdir(), join(__dirname, '../tmp')];
-  const filenames = [];
-
-  tmp.forEach(dirname => {
-    readdirSync(dirname).forEach(file => {
-      filenames.push(join(dirname, file));
-    });
-  });
-
-  const deletedFiles = [];
-
-  filenames.forEach(file => {
-    const stats = statSync(file);
-
-    if (stats.isDirectory()) {
-      console.log(`Skipping directory: ${file}`);
-    } else {
-      unlinkSync(file);
-      deletedFiles.push(file);
-    }
-  });
-
-  conn.reply(m.chat, 'Success!', m);
-
-  if (deletedFiles.length > 0) {
-    console.log('Deleted files:', deletedFiles);
-    conn.reply(m.chat, `Deleted files:\n${deletedFiles.join('\n')}`, m);
-  }
-
-  if (deletedFiles.length == 0) {
-    conn.reply(m.chat, 'Tidak ada file yang tersisa di tmp', m);
-  }
+let handler = async (m, { conn, usedPrefix, command }) => {
+   await m.react('🕓')
+   let res = await fetch('https://api.waifu.pics/nsfw/neko')
+   if (!res.ok) return m.react('❌')
+   let json = await res.json()
+   if (!json.url) return m.react('❌')
+   await conn.sendFile(m.chat, json.url, 'xneko.png', '*RANDOM NEKO*', m)
+   await m.react('✅')
 }
 
-handler.help = ['cleartmp']
-handler.tags = ['owner']
-handler.command = /^(cleartmp|clear|tmpclear|cleantmp)$/i
-handler.rowner = true
+handler.help = ['xneko']
+handler.tags = ['nsfw']
+handler.command = ['xneko']
 
 export default handler

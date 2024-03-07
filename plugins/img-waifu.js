429752cff50c102329a2206618a88786f9e6ec44
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 18:25:17 -0300

    🐶

---
+++ plugins/img-waifu.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/waifu')
+   if (!res.ok) return 
+   let json = await res.json()
+   if (!json.url) return 
+   await conn.sendFile(m.chat, json.url, 'waifu.png', '*WAIFU*', m)
+}
+
+handler.help = ['waifu']
+handler.tags = ['img']
+handler.command = ['waifu']
+
+
+export default handler

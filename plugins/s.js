ba6c08a64b5cc117711a0beedbc952412728feeb
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:51:53 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,17 +1,15 @@
+require('../main')
+
+if (command == 'grupo') {
 if (!m.isGroup) return m.reply(info.group);  
 if (!isBotAdmins) return m.reply(info.botAdmin);  
 if (!isGroupAdmins) return m.reply(info.admin)
-if (!text) return conn.sendPoll(m.chat, `xd\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
+if (!text) return conn.sendPoll(m.chat, `${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
 if (args[0] === 'abrir') {
-m.reply('xd')
+m.reply(lenguaje.grupos.text1)
 await conn.groupSettingUpdate(m.chat, 'not_announcement')
 } else if (args[0] === 'cerrar') {
-m.reply('a')
+m.reply(lenguaje.grupos.text2)
 await conn.groupSettingUpdate(m.chat, 'announcement')
-}
-
-handler.help = ['encuesta question|option|option'];
-handler.tags = ['group'];
-handler.command = ['poll', 'encuesta'];
-export default handler;
+}}

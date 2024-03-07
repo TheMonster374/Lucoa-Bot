e344c8d7f308c9d6d5eeab568a81833ada173ed4
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 20:24:33 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,16 +1,24 @@
-
-const handler = async (m, {conn, isAdmin, isOwner, args, Prefix, command}) => {
-
-  if (!(isAdmin || isOwner)) {
-if (!text) return conn.sendPoll(m.chat, `hola\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
-//m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
-if (args[0] === 'abrir') {
-m.reply('hola')
-await conn.groupSettingUpdate(m.chat, 'not_announcement')
-} else if (args[0] === 'cerrar') {
-m.reply('grupo')
-await conn.groupSettingUpdate(m.chat, 'announcement')
-}}
-
-handler.command = /^(grupo)$/i;
-module.exports = { s }
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+  let name = await conn.getName(m.sender);
+  if (name == 'undefined') name = 'Indefinido';
+  const b = text.split('|');
+  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+  if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+  const caption = `*ᴇᴄᴜᴇsᴛᴀ ʀᴇᴀʟɪᴢᴀᴅᴀ ᴘᴏʀ:*\n${name}\n*ᴘʀᴇɢᴜɴᴛᴀ:*\n${text.split('|')[0]}`.trim();
+  const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
+  const sendPollMessage = {
+    messageContextInfo: {
+        messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="
+    },
+    pollCreationMessage: {
+        name: caption,
+        options: options,
+        selectableOptionsCount: 1,
+    }
+  };
+conn.relayMessage(m.chat, sendPollMessage, {quoted: m});
+};
+handler.help = ['encuesta question|option|option'];
+handler.tags = ['group'];
+handler.command = ['poll', 'encuesta'];
+export default handler;

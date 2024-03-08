45a85c6a53d1759b0dcf61ad797d3802667e246a
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 08:09:28 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -24,12 +24,11 @@ handler.command = ['a', 'test'];
 export default handler;*/
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
 const sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
-if (!text) return sendPoll(m.chat, `*grupo abrir*\n* grupo cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
+if (!text) return sendPoll(m.chat, `TEST`, ['owner','grupo cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
   conn.relayMessage(m.chat, sendPoll, {quoted: m});
-if (args[0] === 'grupo abrir') {
-m.reply('grupo abierto')
-await conn.groupSettingUpdate(m.chat, 'not_announcement')
+if (args[0] === 'owner') {
+m.reply('mi creador es')
 } else if (args[0] === 'cerrar') {
 m.reply('grupo cerrado')
 await conn.groupSettingUpdate(m.chat, 'announcement')

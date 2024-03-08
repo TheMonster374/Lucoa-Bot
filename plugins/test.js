e51e8d8224d70b2c6edbd409435337146d68c047
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 08:44:12 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -24,14 +24,17 @@ handler.command = ['a', 'test'];
 export default handler;*/
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
 const sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
-if (!text) return sendPoll(m.chat, `TEST`, ['owner','xd'])
+if (!text) return sendPoll(m.chat, `TEST`, ['abrir','cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
   conn.relayMessage(m.chat, sendPoll, {quoted: m});
 
-    if (command === 'XD') {
- if (!text) return m.reply(m.chat, `TEST`)
-    }
-}
+if (args[0] === 'abrir') {
+m.reply('grupo abierto')
+await conn.groupSettingUpdate(m.chat, 'not_announcement')
+} else if (args[0] === 'cerrar') {
+m.reply('xd')
+await conn.groupSettingUpdate(m.chat, 'announcement')
+}}
     /*if (args[0] === 'owner') {
 m.reply('mi creador es')
 }

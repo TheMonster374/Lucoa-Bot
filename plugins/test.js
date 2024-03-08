e69a47f6c77575f10c4cea9bd39f598a8d454f2d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 11:16:31 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,17 +2,16 @@
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   
 const sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
-if (!text) return sendPoll(m.chat, `TEST`, ['banear on','banear off'])
-  conn.relayMessage(m.chat, sendPoll, {quoted: m});*/
-
-if (args[0] === "on") {
-global.db.data.chats[m.chat].isBanned = true
-m.reply('grupo baneado')
-} else if (args[0] === "off") {  
-global.db.data.chats[m.chat].isBanned = false
-m.reply('grupo desbandeado')}}}
-
-
+if (!text) return m.reply(` abrir*\n*cerrar*`)
+if (!text) return conn.sendPoll(m.chat, `Selecione una de esta opción`, ['grupo abrir','grupo cerrar'])
+//m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
+if (args[0] === 'abrir') {
+m.reply(`a`)
+await conn.groupSettingUpdate(m.chat, 'not_announcement')
+} else if (args[0] === 'cerrar') {
+m.reply(`xd`)
+await conn.groupSettingUpdate(m.chat, 'announcement')
+}}
 handler.command = /^(ban)$/i;
 
 handler.admin = true;

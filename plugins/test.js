f1659855ef5698491aefba9e369c5a1f559603cc
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 11:19:44 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,7 +2,7 @@
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   
 const sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
-if (!text) return conn.sendPoll(m.chat, `${lenguaje.enable.text}\n*$*${prefix + command} on*\n*${prefix + command} off*`, ['banchat on','banchat off'])
+if (!text) return conn.sendPoll(m.chat, `*$*${prefix + command} on*\n*${prefix + command} off*`, ['banchat on','banchat off'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} on*\n*${prefix + command} off*`)
 if (args[0] === 'abrir') {
 m.reply(`a`)

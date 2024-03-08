fd667cd321f072679ab0dda214f48821f2a58b8d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 11:09:00 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -5,8 +5,8 @@ const sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return 
 /*if (!text) return sendPoll(m.chat, `TEST`, ['abrir','cerrar'])
   conn.relayMessage(m.chat, sendPoll, {quoted: m});*/
 if (command == 'banear') {  
-if (!text) return conn.sendPoll(m.chat, `grupo banear off*`, ['banchat on','banchat off'])
-    conn.relayMessage(m.chat, sendPoll, {quoted: m});
+if (!text) return conn.sendPoll(m.chat, `Selecione una de esta opción`, ['banear on','banear off'])    
+conn.relayMessage(m.chat, sendPoll, {quoted: m});
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} on*\n*${prefix + command} off*`)
 if (args[0] === "on") {
 global.db.data.chats[m.chat].isBanned = true

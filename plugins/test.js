d00b0a56ed1ce5c26c2961226a70cfc6e88b2cbc
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 09:52:56 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,4 +1,4 @@
-ww/*const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+/*const handler = async (m, {conn, text, args, usedPrefix, command}) => {
     let name = await conn.getName(m.sender);
   if (name == 'undefined') name = 'Indefinido';
   const b = text.split('|');

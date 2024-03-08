963017ad5198fc1ff0afd0f97eb2bb60562080d2
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 07:14:40 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,9 +1,6 @@
-const handler = async (m, {conn, command, args, text, usedPrefix}) => {
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   
 if (command == 'test') {
-if (!m.isGroup) return m.reply(info.group);  
-if (!isBotAdmins) return m.reply(info.botAdmin);  
-if (!isGroupAdmins) return m.reply(info.admin)
 if (!text) return conn.sendPoll(m.chat, `\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
 if (args[0] === 'abrir') {

6bba07bdc8b43631a4b37ac51be42bb071fbf572
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-07 21:01:56 -0300

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -1,11 +1,24 @@
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-if (!text) return conn.sendPoll(m.chat, `hola\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
-//m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
-if (args[0] === 'abrir') {
-m.reply('a')
-} else if (args[0] === 'cerrar') {
-m.reply('xd')
-}
+  let name = await conn.getName(m.sender);
+  if (name == 'undefined') name = 'Indefinido';
+  const b = text.split('|');
+  if (!b[1]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+  if (b[12]) throw `*[❗𝐈𝐍𝐅𝐎❗] ғᴏʀᴍᴀᴛᴏ ᴅᴇ ᴜsᴏ ${usedPrefix + command} Pregunta? |Opcion1|Opcion2...*`;
+  const caption = `bienvenido a mi menu`.trim();
+  const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
+  const sendPollMessage = {
+    messageContextInfo: {
+        messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="
+    },
+    pollCreationMessage: {
+        name: caption,
+        options: options,
+        selectableOptionsCount: 1,
+    }
   };
-handler.command = ['grupo'];
+conn.relayMessage(m.chat, sendPollMessage, {quoted: m});
+};
+handler.help = ['encuesta question|option|option'];
+handler.tags = ['group'];
+handler.command = ['poll', 'encuesta'];
 export default handler;

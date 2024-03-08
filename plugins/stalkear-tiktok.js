a8bca0e187d3d92fe5ec03a5f8e550400442b005
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 20:22:17 -0300

    Update stalkear-tiktok.js

--- plugins/stalkear-tiktok.js
+++ plugins/stalkear-tiktok.js
@@ -1,16 +1,14 @@
-import fs from ("fs")
-import path from ("path")
-import chalk from ("chalk");
-import axios from ('axios')
-import fetch from ('node-fetch')
-import cheerio from ('cheerio')
-import fg from ('api-dylux') 
-
+import fetch from 'node-fetch';
 
 const handler = async (m, {conn, text, usedPrefix, args, command}) => {
 if (!args[0]) return m.reply(`*Ingrese el nombre del usuario*\n\n*Ejemplo:* ${prefix + command} EmiliaMermes`)
 try {  	
-let res = await fg.ttStalk(args[0])
+    const res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=${lolkeysapi}`);
+    const res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`;
+    const json = await res.json();
+    if (res.status !== 200) throw await res.text();
+    if (!json.status) throw json;
+    const thumb = await (await fetch(json.result.user_picture)).buffer();
 let txt = `┏━━≪ *TIKTOK STALK* ≫━•
 ┃🔖 *Nombre* ${res.name}
 ┃🔖 nombre de usuario ${res.username}

515bd0334329761a9946b2bd4c120f2d7bca24c9
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 13:47:28 -0300

    🐶

---
+++ plugins/chat-stickers.js
@@ -0,0 +1,16 @@
+import {sticker} from '../lib/sticker.js';
+const handler = (m) => m;
+handler.all = async function(m, {conn}) {
+  const chat = global.db.data.chats[m.chat];
+
+  if (!chat.isBanned && m.text.match(/(:v)/gi)) {
+    if (!db.data.chats[m.chat].audios) return;
+    const vn = './storage/img/test.webp';
+    mconn.conn.sendPresenceUpdate('recording', m.chat);
+    mconn.conn.sendMessage(m.chat, {sticker: {url: vn}, fileName: 'sticker.webp', mimetype: 'image/webp', ptt: true}, {quoted: m});
+  }
+  
+
+  return !0;
+};
+export default handler;

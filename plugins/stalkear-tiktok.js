4eb0b8413e012a127a087e0af5d0562bf2814103
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 18:56:07 -0300

    Create stalkear-tiktok.js

---
+++ plugins/stalkear-tiktok.js
@@ -0,0 +1,20 @@
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!args[0]) return m.reply(`*Ingrese el nombre del usuario*\n\n*Ejemplo:* ${prefix + command} EmiliaMermes`)
+try {  	
+let res = await fg.ttStalk(args[0])
+let txt = `┏━━≪ *TIKTOK STALK* ≫━•
+┃🔖 *Nombre* ${res.name}
+┃🔖 nombre de usuario ${res.username}
+┃👥 seguidores ${res.followers}
+┃🫂 siguiendo ${res.following}
+┃📌 biografia ${res.desc}
+┃━━━━━━━━━━━━━━
+┃ *🔗 Link* : https://tiktok.com/${res.username}
+┗━━━━━━━━━━━━━•`
+await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
+} catch (e) {
+m.reply(info.error)
+console.log(e)}}
+
+handler.command = /^(tiktokstalk)$/i;
+export default handler;

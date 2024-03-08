2383a8feee8808e0c5363f33d81725862082daf9
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 07:38:13 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -3,9 +3,13 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   let name = await conn.getName(m.sender);
   if (name == 'undefined') name = 'Indefinido';  
   
-conn.sendPoll = (jid, name = '', values = [], selectableOptionsCount: 1,) => { return conn.sendMessage(jid, { poll: { name, values, selectableCount }}) }
+  const sendPollMessage = {
+    messageContextInfo: {
+        messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="
+    },
+    pollCreationMessage: { name: 'axd' ['grupo abrir','grupo cerrar']: 1,} };
   
-if (command == 'A') {
+/*if (command == 'A') {
 if (!text) return conn.sendPoll(m.chat, `\n*${prefix + command} abrir*\n*${prefix + command} cerrar*\nSelecione una de esta opción`, ['grupo abrir','grupo cerrar'])
 //m.reply(`${lenguaje.enable.text}\n*${prefix + command} abrir*\n*${prefix + command} cerrar*`)
   conn.relayMessage(m.chat, sendPoll, {quoted: m});
@@ -17,7 +21,7 @@ m.reply('grupo cerrado')
 await conn.groupSettingUpdate(m.chat, 'announcement')
 }}
 }
-
+*/
 handler.command = /^(A)$/i;
 
 export default handler;

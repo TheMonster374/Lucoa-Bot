fcbd3181f8a95e3793ede6f4b860e5c85379f680
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-09 08:12:13 -0300

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,91 +1,70 @@
-import fg from 'api-dylux';
 import fetch from 'node-fetch';
-import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
-import fbDownloader from 'fb-downloader-scrapper';
-import {facebook} from '@xct007/frieren-scraper'; 
 import axios from 'axios';
+import instagramGetUrl from 'instagram-url-direct';
+import {instagram} from '@xct007/frieren-scraper';
+import {instagramdl} from '@bochilteam/scraper';
+import instagramDl from '@sasmeee/igdl';
+import {fileTypeFromBuffer} from 'file-type';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `*ingresa un enlace de facebook*\n\nejemplo ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
-  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `ingresa un enlace de facebook \n\nejemplo ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  if (!args[0]) throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*[ ℹ️ ] Ingrese un enlace de Instagram.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link_`;
+  m.reply(global.wait);
   try {
-    await m.reply(`⌛ Descargando video...`);
-    const d2ata = await facebook.v1(args[0]);
-    let r2es = '';
-    if (d2ata.urls && d2ata.urls.length > 0) {
-      r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
+const img = await instagramDl(args[0]);
+for (let i = 0; i < img.length; i++) {
+    const bufferInfo = await getBuffer(img[i].download_link);
+        if (bufferInfo.detectedType.mime.startsWith('image/')) {
+            await conn.sendMessage(m.chat, {image: {url: img[i].download_link}}, {quoted: m});
+        } else if (bufferInfo.detectedType.mime.startsWith('video/')) {
+            await conn.sendMessage(m.chat, {video: {url: img[i].download_link }}, {quoted: m});
+        }
+}
+  } catch {   
+  try {
+    const datTa = await instagram.download(args[0]);
+    for (const urRRl of datTa) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+      const tXXxt = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrRRl}_`.trim();
+      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
     }
-    conn.sendFile(m.chat, r2es, 'error.mp4', `✅ Aqui tienes tu video`, m);
-  } catch (err1) {
-    try {
-      const req = await igeh(args[0]);
-      conn.sendMessage(m.chat, {video: {url: req.url_list}}, m);
-    } catch (err1_2) {
+  } catch {
       try {
-        const Rres = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=${lolkeysapi}&url=${args[0]}`);
-        const Jjson = await Rres.json();
-        let VIDEO = Jjson.result[0];
-        if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
-        conn.sendFile(m.chat, VIDEO, 'error.mp4', `✅ Aqui tienes video`, m);
-      } catch (err2) {
+        const resultss = await instagramGetUrl(args[0]).url_list[0];
+        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+        const txt2 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl2}_`.trim();
+        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
+      } catch {
         try {
-          const ress = await fg.fbdl(args[0]);
-          const urll = await ress.data[0].url;
-          await conn.sendFile(m.chat, urll, 'error.mp4', '✅ Aqui tienes tu video', m);
-        } catch (err3) {
+          const resultssss = await instagramdl(args[0]);
+          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+          const txt4 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl3}_`.trim();
+          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
+        } catch {
           try {
-            const res = await fbDownloader(args[0]);
-            for (const result of res.download) {
-              const ur = result.url;
-              await conn.sendFile(m.chat, ur, 'error.mp4', '✅ Aqui tienes tu video', m);
-            }
-          } catch (err4) {
-            try {
-              const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
-              const json = await res3.json();
-              const url3 = await json.video;
-              await conn.sendFile(m.chat, url3, 'error.mp4', '✅ Aqui tienes tu video', m);
-            } catch (err5) {
-              try {
-                const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '✅ Aqui tienes tu video', m);
-              } catch (err6) {
-                throw `[❌] ha ocurrido un error inesperado`;
-              }
-            }
+            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
+            const json = await human.json();
+            const videoig = json.result;
+            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+            const txt1 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl1}_`.trim();
+            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
+          } catch {
+            throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
           }
         }
       }
     }
   }
 };
-handler.help = ['facebook']
-handler.tags = ['downloader']
-handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
 export default handler;
 
-async function igeh(url_media) {
-  return new Promise(async (resolve, reject)=>{
-    const BASE_URL = 'https://instasupersave.com/';
-    try {
-      const resp = await axios(BASE_URL);
-      const cookie = resp.headers['set-cookie']; // get cookie from request
-      const session = cookie[0].split(';')[0].replace('XSRF-TOKEN=', '').replace('%3D', '');
-      const config = {method: 'post', url: `${BASE_URL}api/convert`, headers: {'origin': 'https://instasupersave.com', 'referer': 'https://instasupersave.com/pt/', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 'x-xsrf-token': session, 'Content-Type': 'application/json', 'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`}, data: {url: url_media}};
-      axios(config).then(function(response) {
-        const ig = [];
-        if (Array.isArray(response.data)) {
-          response.data.forEach((post) => {
-            ig.push(post.sd === undefined ? post.thumb : post.sd.url);
-          });
-        } else {
-          ig.push(response.data.url[0].url);
-        }
-        resolve({results_number: ig.length, url_list: ig});
-      }).catch(function(error) {
-        reject(error.message);
-      });
-    } catch (e) {
-      reject(e.message);
+const getBuffer = async (url, options) => {
+    options = options || {};
+    const res = await axios({method: 'get', url, headers: {'DNT': 1, 'Upgrade-Insecure-Request': 1}, ...options, responseType: 'arraybuffer'});
+    const buffer = Buffer.from(res.data, 'binary');
+    const detectedType = await fileTypeFromBuffer(buffer);
+    if (!detectedType || (detectedType.mime !== 'image/jpeg' && detectedType.mime !== 'image/png' && detectedType.mime !== 'video/mp4')) {
+        return null;
     }
-  });
-}
+    return { buffer, detectedType };
+};

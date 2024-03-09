85031083c2f1c3322db4784ce5eb8cd4272a44a4
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 21:10:44 -0300

    🐶

--- plugins/IA-bingimg.js
+++ plugins/IA-bingimg.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) throw '[📚] 𝑖𝑛𝑔𝑟𝑒𝑠𝑎 𝑢𝑛 𝑡𝑒𝑥𝑡𝑜 𝑑𝑒 𝑙𝑎 𝑖𝑚𝑎𝑔𝑒𝑛 𝑞𝑢𝑒 𝑞𝑢𝑖𝑒𝑟𝑒𝑠\n\n*[💡] 𝐸𝑗𝑒𝑚𝑝𝑙𝑜:* \n.bingimg mono en el espacio'
+    if (!text) throw '[📚] ingresa un texto de la imagen que quieres\n\n*[💡] ejemplo:* \n.bingimg mono en el espacio'
     let msg = encodeURIComponent(text)
     let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
     let data = await res.json()

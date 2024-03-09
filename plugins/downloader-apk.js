12212463ead15cf40785d6a0edaa50c49031acfe
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-09 11:15:46 -0300

    🐕

--- plugins/downloader-apk.js
+++ plugins/downloader-apk.js
@@ -1,11 +1,11 @@
 import { search, download } from 'aptoide-scraper'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*ingresa el nombre de la app que quieres buscar*\n\nejemplo: ${usedPrefix + command} Pokemon Go`
+if (!text) throw `*_ingresa el nombre de la app que quieres buscar_*\n\n*ejemplo:* ${usedPrefix + command} Pokemon Go`
 try {
 const searchResult = await search(text)
 const data = await download(searchResult[0].id)
 if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 300) {
-return await conn.sendMessage(m.chat, { text: 'El archivo es demasiado pesado y no se enviara...' }, { quoted: m })
+return await conn.sendMessage(m.chat, { text: '🗂 El archivo es demasiado pesado y no se enviara...' }, { quoted: m })
 }
 await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null }, { quoted: m })
 } catch {

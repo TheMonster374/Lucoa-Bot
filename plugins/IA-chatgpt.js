325ce25c5142393ed9a10af3295a586bf6babd5d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-08 21:30:33 -0300

    🐶

--- plugins/IA-chatgpt.js
+++ plugins/IA-chatgpt.js
@@ -9,7 +9,7 @@ const openaiii = new OpenAIApi(configuration)
 var handler = async (m, {conn, text, usedPrefix, command}) => {
   
 if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `[📚] ingresa un texto\n\n*[💡] ejemplo:* !${command} Codigo en JS para un juego de cartas`, m, ) 
+if (!text) return conn.reply(m.chat, `*ingresa un texto*\n\nejemplo: !${command} Codigo en JS para un juego de cartas`, m, ) 
   
 try {
 

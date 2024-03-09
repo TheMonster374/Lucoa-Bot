c0fb5edecacfd5a3e6d4adfc12ee1278425b657d
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-09 09:24:03 -0300

    🐕

--- plugins/IA-Bard-IMG.js
+++ plugins/IA-Bard-IMG.js
@@ -15,7 +15,7 @@ let json = await (await fetch(`https://aemt.me/bardimg?url=${media}&text=${text}
 
 conn.sendMessage(m.chat, { text: json.result }, { quoted: m })
 
-} else return conn.reply(m.chat, `*manda una imagen con el comando y el texto a buscar*\n\nejemplo: !bardimg dame informacion de lo que sale en la imagen `, m, fake)
+} else return conn.reply(m.chat, `*_manda una imagen con el comando y el texto a buscar_*\n\n*ejemplo:* !bardimg dame informacion de lo que sale en la imagen `, m, fake)
 
 }
 handler.help = ['bardimg']

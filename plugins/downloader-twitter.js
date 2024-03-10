adf10bd6ef496ef7926c36045adfddd92bd57b75
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 09:51:35 -0300

    🐕

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -4,10 +4,10 @@ import cheerio from 'cheerio'
 import vm from 'node:vm'
 import qs from 'qs'
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
+//const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
+//}
 const twitterUrlRegex = /^https?:\/\/(www\.)?twitter\.com\/(\w+)\/status\/(\d+)$/i
-if (!text) return conn.reply(m.chat,`*_ingresa un enlace de twitter_*\n\n*ejemplo:* ${usedPrefix + command}  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, fkontak)
+if (!text) return conn.reply(m.chat,`*_ingresa un enlace de twitter_*\n\n*ejemplo:* ${usedPrefix + command}  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, m)
 try{ 
 const apiUrl = `https://api.lolhuman.xyz/api/twitter?apikey=GataDios&url=${encodeURIComponent(text)}`
 const response = await fetch(apiUrl)
@@ -18,7 +18,7 @@ const tweetVideoUrl = tweetData.media[0].url
 const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${text}`)).text()
 const tweetTitleWithoutUrl = tweetTitle.replace(/https?:\/\/t\.co\/\w+/i, '').trim()
 const txt1 = `*Titulo:* ${tweetTitleWithoutUrl}\n\n🔗 *link acortado:*\n• ${shortUrl1}`.trim()
-await conn.sendFile(m.chat, tweetVideoUrl, 'error.mp4', txt1, fkontak)
+await conn.sendFile(m.chat, tweetVideoUrl, 'error.mp4', txt1, m)
 } catch (e) {
 console.log(e)
 }}

3538e96ac51891db125d8179ead13607a20e4ec1
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 11:26:10 -0300

    Update and rename test2.js to sticker-waifu.js

--- plugins/sticker-waifu.js
+++ plugins/sticker-waifu.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     } else { 
       who = m.chat;
     }
-      const textquien = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙾 𝙼𝙴𝙽𝙲𝙸𝙾𝙽𝙰 𝙰 𝙰𝙻𝙶𝚄𝙸𝙴𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n◉ ${usedPrefix + command} @${global.suittag}`;
+/*      const textquien = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙾 𝙼𝙴𝙽𝙲𝙸𝙾𝙽𝙰 𝙰 𝙰𝙻𝙶𝚄𝙸𝙴𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n◉ ${usedPrefix + command} @${global.suittag}`;
     if (who === m.chat && m.isGroup || !who && m.isGroup) return m.reply(textquien, m.chat, {mentions: conn.parseMention(textquien)});
   try {
     let name;
@@ -16,11 +16,11 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     } else {
       name = conn.getName(who);
     }
-    let name2 = conn.getName(m.sender);
+    let name2 = conn.getName(m.sender);*/
     let apislap = await fetch(`https://api.waifu.pics/nsfw/waifu`);
     let jkis = await apislap.json();
     let { url } = jkis;
-    let stiker = await sticker(null, url, `${name2} le dio una bofetada a ${name}`, null);
+    let stiker = await sticker(null, url, ``, null);
     conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {forwardingScore: 200, isForwarded: true}}, {quoted: m});
   } catch {
     throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*`;

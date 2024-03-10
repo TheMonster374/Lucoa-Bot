192e90ac6b79061e91b5c66581f03e938d8ae450
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 12:42:04 -0300

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const handler = async (m, { conn, args, usedPrefix, command }) => {
 	let code = (args[0] || '').replace(/\D/g, '')
 	if (!code) throw 'Input code' 
-	await msg.reply('_🚩 In progress, please wait..._')
+	await m.reply('_🚩 In progress, please wait..._')
 	let res = await fetch('https://mxmxk.herokuapp.com/nhentai?code=' + code)
 	if (!res.ok) throw await res.statusText
 	let json = await res.json()
@@ -11,7 +11,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
 	let json2 = await v.json()
 	let buffer = await (await fetch(json.result.images.pages[0])).buffer()
 	let jpegThumbnail = await extractImageThumb(buffer)
-	return await client.sendMessage(msg.from, { document: { url: json2.result }, jpegThumbnail, fileName: json.result.title.english + '.pdf', mimetype: 'application/pdf' }, { quoted: msg })
+	return await client.sendMessage(m.from, { document: { url: json2.result }, jpegThumbnail, fileName: json.result.title.english + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
 }
 handler.help = ['nhentaipdf']
 handler.tags = ['nsfw']

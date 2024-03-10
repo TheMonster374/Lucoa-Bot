1ddd3326bcdaa579cba292a3170ad56591a43694
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 09:52:29 -0300

    🐕

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -4,8 +4,7 @@ import cheerio from 'cheerio'
 import vm from 'node:vm'
 import qs from 'qs'
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-//const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-//}
+
 const twitterUrlRegex = /^https?:\/\/(www\.)?twitter\.com\/(\w+)\/status\/(\d+)$/i
 if (!text) return conn.reply(m.chat,`*_ingresa un enlace de twitter_*\n\n*ejemplo:* ${usedPrefix + command}  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, m)
 try{ 

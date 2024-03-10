a1609863ff66945a50d91614e9ab04762e3e85a1
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 10:47:11 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -6,17 +6,18 @@ let handler = async (m, {conn, command}) => {
       who = m.chat;
     }
    if (who === m.chat && m.isGroup || !who && m.isGroup) return m.reply(textquien, m.chat, {mentions: conn.parseMention(textquien)});
-  try {{
+  try {
     let name;
     if (who === m.chat) {
       name = "𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝";
     } else {
       name = conn.getName(who);
     }
-  
+  }
+      
   let url = pack[Math.floor(Math.random() * pack.length)];
     await conn.sendFile( m.chat, url, "gimage.mp4", `${name2} le dio una bofetada a ${name}`.trim(), m)
-}}};
+};
 handler.help = ["test"];
 //handler.tags = ["img"];
 handler.command = /^(test)$/i;

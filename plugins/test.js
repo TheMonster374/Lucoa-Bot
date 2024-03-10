a6787cca8ec07c97e69f60bd8990f9fe1c01155d
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-09 23:33:45 -0300

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,4 +1,6 @@
-global.wait = ('Cargando', adReply)
+global.wait =
+  const msg = { 'Cargando',  
+}
   const aa = {quoted: m, userJid: conn.user.jid};
   const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑩𝒐𝒕 | 𝑴𝑰𝑵𝑰', body: 'Whatsapp bot Multi device by als', thumbnail: catalogo, sourceUrl: 'https://atom.bio/als'}}}}, aa);
   if (msg) return conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id});

be9c5274f5b5eada2b3c9f2de055fe65426dd68f
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-09 22:30:20 -0300

    Update downloader-apk.js

--- plugins/downloader-apk.js
+++ plugins/downloader-apk.js
@@ -1,16 +1,18 @@
-import { search, download } from 'aptoide-scraper'
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*_ingresa el nombre de la app que quieres buscar_*\n\n*ejemplo:* ${usedPrefix + command} Pokemon Go`
-try {
-const searchResult = await search(text)
-const data = await download(searchResult[0].id)
-if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 300) {
-return await conn.sendMessage(m.chat, { text: '🗂 El archivo es demasiado pesado y no se enviara...' }, { quoted: m })
-}
-await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null }, { quoted: m })
-} catch {
-}}
-handler.help = ['apk'];
-handler.tags = ['downloader'];
-handler.command = /^(apkmod|modapk|dapk2|aptoide|apk)$/i
-export default handler
+import {search, download} from 'aptoide-scraper';
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
+ if (!text) throw `*[❗] Ingrese el nombre de la APK que quiera buscar.*`;
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `📲 *Descargador de Aptoide* 📲\n\n📌 *Nombre:* ${data5.name}\n📦 *Package:* ${data5.package}\n🕒 *Última actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] El archivo es demasiado pesado por lo que no se enviará.*'}, {quoted: m});
+    }
+    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+  } catch {
+    throw `*[❗] Error, no se encontrarón resultados para su búsqueda.*`;
+  }    
+};
+handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+export default handler;

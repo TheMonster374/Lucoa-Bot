0371134f479d4f2c4acbf82a651c0d502c2b5d66
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 12:43:15 -0300

    🐕

--- plugins/stickers-nsfw.js
+++ plugins/stickers-nsfw.js
@@ -40,18 +40,20 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   };
 };   
 
-    if (command == 'shentai') {
+    if (command == 'strap') {
     try {
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://nekobot.xyz/api/sticker?type=hentai`);
+    let apislap = await fetch(`https://api.waifu.pics/nsfw/blowjob`);
     let jkis = await apislap.json();
     let { url } = jkis;
-    let stiker = await sticker(null, url, `Hentai`, `Jotchua-Mini`);
+    let stiker = await sticker(null, url, `Trap`, `Jotchua-Mini`);
     conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {forwardingScore: 200, isForwarded: true}}, {quoted: m});
   } catch {
     throw `*ocurrio un error inesperado*`;
   };
 };      
-};    
-handler.command = ['sneko', 'swaifu', 'sblowjob', 'shentai'];
+};  
+handler.help = ['sneko', 'swaifu', 'sblowjob', 'strap'];
+handler.tags = ['sticker']
+handler.command = ['sneko', 'swaifu', 'sblowjob', 'strap'];
 export default handler;

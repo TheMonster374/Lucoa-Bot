8e73a20136db568babc849ee3b8bcdc902728a58
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 21:20:48 -0300

    Create xdd.js

---
+++ plugins/xdd.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+
+
+  if (!args[0]) throw `test`;
+
+  let res = await fetch(`https://api.cafirexos.com/api/nsfw/nsfwass`);
+  if (!res.ok) throw await res.text();
+
+  let json = await res.json();
+  if (!json.url) throw 'Error!';
+
+  conn.sendFile(m.chat, json.url, '', global.wm, m);
+};
+
+handler.command = /^(xdd)$/i
+handler.tags = ['nsfw']
+handler.help = ['xdd']
+
+export default handler

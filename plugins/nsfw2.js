f5720c11b209d593b640edf4187dd5b04a2cf8f6
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 22:33:56 -0300

    🐕

--- plugins/nsfw2.js
+++ plugins/nsfw2.js
@@ -1,42 +1,40 @@
 import axios from 'axios';
 import fetch from 'node-fetch';
 const handler = async (m, {command, conn}) => {
-  if (!db.data.chats[m.chat].nsfw && m.isGroup) throw '*𝑬𝒍 𝒏𝒔𝒇𝒘 𝒆𝒔𝒕𝒂 𝒅𝒆𝒔𝒉𝒂𝒃𝒊𝒍𝒊𝒕𝒂𝒅𝒐, 𝒖𝒔𝒆 #𝒆𝒏𝒂𝒃𝒍𝒆 𝒏𝒔𝒇𝒘 𝒑𝒂𝒓𝒂 𝒂𝒄𝒕𝒊𝒗𝒂𝒓𝒍𝒐*';
-
   if (command == 'ahegao') {
     const res = (await axios.get(`https://raw.githubusercontent.com/KazukoGans/database/main/nsfw/ahegao.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'tentaculos') {
     const res = (await axios.get(`https://raw.githubusercontent.com/KazukoGans/database/main/nsfw/tentacles.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'manga') {
     const res = (await axios.get(`https://raw.githubusercontent.com/KazukoGans/database/main/nsfw/manga.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
     if (command == 'nsfwloli') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwloli.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
     if (command == 'nsfwcum') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwcum.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
     if (command == 'nsfwbdsm') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwbdsm.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 };
 handler.help = ['ahegao', 'manga', 'tentaculos'];

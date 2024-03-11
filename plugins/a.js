d7012e625e1515ef7134e2098887789a95034e91
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 00:35:33 -0300

    Update a.js

--- plugins/a.js
+++ plugins/a.js
@@ -1,6 +1,45 @@
+
+import { MessageType, MessageOptions, Mimetype } from '@whiskeysockets/baileys'
+
 let handler = async (m, {conn, command}) => {
-// send a link
-const m.reply  = await m.reply( { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys' })
+
+const id = 'abcd@s.whatsapp.net' // the WhatsApp ID 
+// send a simple text!
+const sentMsg  = await sock.sendMessage(id, { text: 'oh hello there' })
+// send a reply messagge
+const sentMsg  = await sock.sendMessage(id, { text: 'oh hello there' }, { quoted: message })
+// send a mentions message
+const sentMsg  = await sock.sendMessage(id, { text: '@12345678901', mentions: ['12345678901@s.whatsapp.net'] })
+// send a location!
+const sentMsg  = await sock.sendMessage(
+    id, 
+    { location: { degreesLatitude: 24.121231, degreesLongitude: 55.1121221 } }
+)
+// send a contact!
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Jeff Singh\n' // full name
+            + 'ORG:Ashoka Uni;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=911234567890:+91 12345 67890\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+const sentMsg  = await sock.sendMessage(
+    id,
+    { 
+        contacts: { 
+            displayName: 'Jeff', 
+            contacts: [{ vcard }] 
+        }
+    }
+)
+
+const reactionMessage = {
+    react: {
+        text: "💖", // use an empty string to remove the reaction
+        key: message.key
+    }
+}
+
+const sendMsg = await sock.sendMessage(id, reactionMessage)
 };
   handler.command = /^(XD)$/i;
 export default handler;

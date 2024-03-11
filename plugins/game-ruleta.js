07486fd172134506f02bee0d3b65ea9a7e7a3488
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 01:42:56 -0300

    🐕

---
+++ plugins/game-ruleta.js
@@ -0,0 +1,30 @@
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+
+    const ruletaresultado = "https://telegra.ph/file/561da43337232c7d6389a.mp4"
+
+    let amount = parseInt(args[0])
+    let color = args[1]?.toLowerCase()
+    if (args.length < 2 || !color) throw `*_ingresa la cantidad que quieres apostar y el color rojo o negro._*`
+    let colores = ['rojo', 'negro']
+    let colour = colores[Math.floor(Math.random() * colores.length)];
+    let user = global.db.data.users[m.sender]
+    if (isNaN(amount) || amount < 10) throw `*_Lo minimo para apostar son 10 Pesos 🪙_*.`
+    if (!colores.includes(color)) throw '*_Debes especificar un color válido: rojo o negro_*'
+    if (user.exp < amount) throw '*_¡No tienes suficiente pesos!_*'
+    if (amount > 100000) throw `*_No puedes apostar mas de 100000 Pesos 🪙_.*`
+    let result = ''
+    if (colour == color) {
+        result = `${colour == 'rojo' ? '*La pelota paro en el color:* 🔴' : '*La pelota paro en el color:* ⚫'} \n\n *+ ${amount * 2} Pesos 🪙*`
+        user.exp += amount * 2
+    } else {
+        result = `${colour == 'rojo' ? '*La pelota paro en el color:* 🔴' : 'La pelota paro en el color:* ⚫'} \n\n *- ${amount} Pesos 🪙*`
+        user.exp -= amount
+    }
+    conn.sendMessage(m.chat, { video: { url: ruletaresultado }, caption: result }, { quoted: m })
+}
+handler.help = ['ruleta apuesta/color']
+handler.tags = ['game']
+handler.command = ['ruleta', 'rt']
+
+
+export default handler

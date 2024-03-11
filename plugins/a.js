f645b422ce3de6c9d6e2cef99e11c3ebd0595639
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 00:33:49 -0300

    Update a.js

--- plugins/a.js
+++ plugins/a.js
@@ -1,6 +1,6 @@
 let handler = async (m, {conn, command}) => {
 // send a link
-const sentMsg  = await sock.sendMessage(id, { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys' })
+const sentMsg  = await m.reply(id, { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys' })
 };
   handler.command = /^(XD)$/i;
 export default handler;

a6a187160cfc6ebb8aa3aec7b208f70124219c19
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 22:11:18 -0300

    🐕

--- plugins/nsfw2.js
+++ plugins/nsfw2.js
@@ -20,8 +20,26 @@ const handler = async (m, {command, conn}) => {
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
+
+    if (command == 'nsfwloli') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwloli.json`)).data;
+    const haha = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
+
+    if (command == 'nsfwcum') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwcum.json`)).data;
+    const haha = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
+
+    if (command == 'nsfwbdsm') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwbdsm.json`)).data;
+    const haha = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
 };
 handler.help = ['ahegao', 'manga', 'tentaculos'];
-handler.command = ['ahegao', 'manga', 'tentaculos'];
+handler.command = ['ahegao', 'manga', 'tentaculos', 'nsfwloli', 'nsfwcum', 'nsfwbdsm'];
 handler.tags = ['nsfw'];
 export default handler;

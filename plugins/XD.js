c6a7aebfebd4dac856cd4d622b8c8d3c066d34e2
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 01:27:45 -0300

    Update XD.js

--- plugins/XD.js
+++ plugins/XD.js
@@ -6,11 +6,12 @@ var jugadores = ["Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4", "Jugador 5"
 // Función para simular la ruleta rusa
 function ruletaRusa(jugadores) {
   var disparo = Math.floor(Math.random() * jugadores.length); // Generar un número aleatorio
+  conn.sendFile(m.chat, disparo, 'error.jpg', ``, m);
   console.log("El jugador " + jugadores[disparo] + " ha sido eliminado."); // Mostrar el jugador eliminado
   jugadores.splice(disparo, 1); // Eliminar al jugador del array
 }
 
 // Llamar a la función para simular la ruleta rusa
 ruletaRusa(jugadores);
-
+}
   handler.command = ['xd']

45214d478ce358decfcc9e411ce91ada88053490
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 01:48:29 -0300

    🐕

--- plugins/img-waifu.js
+++ plugins/img-waifu.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
    if (!res.ok) return 
    let json = await res.json()
    if (!json.url) return 
-   await conn.sendFile(m.chat, json.url, 'waifu.png', '*WAIFU*', m)
+   await conn.sendFile(m.chat, json.url, 'waifu.png', '*Waifu*', m)
 }
 
 handler.help = ['waifu']

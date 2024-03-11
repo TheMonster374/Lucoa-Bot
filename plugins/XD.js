1552f935596b39cbd959ba2fb0647e28d2103d9d
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 01:24:22 -0300

    Create XD.js

---
+++ plugins/XD.js
@@ -0,0 +1,16 @@
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+// Definir un array con los jugadores
+var jugadores = ["Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4", "Jugador 5"];
+
+// Función para simular la ruleta rusa
+function ruletaRusa(jugadores) {
+  var disparo = Math.floor(Math.random() * jugadores.length); // Generar un número aleatorio
+  console.log("El jugador " + jugadores[disparo] + " ha sido eliminado."); // Mostrar el jugador eliminado
+  jugadores.splice(disparo, 1); // Eliminar al jugador del array
+}
+
+// Llamar a la función para simular la ruleta rusa
+ruletaRusa(jugadores);
+
+  handler.command = ['xd']

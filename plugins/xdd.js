47db37bd2f9e65cbd8b2aabc75fedfc143398c75
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-10 21:30:05 -0300

    Update xdd.js

--- plugins/xdd.js
+++ plugins/xdd.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const handler = async (m, { conn, args, usedPrefix, command }) => {
     try {
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://api.cafirexos.com/api/nsfw?text=${text}`);
+    let apislap = await fetch(`https://api.cafirexos.com/api/nsfw/?text=${text}`);
     let jkis = await apislap.json();
     let { url } = jkis;
     let stiker = await sticker(null, url, `Waifu`, `Jotchua-Mini`);
@@ -12,7 +12,6 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     throw `*ocurrio un error inesperado*`;
   };
 };
- 
-};  
+  
 handler.command = ['nsfwass'];
 export default handler;

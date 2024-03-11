8e3340bf6766702ed0e4a2a4e435ca2547074c18
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-11 00:46:07 -0300

    Update tool-tomp3.js

--- plugins/tool-tomp3.js
+++ plugins/tool-tomp3.js
@@ -1,21 +1,17 @@
 import { toAudio } from '../lib/converter.js'
 
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-let q = m.quoted ? m.quoted : m
-let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `*⚠️ RESPONDA A UN VIDEO O NOTA DE VOS QUE DESEE CONVERTIR A AUDIO/MP3*`
-let media = await q.download?.()
-if (!media && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!media && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-let audio = await toAudio(media, 'mp4')
-if (!audio.data && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!audio.data && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/mp4' })
-
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
+    let media = await q.download?.()
+    if (!media) throw '❎ Failed to download media'
+    let audio = await toAudio(media, 'mp4')
+    if (!audio.data) throw '❎ Error converting'
+    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
-handler.tags = ['transformador']
+handler.tags = ['fun']
 handler.command = /^to(mp3|a(udio)?)$/i
 
 export default handler

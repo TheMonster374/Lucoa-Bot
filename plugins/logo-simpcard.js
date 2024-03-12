204cd367e102896947cce862327fd6b9ed777081
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-12 09:19:02 -0300

    🐕

--- plugins/logo-simpcard.js
+++ plugins/logo-simpcard.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/simpcard', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', 'Miren un Simp', m);
+  }), 'error.png', '*_Simpcard_*', m);
 };
 handler.help = ['simpcard'];
 handler.tags = ['efectos'];

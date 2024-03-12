fe10fc9ddab44cb3d7752ce696e21202e07d2ecc
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-12 09:53:24 -0300

    🐕

--- plugins/main-dash.js
+++ plugins/main-dash.js
@@ -20,7 +20,7 @@ if (command == 'database' || command == 'usuarios' || command == 'user') {
 let totalreg = Object.keys(global.db.data.users).length
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
 
-conn.reply(m.chat, `
+conn.reply(m.chat, ` 
 
 🗂️ *Tengo ${rtotalreg} usuarios registrados*
 

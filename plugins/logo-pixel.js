3cb67275761fb0c9f6b66abf554ca8703a9b0563
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-12 09:17:55 -0300

    🐕

--- plugins/logo-pixel.js
+++ plugins/logo-pixel.js
@@ -4,7 +4,7 @@ conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/pixelate
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 comment: text,
 username: conn.getName(m.sender)
-}), 'error.png', 'Imagen pixeleada', m)
+}), 'error.png', '*_Imagen pixeleada_*', m)
 }
 handler.help = ['pixel']
 handler.tags = ['efectos']

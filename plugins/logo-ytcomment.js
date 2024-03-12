03d194edff8ce4bc9252426ead1c7b6ec22646f4
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-12 09:21:14 -0300

    🐕

--- plugins/logo-ytcomment.js
+++ plugins/logo-ytcomment.js
@@ -1,10 +1,10 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '[📚] 𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒖𝒏 𝒕𝒆𝒙𝒕𝒐';
+  if (!text) throw '*_ingresa un texto_*';
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender),
-  }), 'error.png', '𝑨𝒒𝒖𝒊 𝒆𝒔𝒕𝒂  𝒕𝒖 𝒄𝒐𝒎𝒆𝒏𝒕𝒂𝒓𝒊𝒐 [💬]', m);
+  }), 'error.png', '`aqui tienes tu comentario`', m);
 };
 handler.help = ['ytcomment <texto>'];
 handler.tags = ['efectos'];

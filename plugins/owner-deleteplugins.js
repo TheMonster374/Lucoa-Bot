cf94c4f153f556002f6ec2f2a3c6457c90029a2c
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-13 10:10:43 -0300

    🐕

--- plugins/owner-deleteplugins.js
+++ plugins/owner-deleteplugins.js
@@ -12,8 +12,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
 
 let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `📌 *_𝑬𝒋𝒆𝒎𝒑𝒍𝒐 𝒅𝒆 𝒖𝒔𝒐:_*\n*#deleteplugin Menu▪oficial*`
-    if (!ar1.includes(args[0])) return m.reply(`*[ ❌ ] 𝑵𝑶 𝑬𝑵𝑪𝑶𝑵𝑻𝑹𝑨𝑫𝑶!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!text) throw `📌 *_ejemplo de uso:_*\n*#deleteplugin Menu▪oficial*`
+    if (!ar1.includes(args[0])) return m.reply(`*[ ❌ ] No encontrado!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
 const file = join(__dirname, '../plugins/' + args[0] + '.js')
 unlinkSync(file)
 conn.reply(m.chat, `*_El plugin "plugins/${args[0]}.js" se ha eliminado con éxito._* [ ✅ ]`, m)

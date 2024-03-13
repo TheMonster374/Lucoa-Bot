d8cf2fb80089a758392f67b5d112d381863f4d5c
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-13 09:16:17 -0300

    Update search-imagen.js

--- plugins/search-imagen.js
+++ plugins/search-imagen.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   const res = await googleImage(text);
   const image = await res.getRandom();
   const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `*resultado de* ${text}`, m);
+  conn.sendFile(m.chat, link, 'error.jpg', `*resultado de* ${text}`, m, fake);
 };
 handler.help = ['imagen'];
 handler.tags = ['serch', 'tools'];

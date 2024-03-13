7893bb1a644869a1eb31a273f437229eea20fa2a
ALS-2 <157686445+ALS-2@users.noreply.github.com>
2024-03-13 09:32:54 -0300

    🐕

--- plugins/owner-addowner.js
+++ plugins/owner-addowner.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `etiqueta la persona que quieres que sea owner`
-    if (global.owner.includes(who.split`@`[0])) throw 'Ya eres mi owner'
+    if (!who) throw `*_etiqueta la persona que quieres que sea owner_*`
+    if (global.owner.includes(who.split`@`[0])) throw '_*Ya eres mi owner*_'
     global.owner.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} ahora eres mi owner`, m, {
+    conn.reply(m.chat, `*@${who.split`@`[0]} ahora eres mi owner*`, m, {
         contextInfo: {
             mentionedJid: [who]
         }

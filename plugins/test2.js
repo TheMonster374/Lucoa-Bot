244f90b4ab3d3b72522167c3fc942a8ce9ef6299
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 11:20:32 -0300

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const handler = async (m, { conn, args, usedPrefix, command }) => {
     try {
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://nekos.pro/api/boobs`);
+    let apislap = await fetch(`https://api.waifu.im/maid`);
     let jkis = await apislap.json();
     let { url } = jkis;
     conn.sendFile(m.chat,  , {quoted: m});

e6b206d8946b8db018e1655c089f47b7428e54c2
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 07:25:43 -0300

    🐕

--- plugins/owner-savefile.js
+++ plugins/owner-savefile.js
@@ -1,11 +1,11 @@
 //creditos a StarLigthsTeam
 import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `Que nombre le pongo al archivo?`, m, adReply)
-    if (!m.quoted.text) return conn.reply(m.chat, `Responder al mensaje!`, m, adReply)
+    if (!text) return conn.reply(m.chat, `Que nombre le pongo al archivo?`, m)
+    if (!m.quoted.text) return conn.reply(m.chat, `Responder al mensaje!`, m)
     let path = `${text}.js`
     await fs.writeFileSync(path, m.quoted.text)
-    await conn.reply(m.chat, `Guardado en ${path}`, m, adReply)
+    await conn.reply(m.chat, `Guardado en ${path}`, m)
 }
 handler.help = ['savefile'].map(v => v + ' <nombre>')
 handler.tags = ['owner']

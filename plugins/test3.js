b784d3e111757bd3502e07d628865f0cc7cc35fa
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 13:34:44 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,14 +1,9 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, text }) => {
-let res = await fetch('https://api.lolhuman.xyz/api/random/nsfw/yaoi?apikey=HIRO')
-if (!res.ok) throw await `${res.status} ${res.statusText}`;
-let json = await res.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Lu pasti gay kntl', '© SHIRAORI BOT', 'Next', '.yaoi', m)
-}
+	conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/random/elf?apikey=${global.lolkey}`, 'Kikuchanj.jpg',, m)
+
 handler.command = /^(yaoi)$/i
 handler.tags = ['nsfw']
-handler.help = ['yaoi']
+handler.help = ['XD']
 
 export default handler

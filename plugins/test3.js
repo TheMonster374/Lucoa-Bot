c6ee19757a349cb36ae0bc7eeb7149567f0cd1b6
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 12:39:36 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -17,7 +17,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
       name = conn.getName(who);
     }
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://nekos.best/kitsune`);
+    let apislap = await fetch(`https://nekos.best/api/v2/waifu`);
     let jkis = await apislap.json();
     let { url } = jkis;
     let stiker = await sticker(null, url, `${name2} le dio una bofetada a ${name}`, null);

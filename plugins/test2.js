24d9c19a90e4df4f722adc7e6d3e055120347f7a
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 11:21:58 -0300

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -3,10 +3,11 @@ import fetch from "node-fetch";
 const handler = async (m, { conn, args, usedPrefix, command }) => {
     try {
     let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://api.waifu.im/maid`);
+    let apislap = await fetch(`https://api.waifu.im/milf`);
     let jkis = await apislap.json();
     let { url } = jkis;
-    conn.sendFile(m.chat,  , {quoted: m});
+    let stiker = await sticker(null, url, `Waifu`, `Jotchua-Mini`);
+    conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {forwardingScore: 200, isForwarded: true}}, {quoted: m});
   } catch {
     throw `*ocurrio un error inesperado*`;
   };

d734788c63c85e63c8b901514551f942bfd0d665
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 12:43:55 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,35 +1,50 @@
-import {sticker} from "../lib/sticker.js";
-import fetch from "node-fetch";
-const handler = async (m, { conn, args, usedPrefix, command }) => {
-    let who;
-    if (m.isGroup) {
-      who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
-    } else { 
-      who = m.chat;
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  m.react(rwait);
+
+  let type = (command).toLowerCase();
+  let baseUrl = 'https://weeb-api.vercel.app/';
+
+  const fetchImage = async (endpoint) => {
+    try {
+      const response = await fetch(baseUrl + endpoint);
+      if (!response.ok) throw `❎ Error fetching ${type} image`;
+      const imageBuffer = await response.buffer(); // Get the image data as a buffer
+      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random ${type}`, m);
+      m.react(dmoji);
+    } catch (error) {
+      console.error(error);
+      m.reply(`❎ An error occurred while fetching the ${type} image.`);
     }
-      const textquien = `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙾 𝙼𝙴𝙽𝙲𝙸𝙾𝙽𝙰 𝙰 𝙰𝙻𝙶𝚄𝙸𝙴𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n◉ ${usedPrefix + command} @${global.suittag}`;
-    if (who === m.chat && m.isGroup || !who && m.isGroup) return m.reply(textquien, m.chat, {mentions: conn.parseMention(textquien)});
-  try {
-    let name;
-    if (who === m.chat) {
-      name = "xd";
-    } else {
-      name = conn.getName(who);
-    }
-    let name2 = conn.getName(m.sender);
-    let apislap = await fetch(`https://nekos.best/api/v2/cry`);
-    let jkis = await apislap.json();
-    let { url } = jkis;
-    let stiker = await sticker(null, url, `${name2} le dio una bofetada a ${name}`, null);
-    conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {forwardingScore: 200, isForwarded: true}}, {quoted: m});
-  } catch {
-    throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*`;
   };
+
+  switch (type) {
+    case 'loli':
+      fetchImage('loli');
+      break;
+
+    case 'waifu':
+      fetchImage('waifu');
+      break;
+
+    case 'neko':
+      fetchImage('neko');
+      break;
+
+    case 'zerotwo':
+      fetchImage('zerotwo');
+      break;
+
+    default:
+      
+      break;
+  }
 };
-handler.help = ["slap"];
-handler.tags = ["General"];
-handler.command = /^(test3)$/i;
-export default handler;
+
+handler.help = ['waifu', 'neko', 'zerotwo', 'loli']
+handler.tags = ['anime']
+handler.command = ['waifu', 'neko', 'zerotwo', 'loli'] 
 
 
-
+export default handler

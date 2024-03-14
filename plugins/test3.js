186e385ca8d81bf0a4fe573799ade533450cd6df
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 12:50:02 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,24 +1,40 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  m.react(rwait);
 
   let type = (command).toLowerCase();
-  let baseUrl = 'https://nekos.best/api/v2/kitsune';
+  let baseUrl = 'https://nekos.best/api/v2/';
 
   const fetchImage = async (endpoint) => {
     try {
       const response = await fetch(baseUrl + endpoint);
-      if (!response.ok) throw `❎ Error fetching kitsune image`;
+      if (!response.ok) throw `❎ Error fetching ${type} image`;
       const imageBuffer = await response.buffer(); // Get the image data as a buffer
-      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random kitsune`, m);
+      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random ${type}`, m);
+      m.react(dmoji);
     } catch (error) {
       console.error(error);
       m.reply(`❎ An error occurred while fetching the ${type} image.`);
     }
   };
+
+  switch (type) {
+    case 'husbando':
+      fetchImage('husbando');
+      break;
+
+    case 'kitsune':
+      fetchImage('kitsune');
+      break;
+
+    default:
+      
+      break;
+  }
 };
 
-handler.command = ['ktisune'] 
+handler.command = ['husbando', 'kitsune'] 
 
 
 export default handler

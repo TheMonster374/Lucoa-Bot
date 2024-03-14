e7c8b628e73c2e68b739a37878b878323783025a
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-14 15:55:35 -0500

    index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Jotchua Mini', {
+say('INABAKUMORI-BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`by als`, {
+say(`By Monster`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});

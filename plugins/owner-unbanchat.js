fbea0d7acc62564d215d15ef4298e88f7aed8076
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 07:30:45 -0300

    🐕

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[🔓] 𝑪𝒉𝒂𝒕 𝑫𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐*');
+  m.reply('*_[🔓] Chat Desbaneado, ahora respondere a todos los comandos_*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];

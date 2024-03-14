cf87e8bad22e7b98dd9f40d3a45431ba857eddeb
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 13:11:32 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -5,12 +5,12 @@ const handler = async (m, {conn}) => {
   try {
     if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender);
     if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
-    const res = await fetch('https://api.waifu.pics/sfw/pat');
+    const res = await fetch('https://weeb-api.vercel.app/zerotwo');
     const json = await res.json();
     const {url} = json;
     const stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`);
     conn.sendFile(m.chat, stiker, null, {asSticker: true});
   } catch (e) { }
 };
-handler.command = /^(pat|palmaditas|cariños|mimos|patt)$/i;
+handler.command = /^(test4)$/i;
 export default handler;

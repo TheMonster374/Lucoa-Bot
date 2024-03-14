8a44f451f63d495af9e91ace11f1010e551ce6e4
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 12:46:33 -0300

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -3,46 +3,22 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   let type = (command).toLowerCase();
-  let baseUrl = 'https://weeb-api.vercel.app/';
+  let baseUrl = 'https://nekos.best/api/v2/kitsune';
 
   const fetchImage = async (endpoint) => {
     try {
       const response = await fetch(baseUrl + endpoint);
       if (!response.ok) throw `❎ Error fetching ${type} image`;
       const imageBuffer = await response.buffer(); // Get the image data as a buffer
-      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random ${type}`, m);
+      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random kitsune`, m);
     } catch (error) {
       console.error(error);
       m.reply(`❎ An error occurred while fetching the ${type} image.`);
     }
   };
-
-  switch (type) {
-    case 'loli':
-      fetchImage('loli');
-      break;
-
-    case 'waifu':
-      fetchImage('waifu');
-      break;
-
-    case 'neko':
-      fetchImage('neko');
-      break;
-
-    case 'zerotwo':
-      fetchImage('zerotwo');
-      break;
-
-    default:
-      
-      break;
-  }
 };
 
-handler.help = ['waifu', 'neko', 'zerotwo', 'loli']
-handler.tags = ['anime']
-handler.command = ['waifu', 'neko', 'zerotwo', 'loli'] 
+handler.command = ['test3'] 
 
 
 export default handler

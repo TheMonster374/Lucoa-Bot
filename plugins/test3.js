50f6dec831716899f2985664467a90856f70f039
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-14 20:03:04 -0300

    🤍

--- plugins/test3.js
+++
@@ -1,9 +0,0 @@
-let fetch = require('node-fetch')
-
-	conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/random/elf?apikey=${global.lolkey}`, 'Kikuchanj.jpg',, m)
-
-handler.command = /^(yaoi)$/i
-handler.tags = ['nsfw']
-handler.help = ['XD']
-
-export default handler

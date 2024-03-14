3411272eb9f36dbc2f1121bc08d70db7c923fc23
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 08:52:38 -0300

    🐕

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,11 +1,11 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {args}) {
-  if (!args[0]) throw '𝒊𝒏𝒈𝒓𝒆𝒔𝒂 𝒕𝒖 𝒏𝒖𝒎𝒆𝒓𝒐 𝒅𝒆 𝒔𝒆𝒓𝒊𝒆, 𝒔𝒊 𝒏𝒐 𝒍𝒐 𝒓𝒆𝒄𝒖𝒆𝒓𝒅𝒂𝒔 𝒑𝒖𝒆𝒅𝒆𝒔 𝒖𝒔𝒂𝒓 #myns*';
+  if (!args[0]) throw 'ingresa tu numero de serie\n si no lo recuerdas puedes usar *#myns*';
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw '𝒏𝒖𝒎𝒆𝒓𝒐 𝒅𝒆 𝒔𝒆𝒓𝒊𝒆 𝒊𝒏𝒄𝒐𝒓𝒓𝒆𝒄𝒕𝒐, 𝒄𝒐𝒎𝒑𝒓𝒖𝒆𝒃𝒂 𝒒𝒖𝒆 𝒍𝒐 𝒉𝒂𝒚𝒂𝒔 𝒆𝒔𝒄𝒓𝒊𝒕𝒐 𝒃𝒊𝒆𝒏\n\n𝒔𝒊 𝒏𝒐 𝒍𝒐 𝒓𝒆𝒄𝒖𝒆𝒓𝒅𝒂𝒔 𝒑𝒖𝒆𝒅𝒆𝒔 𝒖𝒔𝒂𝒓 𝒆𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐 #myns';
+  if (args[0] !== sn) throw 'numero incorrecto\n\n comprueba que lo hayas escrito bien\n\nsi no lo recuerdas puedes usar el comando *#myns*';
   user.registered = false;
-  m.reply(`𝒆𝒍𝒊𝒎𝒊𝒏𝒂𝒄𝒊𝒐𝒏 𝒆𝒙𝒊𝒕𝒐𝒔𝒂, 𝒚𝒂 𝒏𝒐 𝒆𝒔𝒕𝒂𝒔 𝒓𝒆𝒈𝒊𝒔𝒕𝒓𝒂𝒅𝒐 𝒆𝒏 𝒆𝒍 𝒃𝒐𝒕`);
+  m.reply(`eliminacion exitosa \n ya no estas registrado en el bot`);
 };
 handler.help = ['unreg <numero de serie>']
 handler.tags = ['rg'];

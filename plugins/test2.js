3a3186c98eac6529aa18588ebd8cf49813c0954d
ALS-4 <163400434+ALS-4@users.noreply.github.com>
2024-03-14 11:04:17 -0300

    Update test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -1,5 +1,16 @@
-const handler = async (m, {conn}) => {
-  conn.sendFile(m.chat, `https://api.waifu.im/fav`, 'error.webp', '', m, {asSticker: true});
+import {sticker} from "../lib/sticker.js";
+import fetch from "node-fetch";
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    try {
+    let name2 = conn.getName(m.sender);
+    let apislap = await fetch(`https://nekos.pro/api/nsfw-neko`);
+    let jkis = await apislap.json();
+    let { url } = jkis;
+    let stiker = await sticker(null, url, `Waifu`, `Jotchua-Mini`);
+    conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {forwardingScore: 200, isForwarded: true}}, {quoted: m});
+  } catch {
+    throw `*ocurrio un error inesperado*`;
+  };
 };
 handler.tags = [''];
 handler.command = ['test2'];

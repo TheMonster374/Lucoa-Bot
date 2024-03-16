66c15c6a2fd3443905d9691e33c5c1c71890c43c
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-16 18:11:43 -0300

    Update fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,3 +1,22 @@
+//creditos a ALS
+
+const handler = async (m, {conn}) => {
+ const vn = './storagemp3/gay2.mp3';
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*ostia puta un maricon*', m);
+  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+handler.help = ['gay'];
+handler.tags = ['logo'];
+handler.command = /^(gay)$/i;
+export default handler;
+
+
+
+
+/*
 let handler = async (m, { conn }) => {
 let vn = './storagemp3/gay2.mp3'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
@@ -12,3 +31,4 @@ handler.help = ['gay']
 handler.tags = ['maker']
 handler.command = /^(gay)$/i
 export default handler
+*/

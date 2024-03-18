7a642dde8e087ef39aa1f27c27c120aaa96a8157
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-18 02:07:51 -0500

    grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -30,7 +30,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)
 }} 
 handler.help = ['kick']
-handler.tags = ['group']
+handler.tags = ['grupos']
 handler.command = /^(kick|echar|hechar|sacar|ban)$/i
 handler.admin = handler.group = handler.botAdmin = true
 export default handler

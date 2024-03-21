4ce127b0eaa7d8e549f17e266a8c3e53bfc32f87
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-21 00:37:34 -0500

    nsfw-fucktag.js

--- plugins/nsfw-fucktag.js
+++ plugins/nsfw-fucktag.js
@@ -2,41 +2,30 @@ import fetch from "node-fetch";
 
 const handler = async (m, { conn, command, text }) => {
     if (!text) throw `*Por favor, menciona el usuario al que quieres enviar el mensaje.*`;
-    
+
         // Obtiene el usuario mencionado en el texto del comando
-        let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-    let mentionedUser = await conn.getName(m.sender) // Obtenemos el nombre del usuario que envió el mensaje original
-    if (!mentionedUser) throw 'No se pudo obtener el nombre del usuario.'
- 
+        let user = text.trim();
 
         // URL de la API según el comando
-        let apiUrl = 'https://nekos.pro/api';
-        if (command == 'fuck') {
-            apiUrl = 'https://nekos.pro/api/fucking';
-        } 
-
-        // Obtiene la imagen de la API
-        let jkis = await (await fetch(apiUrl)).json();
+        let apiUrl = 'https://api.waifu.pics/nsfw';
+    let jkis = await (await fetch(apiUrl)).json();
         let { url } = jkis;
 
         // Envía el texto mencionando al usuario con la imagen
         conn.reply(m.chat, `
-🤤👅🥵 *TE HAS FOLLADO A ${mentionedUser}*🥵👅🤤
-
-*¡${mentionedUser}!* HAS SIDO VIOLAD@ COMO TE ENCANTA LA PIJA😈
-🤤🥵 *TREMENDA SORRA* 🥵🤤`, null, { mentions: [m.sender] });
+🤤👅🥵 *TE HAN HECHO UNA MMDA*🥵👅🤤
+*¡${user}!* TE MAMO EL MONSTRUO 😈
+🤤🥵 *¡QUE PERRA ERES* 🥵🤤`, null, { mentions: [m.sender] });
 
         // Envía la imagen obtenida de la API
-        conn.sendFile(m.chat, url, '', '*MIREN A LA MUY PUTA*', m, false, { mimetype: 'video/mp4' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
-}
-        
+        conn.sendFile(m.chat, url, '', '*MIREN A LA MUY PUTA*', m, false, { mimetype: 'image/jpeg' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
+
     } catch {
         throw `*Ocurrió un error inesperado*`;
     }
 };
-
-handler.help = ['fuck'].map((v) => v + ' <@usuario>');
+handler.help = ['blowjob'].map((v) => v + ' <@usuario>');
 handler.tags = ['nsfw'];
-handler.command = /^(fuck)$/i;
+handler.command = /^(blowjob)$/i;
 handler.register = true;
 export default handler;

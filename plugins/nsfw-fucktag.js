aab1fa2eab033f99c305e3eea8fcc161a3ac8722
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-20 23:59:34 -0500

    nsfw-fucktag.js

--- plugins/nsfw-fucktag.js
+++ plugins/nsfw-fucktag.js
@@ -5,7 +5,10 @@ const handler = async (m, { conn, command, text }) => {
         if (!text) throw `*Por favor, menciona el usuario al que quieres enviar el mensaje.*`;
 
         // Obtiene el usuario mencionado en el texto del comando
-        let user = text.trim();
+        let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+    let mentionedUser = await conn.getName(m.sender) // Obtenemos el nombre del usuario que envió el mensaje original
+    if (!mentionedUser) throw 'No se pudo obtener el nombre del usuario.'
+ 
 
         // URL de la API según el comando
         let apiUrl = 'https://nekos.pro/api';
@@ -17,15 +20,15 @@ const handler = async (m, { conn, command, text }) => {
         let jkis = await (await fetch(apiUrl)).json();
         let { url } = jkis;
 
-        // Envía el texto mencionando al usuario con la imagen
+       /* // Envía el texto mencionando al usuario con la imagen
         conn.reply(m.chat, `
 🤤👅🥵 *TE HAS FOLLADO A ${user}*🥵👅🤤
 
 *¡${user}!* HAS SIDO VIOLAD@ COMO TE ENCANTA LA PIJA😈
-🤤🥵 *TREMENDA SORRA* 🥵🤤`, null, { mentions: [m.sender] });
+🤤🥵 *TREMENDA SORRA* 🥵🤤`, null, { mentions: [m.sender] });*/
 
         // Envía la imagen obtenida de la API
-        conn.sendFile(m.chat, url, '', '*MIREN A LA MUY PUTA*', m, false, { mimetype: 'image/jpeg' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
+        conn.sendFile(m.chat, url, '', '*🤤👅🥵 *TE COGISTES A ${mentionedUser}*🥵👅🤤 *¡${mentionedUser}!* HAS SIDO VIOLADO 😈🤤🥵 *¡QUE PERRA ERES* 🥵🤤`, null, { mentions: [m.sender] });*', m, false, { mimetype: 'image/jpeg' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
 
     } catch {
         throw `*Ocurrió un error inesperado*`;

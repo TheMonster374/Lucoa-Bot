5408d86f823a155087a1a86354c60e41e21a5e0b
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-21 00:10:23 -0500

    nsfw-fucktag.js

--- plugins/nsfw-fucktag.js
+++ plugins/nsfw-fucktag.js
@@ -28,8 +28,12 @@ const handler = async (m, { conn, command, text }) => {
 🤤🥵 *TREMENDA SORRA* 🥵🤤`, null, { mentions: [m.sender] });*/
 
         // Envía la imagen obtenida de la API
-        conn.sendFile(m.chat, url, '', '🤤👅🥵 TE COGISTES A ${mentionedUser}🥵👅🤤', null, { mentions: [m.sender] });, m, false, { mimetype: 'image/jpeg' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
+        conn.sendFile(m.chat, url, '', '🤤👅🥵 TE COGISTES A' ${user}🥵👅🤤, null, {
+            mentions: [m.sender] });, m, false, { mimetype: 'image/jpeg' }); // Ajusta el mimetype según el tipo de archivo que obtienes de la API
 
+
+})
+        
     } catch {
         throw `*Ocurrió un error inesperado*`;
     }

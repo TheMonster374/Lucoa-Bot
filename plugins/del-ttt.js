4415b56608384ff516abd5138476b0f217bf34d1
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-21 16:14:57 -0300

    Create del-ttt.js

---
+++ plugins/del-ttt.js
@@ -0,0 +1,10 @@
+//creditos a als xdxd
+import MessageType from '@whiskeysockets/baileys'
+let handler = async (m, { conn, usedPrefix, command }) => {
+let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
+if (room == undefined) return conn.reply(m.chat, `no estás en ninguna partida de tres en raya\n\npara iniciar una nueva sala de juego, puedes usar: ${usedPrefix}ttt partida nueva`, m)
+delete conn.game[room.id]
+await m.reply('[ ✔ ] _se eliminó la sala de tres en raya_')}
+handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i
+handler.fail = null
+export default handler

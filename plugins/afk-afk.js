6f8e55177724217640f1fb3f2ecd27830559e5d9
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-21 12:23:55 -0500

    afk-afk.js

---
+++ plugins/afk-afk.js
@@ -0,0 +1,11 @@
+const handler = async (m, {text}) => {
+  const user = global.db.data.users[m.sender];
+  user.afk = + new Date;
+  user.afkReason = text;
+  m.reply(`*el usuario ${conn.getName(m.sender)} estara inactivo (afk), porfavor no lo etiqueten*\n\n*motivo de la inactividad (afk)${text ? ': ' + text : ''}*
+`);
+};
+handler.help = ['afk [razón]'];
+handler.tags = ['rpg'];
+handler.command = /^afk$/i;
+export default handler;

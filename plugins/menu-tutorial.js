54fcc68ebdcc2cc09769fca6a13487baf622460b
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 06:00:02 -0500

    menu-tutorial.js

---
+++ plugins/menu-tutorial.js
@@ -0,0 +1,49 @@
+let handler = async (m, {usedPrefix}) => {	
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+else who = m.sender
+let name = conn.getName(who) 
+m.reply(`
+┌───⊷ _𝔹𝕀𝔼ℕ𝕍𝔼ℕ𝕀𝔻𝕆_ ⊶
+▢ ¡Hola! *${name}* bienvenido al grupo 
+▢ Yo soy *Inabakumori*, un bot divertido de WhatsApp
+└──────────────
+%readmore
+┌───⊷ *Pero.. ¿Que es un bot? 🤖* ⊶
+▢  Es un robot que responde a ciertos comandos 
+▢  y tiene múltiples funciones, como *descargar música/videos de cualquier plataforma*
+▢  *tiene diferentes IAs y herramientas de moderación ee grupos*
+▢  *¡y muchas mas cosas divertidas!*
+└──────────────
+%readmore
+┌───⊷ *¿Como lo uso?*⊶
+▢  Para usar el bot, primero te tienes que registrar 
+▢  en su base de datos, tranquilo no es nada difícil,
+▢  tienes que escribir el siguiente comando:
+❏ *${usedPrefix}reg [TuNombre].[TuEdad]
+▢
+▢  ¡Y listo, ya puedes usar los diferentes comandos del bot!
+└──────────────
+%readmore
+┌───⊷_*Mas tutoriales (pronto)*_
+❏ *${usedPrefix}tutorpg*
+❏ *${usedPrefix}tutoias*
+❏ *${usedPrefix}tutosubbots*
+❏ *${usedPrefix}tutodescargas*
+❏ *${usedPrefix}tutogrupos (solo admins)*
+❏ *${usedPrefix}tutoenable (solo admins)*
+❏ *${usedPrefix}tutonsfw
+❏ *${usedPrefix}tutojuegos
+❏ *${usedPrefix}tutofun*
+❏ *${usedPrefix}tutotools*
+└──────────────
+)`
+}
+
+handler.command =/^(tutorial|tuto|new|nuevo)/i 
+handler.help = ['tutorial'] 
+handler.tags = ['begin']  
+handler.group = true
+handler.fail = null
+handler.register = false
+export default handler 

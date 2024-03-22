0ceecbdc4b4b1b4eb44cb204f99896135d9899c0
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 04:08:19 -0500

    beginners-tutorial.js

--- plugins/beginners-tutorial.js
+++ plugins/beginners-tutorial.js
@@ -1,10 +1,17 @@
-let handler = async (m, {
-	command,
-	args,
-	text,
-	usedPrefix
-}) => {
- 
+let handler = async (m, {usedPrefix}) => {	
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+else who = m.sender
+let name = conn.getName(who) 
+m.reply(`
+┌───⊷ _BIENVENIDO_ ⊶
+▢ *𝑵𝒐𝒎𝒃𝒓𝒆:* ${name}
+▢ *𝑫𝒊𝒂𝒎𝒂𝒏𝒕𝒆:* ${global.db.data.users[who].limit}💎
+└──────────────
+*𝑵𝒐𝒕𝒂:* 
+*𝑷𝒖𝒆𝒅𝒆𝒔 𝒄𝒐𝒎𝒑𝒓𝒂𝒓 𝒅𝒊𝒂𝒎𝒂𝒏𝒕𝒆 💎 𝑼𝒔𝒂𝒅𝒐 𝒍𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐́*
+❏ *${usedPrefix}buy <cantidad>*
+❏ *${usedPrefix}buyall*`)
 }
 
 handler.command =/^(tutorial|tuto|new|nuevo)/i 

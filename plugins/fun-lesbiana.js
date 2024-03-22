04f8127bcbc4132e46e1ffed09ca5e7b9e9f0603
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 07:07:22 -0500

    fun-lesbiana.js

--- plugins/fun-lesbiana.js
+++ plugins/fun-lesbiana.js
@@ -4,7 +4,7 @@ if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber q ta
    let taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     let porcentaje = Math.floor(Math.random() * 501); // Generar un número aleatorio entre 0 y 500
      conn.reply(m.chat,`*🏳️‍🌈🏳️‍🌈 ${taguser} ES ${porcentaje}% LESBIANA, QUE GAY🏳️‍🌈🏳️‍🌈*`., null, { mentions: [taguser] }
-};
+}
 
 handler.help = ['lesbiana'].map((v) => v + ' <@user>')
 handler.tags = ['fun']

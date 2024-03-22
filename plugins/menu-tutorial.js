120d77cf3f1c45dba628cc927d1e4b52abb897f3
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 06:21:36 -0500

    menu-tutorial.js

--- plugins/menu-tutorial.js
+++ plugins/menu-tutorial.js
@@ -62,8 +62,8 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ❏ *${usedPrefix}tutofun*
 ❏ *${usedPrefix}tutotools*
 └──────────────
-)`
-.trim();
+)
+`.trim();
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
@@ -74,6 +74,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   } catch {
     conn.reply(m.chat, '*[❌] ocurrio un error inesperado al enviar el menu*', m);
       }
+};
 handler.help = ['tutorial'];
 handler.command =/^(tutorial|tuto|new|nuevo)/i ;
 handler.tags = ['begin'];

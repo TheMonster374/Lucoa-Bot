0be6493c803432159546d410a01b4169db7250a6
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 07:50:51 -0500

    Menu-Juegos.js

--- plugins/Menu-Juegos.js
+++ plugins/Menu-Juegos.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const pp = imagen4;
+    const pp = './Menu4.jpg';
     const img = './Menu4.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';

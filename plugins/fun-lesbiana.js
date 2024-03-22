32412da2acaf8067f55179163f959b33884afc43
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 07:09:50 -0500

    fun-lesbiana.js

--- plugins/fun-lesbiana.js
+++ plugins/fun-lesbiana.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber q tan puta es*`
    let taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     let porcentaje = Math.floor(Math.random() * 501); // Generar un número aleatorio entre 0 y 500
-     conn.reply(m.chat,`*🏳️‍🌈🏳️‍🌈 ${taguser} ES ${porcentaje}% LESBIANA, QUE GAY🏳️‍🌈🏳️‍🌈*.`, null, { mentions: [taguser] }
+     conn.reply(m.chat,`*🏳️‍🌈🏳️‍🌈 ${taguser} ES ${porcentaje}% LESBIANA, QUE GAY🏳️‍🌈🏳️‍🌈*.`, null, { mentions: [taguser] })
 }
 
 handler.help = ['lesbiana'].map((v) => v + ' <@user>')

c3a55e32284290497bef36a975cbdb2bd45f0e7c
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-22 03:57:47 -0500

    beginners-tutorial.js

---
+++ plugins/beginners-tutorial.js
@@ -0,0 +1,16 @@
+let handler = async (m, {
+	command,
+	args,
+	text,
+	usedPrefix
+}) => {
+ 
+}
+
+handler.command =/^(tutorial|tuto|new|nuevo)/i 
+handler.help = ['tutorial', 'tuto', 'new', 'nuevo'] 
+handler.tags = ['Principiantes']  
+handler.group = true
+handler.fail = null
+handler.register = false
+export default handler 

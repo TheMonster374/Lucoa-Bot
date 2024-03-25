d42f82a73539074f5931fef91152b1358206f4f5
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-25 00:02:18 -0500

    game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -1,92 +1,83 @@
-/* CREDITOS: https://github.com/FG98F */
+import db from '../lib/database.js'
 
-let handler = async (m, { args, usedPrefix, command, conn }) => {
-let fa = `𝙐𝙨𝙖𝙧 𝙙𝙚𝙡 𝙡𝙖 𝙨𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 𝙢𝙖𝙣𝙚𝙧𝙖:
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    let fa = `
+Cuanto quieres apostar? 
 
-𝙀𝙟𝙚𝙢𝙥𝙡𝙤:
-*${usedPrefix + command} 50*`.trim()
+📌 Ejemplo :
+*${usedPrefix + command}* 100`.trim()
+    if (!args[0]) throw fa
+    if (isNaN(args[0])) throw fa
+    let apuesta = parseInt(args[0])
+    let users = global.db.data.users[m.sender]
+    let time = users.lastslot + 10000
+    if (new Date - users.lastslot < 10000) throw `⏳ Espere ${msToTime(time - new Date())}`
+    if (apuesta < 100) throw '✳️ Mínimo de la apuesta es *100 XP*'
+    if (users.exp < apuesta) {
+        throw `✳️ Tu *XP* no es suficiente`
+    }
 
-if (!args[0]) throw fa
-if (isNaN(args[0])) throw fa
-let apuesta = parseInt(args[0])
-
-let users = global.db.data.users[m.sender]
-
-if (apuesta < 10) throw `𝑫𝒆𝒃𝒆𝒓 𝒅𝒆𝒍 𝒂𝒑𝒐𝒔𝒕𝒂𝒓 𝒖𝒏 𝒎𝒊́𝒏𝒊𝒎𝒐 𝒅𝒆 *10*`
-
-if (users.exp < apuesta) {
-throw `𝑵𝒐 𝒂𝒍𝒄𝒂𝒏𝒛𝒂 𝒑𝒂𝒓𝒂 𝒂𝒑𝒐𝒔𝒕𝒂𝒓 𝒆𝒙𝒑, 𝒍𝒆 𝒓𝒆𝒄𝒐𝒎𝒊𝒆𝒏𝒅𝒐 𝒊𝒏𝒕𝒆𝒓𝒂𝒄𝒕𝒖𝒂𝒓 𝒄𝒐𝒏 𝒆𝒍 𝒃𝒐𝒕 𝒑𝒂𝒓𝒂 𝒐𝒃𝒕𝒆𝒏𝒆𝒓 𝒎𝒂́𝒔 𝒓𝒆𝒄𝒖𝒓𝒔𝒐𝒔`    
+    let emojis = ["🐋", "🐉", "🕊️"];
+    let a = Math.floor(Math.random() * emojis.length);
+    let b = Math.floor(Math.random() * emojis.length);
+    let c = Math.floor(Math.random() * emojis.length);
+    let x = [],
+        y = [],
+        z = [];
+    for (let i = 0; i < 3; i++) {
+        x[i] = emojis[a];
+        a++;
+        if (a == emojis.length) a = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+        y[i] = emojis[b];
+        b++;
+        if (b == emojis.length) b = 0;
+    }
+    for (let i = 0; i < 3; i++) {
+        z[i] = emojis[c];
+        c++;
+        if (c == emojis.length) c = 0;
+    }
+    let end;
+    if (a == b && b == c) {
+        end = `GANASTE 🎁   *+${apuesta + apuesta} XP*`
+        users.exp += apuesta
+    } else if (a == b || a == c || b == c) {
+        end = `🔮 Casi lo logras sigue intentando :) \nTen *+10 XP*`
+        users.exp += 10
+    } else {
+        end = `Perdiste  *-${apuesta} XP*`
+        users.exp -= apuesta
+    }
+    users.lastslot = new Date * 1
+    let name = await conn.getName(m.sender)
+    let fakemsg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "17608914335-1625305606@g.us" } : {}) }, message: { "extendedTextMessage": { "text": `${end}\n• ${name}`, "title": '🐱⸽⃕NʏᴀɴCᴀᴛBᴏᴛ - MD🍁⃨፝⃕✰', 'jpegThumbnail': catalogo}}}
+    return await conn.reply(m.chat,
+        `
+  🎰 | *SLOTS* 
+──────────
+${x[0]} : ${y[0]} : ${z[0]}
+${x[1]} : ${y[1]} : ${z[1]}
+${x[2]} : ${y[2]} : ${z[2]}
+────────── `, fakemsg) 
 }
-if (command == 'slot1') {
-let time = global.db.data.users[m.sender].lastslot + 60000
-if (new Date - users.lastslot < 60000) throw `*𝑽𝒖𝒆𝒍𝒗𝒂 𝒆𝒏 ${msToTime(time - new Date())} 𝑷𝒂𝒓𝒂 𝒄𝒐𝒏𝒕𝒊𝒏𝒖𝒂𝒓 𝒂 𝒑𝒐𝒔𝒕𝒂𝒏𝒅𝒐 𝒆𝒙𝒑* 🎰`
-users.lastslot = new Date * 1
-    
-let emojis = ["🍁", "⚡", "🍇"];
-let a = Math.floor(Math.random() * emojis.length);
-let b = Math.floor(Math.random() * emojis.length);
-let c = Math.floor(Math.random() * emojis.length);
-let x = [],
-y = [],
-z = [];
-for (let i = 0; i < 3; i++) {
-x[i] = emojis[a];
-a++;
-if (a == emojis.length) a = 0;
-}
-for (let i = 0; i < 3; i++) {
-y[i] = emojis[b];
-b++;
-if (b == emojis.length) b = 0;
-}
-for (let i = 0; i < 3; i++) {
-z[i] = emojis[c];
-c++;
-if (c == emojis.length) c = 0;
-}
-let end;
-if (a == b && b == c) {
-end = `✨ *QUE PRO!! HAS GANADO +${apuesta + apuesta} EXP*`
-users.exp += apuesta
-} else if (a == b || a == c || b == c) {
-end = `🙀 *CASI!!, VUELVA A INTENTAR*\n*BONO DE +50 EXP*`
-users.exp += 50
-} else {
-end = `😿 *HA PERDIDO!! ❌ -${apuesta} EXP*`
-users.exp -= apuesta
-}
+handler.help = ['slot *<apuesta>*']
+handler.tags = ['game']
+handler.command = ['slot']
+handler.register = true
 
-await delay(5000)
-let s = `🎰 | *RANURAS* | 🎰 
- ────────
-  ${x[0]} : ${y[0]} : ${z[0]}
-  ${x[1]} : ${y[1]} : ${z[1]}
-  ${x[2]} : ${y[2]} : ${z[2]}
- ────────
-🎰 |   *SLOTS*   | 🎰
-
-${end}`
-await conn.reply(m.chat, s, m)}
-
-const delay = time => new Promise(res => setTimeout(res, time))
+export default handler
 
 function msToTime(duration) {
-var milliseconds = parseInt((duration % 1000) / 100),
-seconds = Math.floor((duration / 1000) % 60),
-minutes = Math.floor((duration / (1000 * 60)) % 60),
-hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+    var milliseconds = parseInt((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
+    hours = (hours < 10) ? "0" + hours : hours
+    minutes = (minutes < 10) ? "0" + minutes : minutes
+    seconds = (seconds < 10) ? "0" + seconds : seconds
 
-return minutes + " m " + seconds + " s " 
+    return minutes + " m " + seconds + " s "
 }
-
-    
-handler.help = ['slot <apuesta>']
-handler.tags = ['game']
-handler.command = ['slot', 'apostar', 'slot1', 'slot2', 'slot3']
-handler.register = true
-export default handler
-    

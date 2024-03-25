7cbe2cf2004b5dfcedc74e42d840e2ca556d167b
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-25 00:30:48 -0500

    Menu-subbots.js

--- plugins/Menu-subbots.js
+++ plugins/Menu-subbots.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const pp = imagen8
-    const img = './Menu5.jpg';
+    const pp = imagen10
+    const img = './Menu7.png';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});

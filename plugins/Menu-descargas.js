9417d110908de6116f3e22eedbb98e70e8fc7817
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-27 00:37:55 -0500

    Menu-descargas.js

--- plugins/Menu-descargas.js
+++ plugins/Menu-descargas.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
     const pp = imagen9
-    const img = './Menu6.jpg';
+    const img = './storage/img/Menu6.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});

c51c4496edd9439551f3176815ec3a0febea0c99
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-27 00:30:03 -0500

    grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   } else throw `⚠️ Ingrese el mensaje de Bienvenida\n\n@user (mención)\n@group (Nombre de grupo)\n@desc (description de grupo)\n@bio (Biografía)\n@time (Hora local)\n@date (Fecha)`
 }
 handler.help = ['setwelcome *<text>*']
-handler.tags = ['group']
+handler.tags = ['grupo']
 handler.command = ['setwelcome'] 
 handler.admin = true
 handler.owner = false

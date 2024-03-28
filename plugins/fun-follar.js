94bb66c4c68e930e79556b854f7d2f538fca6912
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 05:13:57 -0500

    fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
  /*let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender*/
 try {
- const pp = imagen9
+ const pp = img6
  const user = global.db.data.users[m.sender];
 const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
  const str = `

c80a9193842450e5f85e8d68d8f9f51fcaddfb86
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 10:37:16 -0500

    fun-gay2.js

--- plugins/fun-gay2.js
+++ plugins/fun-gay2.js
@@ -1,8 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber q tan puta es*`
-  let user = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+/*  let user = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;*/
     let porcentaje = Math.floor(Math.random() * 501); // Generar un número aleatorio entre 0 y 500
-    conn.reply(m.chat, `*🏳️‍🌈🏳️‍🌈 ${user} ES ${porcentaje}% GAY, QUE GAY🏳️‍🌈🏳️‍🌈*.`, null, { mentions: [user] })
+    let gay2 = `*🏳️‍🌈🏳️‍🌈 ${text} ES ${porcentaje}% GAY, QUE GAY🏳️‍🌈🏳️‍🌈*.
+    `.trim();
+m.reply(gay2, null, { mentions: conn.parseMention(gay2) })}
 }
 
 handler.help = ['gay2'].map((v) => v + ' <@user>')

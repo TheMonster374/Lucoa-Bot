4fc8a66357efc1efc8cfc7338d9b47e7db814167
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 13:28:41 -0500

    fun-puta.js

--- plugins/fun-puta.js
+++ plugins/fun-puta.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber q tan puta es*`
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-    let porcentaje = Math.floor(Math.random() * 501); // Generar un número aleatorio entre 0 y 500
-    conn.reply(m.chat, `*${user} ES ${porcentaje}% PUTA, ESCRIBANLE AL PRIV*.`, null, { mentions: [user] })
-}
+let gay2 = `
+ *${text} ES ${Math.floor(Math.random() * 600)}% PUTA, ESCRIBANLE AL PRIV*.
+    `.trim(); m.reply(gay2, null, { mentions: conn.parseMention(gay2) })}
+
 
 handler.help = ['puta'].map((v) => v + ' <@user>')
 handler.tags = ['fun']

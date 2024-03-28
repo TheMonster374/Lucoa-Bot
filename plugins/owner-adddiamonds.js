3d6277a9c0fcf3d1d603d21f5c8ea45ccfba75a3
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 00:33:18 -0500

    owner-adddiamonds.js

---
+++ plugins/owner-adddiamonds.js
@@ -0,0 +1,34 @@
+import db from '../lib/database.js'
+
+import MessageType from '@whiskeysockets/baileys'
+let impts = 0
+let handler = async (m, { conn, text }) => {
+  let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw '⚠️ *_Taguea al usuario_*'
+  let txt = text.replace('@' + who.split`@`[0], '').trim()
+  if (!txt) throw '⚠️️ *_Ingrese la cantidad de -DIAMANTES- que quiere añadir_*'
+  if (isNaN(txt)) throw ' 🔢 *_sólo números_*'
+  let limit = parseInt(txt)
+  let diamonds = limit
+  let pjk = Math.ceil(xp * impts)
+  diamonds += pjk
+  if (diamonds < 1) throw '⚠️ Mínimo es  *1*'
+  let users = global.db.data.users
+  users[who].exp += xp
+
+  await m.reply(`⊜ *💎 DIAMANTES AÑADIDO*
+┏━━━━━━━━━━━⬣
+┃⋄  *Total:* ${limit}
+┗━━━━━━━━━━━⬣`)
+ conn.fakeReply(m.chat, `⊜ *_Recibiste_* \n\n *_+${limit} DIAMANTES 💎_*`, who, m.text)
+}
+
+handler.help = ['adddiamonds *<@user>*']
+handler.tags = ['owner']
+handler.command = ['adddiamonds'] 
+handler.rowner = true
+
+export default handler
+

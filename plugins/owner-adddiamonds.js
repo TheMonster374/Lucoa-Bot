7764c480a0dfbaae1e6b9a2b9d03d56525177817
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 00:40:04 -0500

    owner-adddiamonds.js

--- plugins/owner-adddiamonds.js
+++ plugins/owner-adddiamonds.js
@@ -10,19 +10,19 @@ let handler = async (m, { conn, text }) => {
   let txt = text.replace('@' + who.split`@`[0], '').trim()
   if (!txt) throw '⚠️️ *_Ingrese la cantidad de -DIAMANTES- que quiere añadir_*'
   if (isNaN(txt)) throw ' 🔢 *_sólo números_*'
-  let limit = parseInt(txt)
-  let diamonds = limit
-  let pjk = Math.ceil(limit * impts)
-  diamonds += pjk
-  if (diamonds < 1) throw '⚠️ Mínimo es  *1*'
+  let diamonds= parseInt(txt)
+  let limit = diamonds 
+  let pjk = Math.ceil(diamonds * impts)
+  limit += pjk
+  if (limit < 1) throw '⚠️ Mínimo es  *1*'
   let users = global.db.data.users
-  users[who].diamonds += limit
+  users[who].diamonds += diamonds 
 
   await m.reply(`⊜ *💎 DIAMANTES AÑADIDO*
 ┏━━━━━━━━━━━⬣
-┃⋄  *Total:* ${limit}
+┃⋄  *Total:* ${diamonds}
 ┗━━━━━━━━━━━⬣`)
- conn.fakeReply(m.chat, `⊜ *_Recibiste_* \n\n *_+${limit} DIAMANTES 💎_*`, who, m.text)
+ conn.fakeReply(m.chat, `⊜ *_Recibiste_* \n\n *_+${diamonds} DIAMANTES 💎_*`, who, m.text)
 }
 
 handler.help = ['adddiamonds *<@user>*']

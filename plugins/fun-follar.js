f9d7d0c3248fdd8dc63cc5e10d841bbc5fd1e110
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 05:19:21 -0500

    fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,4 +1,7 @@
-let handler = async (m, { conn, command, text }) => {
+/*let handler = async (m, { conn, command, text }) => {*/
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
  /*let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender*/
 try {

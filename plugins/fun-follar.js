021d74dee3beceb411719a61885b6202731dc77a
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 04:32:05 -0500

    fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,4 +1,6 @@
-
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender

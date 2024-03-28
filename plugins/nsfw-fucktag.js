66c1b1e2e05533a3792da4b45c388db396c8d5e1
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 15:38:01 -0500

    nsfw-fucktag.js

--- plugins/nsfw-fucktag.js
+++ plugins/nsfw-fucktag.js
@@ -56,6 +56,4 @@ handler.help = ['fuck'].map((v) => v + ' <@usuario>');
 handler.tags = ['nsfw'];
 handler.command = /^(fuck)$/i;
 handler.register = true;
-export default handler;
-
-*/
+export default handler;*/

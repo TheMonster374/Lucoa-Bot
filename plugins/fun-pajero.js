24d9cd34acd752ea717c9c2193aec3e00dd1e2a8
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-28 13:27:33 -0500

    fun-pajero.js

--- plugins/fun-pajero.js
+++ plugins/fun-pajero.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber q tan puta es*`
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-    let porcentaje = Math.floor(Math.random() * 501); // Generar un número aleatorio entre 0 y 500
-    conn.reply(m.chat, `*${user} ES ${porcentaje}% PAJERO, MIREN AL VIRGEN 🫵*.`, null, { mentions: [user] })
-}
+let pajero = `
+    *${text} ES ${Math.floor(Math.random() * 600)}% PAJERO, MIREN AL VIRGEN *.
+    `.trim(); m.reply(pajero, null, { mentions: conn.parseMention(pajero) })}
+
 
 handler.help = ['pajero'].map((v) => v + ' <@user>')
 handler.tags = ['fun']

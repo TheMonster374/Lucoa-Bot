fe24f56ddbfcc5b0361b60a96bf8ace25516db97
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-29 01:48:56 -0500

    nsfw-blowjobtag.js

--- plugins/nsfw-blowjobtag.js
+++ plugins/nsfw-blowjobtag.js
@@ -20,9 +20,9 @@ if (!text) throw `*Por favor, menciona el usuario*`;
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
 
-     *🤤¡TE LA HA CHUPADO* *${text}*!🤤\n
-😈 *¡${text}!* *QUE SORRA ERES* 😈 \n
- *¡COMO TE ENCANTA LA VERGA!* 🥵
+     *🤤¡TE LA HA CHUPADO* ${text}!🤤\n
+😈¡${text}! *QUE SORRA ERES*😈 \n
+ *¡COMO TE ENCANTA LA VERGA!*🥵
   
     `.trim();
     if (m.isGroup) {

8349f8f94aef1337a971ee87abba1fced1c35839
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-29 01:25:15 -0500

    nsfw-cumtag.js

---
+++ plugins/nsfw-cumtag.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `el nsfw esta desactivado`;
+    
+if (!text) throw `*Por favor, menciona el usuario*`;
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = 'https://telegra.ph/file/c24af1bf00858a8e975ef.mp4' 
+ let pp2 = 'https://telegra.ph/file/995b9b523f2986582784f.mp4' 
+ let pp3 = 'https://telegra.ph/file/daccf451c6c918cb399df.mp4'
+ let pp4 = 'https://telegra.ph/file/2b4be684bc51cef39811d.mp4'
+ let pp5 = 'https://telegra.ph/file/982426e4ffcc93025817b.mp4'
+ let pp6 = 'https://telegra.ph/file/ad5cdfbb1e8fc6d81f7fe.mp4'
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+
+    🤤👅🥵 *🤤👅🥵 TE LA HAS CORRIDO EN* *${text}*🥵👅🤤
+*¡${text}!* *TE HAN CUMEADO* 😈
+🤤🥵 *¡EL SEMEN TE FASCINA!* 🥵🤤
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['cum'].map((v) => v + ' <@usuario>');
+handler.tags = ['nsfw'];
+handler.command = /^(cum)$/i;
+handler.register = true;
+export default handler;

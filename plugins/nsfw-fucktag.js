81c4f26d4fc9a4779ace0d7b2b1fff61b88d2190
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-03-29 00:33:52 -0500

    nsfw-fucktag.js

--- plugins/nsfw-fucktag.js
+++ plugins/nsfw-fucktag.js
@@ -10,8 +10,8 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-    🤤👅🥵 *🤤👅🥵 *TE HAS COGIDO A ${taguser}*🥵👅🤤
-*¡${taguser}!* TE HAN VIOLADO 😈
+    🤤👅🥵 *🤤👅🥵 *TE HAS COGIDO A ${text}*🥵👅🤤
+*¡${text}!* TE HAN VIOLADO 😈
 🤤🥵 *¡QUE PERRA ERES* 🥵🤤
     `.trim();
     if (m.isGroup) {

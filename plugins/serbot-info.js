6b3173be1cd95857d56c5a99fdf63817395ce176
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-31 14:08:43 -0300

    Update serbot-info.js

--- plugins/serbot-info.js
+++ plugins/serbot-info.js
@@ -1,18 +1,44 @@
-
 import ws from 'ws';
-async function handler(m, { usedPrefix }) {
-  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map(conn => conn.user)])]
-  let b = users.map((v, i) => `_*${i + 1}.*_ *${v.name}*\nwa.me/${v.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')
- m.reply(` 
-≡ *${mssg.botlist}*
+async function handler(m, { conn: _envio, usedPrefix }) {
+  const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+  function convertirMsADiasHorasMinutosSegundos(ms) {
+  var segundos = Math.floor(ms / 1000);
+  var minutos = Math.floor(segundos / 60);
+  var horas = Math.floor(minutos / 60);
+  var días = Math.floor(horas / 24);
 
-▢ *${mssg.total}:* ${users.length} 
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
 
-${b}`) 
-  
+  var resultado = "";
+  if (días !== 0) {
+    resultado += días + " días, ";
+  }
+  if (horas !== 0) {
+    resultado += horas + " horas, ";
+  }
+  if (minutos !== 0) {
+    resultado += minutos + " minutos, ";
+  }
+  if (segundos !== 0) {
+    resultado += segundos + " segundos";
+  }
+
+  return resultado;
 }
-handler.help = ['serbot']
-handler.tags = ['jadibot']
-handler.command = ['listbot', 'listbots', 'bots', 'bebots', 'botlist'] 
 
-export default handler
+  const message = users.map((v, index) => `*${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n*Link:* wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}infobot\n*Nombre:* ${v.user.name || '-'}\n*tiempo activo:* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n\n');
+  const replyMessage = message.length === 0 ? '*No hay SubBots activos en estos momentos.*' : message;
+  const totalUsers = users.length;
+  const responseMessage = `
+*Subbots Activos* ${totalUsers || '0'}
+
+${replyMessage.trim()}`.trim();
+
+  await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m});
+}
+handler.command = ['subsbots', 'bots', 'jadibots'];
+handler.help = ['bots'];
+handler.tags = ['jadibot'];
+export default handler;

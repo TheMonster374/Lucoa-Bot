3853450fa1822bbe9323406c536d8a28a226f14e
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-02 02:05:43 -0500

    fun-mamada.js

---
+++ plugins/fun-mamada.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, command, text }) => {
+if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
+ let follar = `
+🤤¡TE LA HA CHUPADO ${text}!🤤\n
+😈¡${text}! *QUE SORRA ERES*😈 \n
+ *¡COMO TE ENCANTA LA VERGA!*🥵
+
+`.trim();
+m.reply(follar, null, { mentions: conn.parseMention(follar) })}
+
+handler.help = ['mamadq'].map((v) => v + ' <@user>')
+handler.tags = ['fun']
+handler.command =/^(Mamada|manada)/i
+handler.fail = null
+handler.register = true
+export default handler

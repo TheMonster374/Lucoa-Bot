7da26178df780d4c5e2dcd8e3bc965dfc0be1e12
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-02 23:16:09 -0500

    Update game-adv.pelicula-respuesta.js

--- plugins/game-adv.pelicula-respuesta.js
+++ plugins/game-adv.pelicula-respuesta.js
@@ -4,15 +4,15 @@ const handler = (m) => m;
 handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
-  this.tekateki2 = this.tekateki2 ? this.tekateki2 : {};
- if (!(id in this.tekateki2)) return m.reply('*esa adivinanza ya termino*');
-  if (m.quoted.id == this.tekateki2[id][0].id) {
-    const json = JSON.parse(JSON.stringify(this.tekateki2[id][1]));
+  this.tekatekidos = this.tekatekidos ? this.tekatekidos : {};
+ if (!(id in this.tekatekidos)) return m.reply('*esa adivinanza ya termino*');
+  if (m.quoted.id == this.tekatekidos[id][0].id) {
+    const json = JSON.parse(JSON.stringify(this.tekatekidos[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
-      global.db.data.users[m.sender].exp += this.tekateki2[id][2];
-      m.reply(`*Respuesta correcta!*\n+${this.tekateki2[id][2]} Exp`);
+      global.db.data.users[m.sender].exp += this.tekatekidos[id][2];
+      m.reply(`*Respuesta correcta!*\n+${this.tekatekidos[id][2]} Exp`);
       clearTimeout(this.tekateki2[id][3]);
-      delete this.tekateki2[id];
+      delete this.tekatekidos[id];
     } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras`);
     else m.reply('Respuesta incorrecta!');
   }

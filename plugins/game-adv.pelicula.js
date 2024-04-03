905906924e1b1ea49486ae397e05644a30e0f1ba
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-02 22:45:27 -0500

    game-adv.pelicula.js

--- plugins/game-adv.pelicula.js
+++ plugins/game-adv.pelicula.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 const timeout = 60000;
 const poin = 1000;
 const handler = async (m, {conn, usedPrefix}) => {
-  conn.tekateki = conn.tekateki ? conn.tekateki : {};
+  conn.tekateki2 = conn.tekateki2 ? conn.tekateki2 : {};
   const id = m.chat;
-  if (id in conn.tekateki) {
-    conn.reply(m.chat, '[⚠] Todavia hay adivinanzas sin responder en este chat', conn.tekateki[id][0]);
+  if (id in conn.tekateki2) {
+    conn.reply(m.chat, '[⚠] Todavia hay adivinanzas sin responder en este chat', conn.tekateki2[id][0]);
     throw false;
   }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/peliculas.json`));
-  const json = tekateki[Math.floor(Math.random() * tekateki.length)];
+  const tekateki2 = JSON.parse(fs.readFileSync(`./src/game/peliculas.json`));
+  const json = tekateki2[Math.floor(Math.random() * tekateki2.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
   const caption = `
@@ -17,12 +17,12 @@ const handler = async (m, {conn, usedPrefix}) => {
 *• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *• Bono:* +${poin} Exp
 `.trim();
-  conn.tekateki[id] = [
+  conn.tekateki2[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabo el tiempo!\nla respuesta es: ${json.response}`, conn.tekateki[id][0]);
-      delete conn.tekateki[id];
+      if (conn.tekateki2[id]) await conn.reply(m.chat, `Se acabo el tiempo!\nla respuesta es: ${json.response}`, conn.tekateki2[id][0]);
+      delete conn.tekateki2[id];
     }, timeout)];
 };
 handler.help = ['pelicula'];

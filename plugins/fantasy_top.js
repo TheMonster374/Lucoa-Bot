166323694c57ab5c5ebac7bbce3204c76861d66c
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-03 00:32:58 -0500

    fantasy_top.js

--- plugins/fantasy_top.js
+++ plugins/fantasy_top.js
@@ -454,10 +454,8 @@ ${personajesNoGustados > 0 ? txtDislike : personajesNoGustados}
 `
 //let fake = { contextInfo: { externalAdReply: { title: `🌟 FANTASÍA RPG`, body: `😼 RPG de: » ${conn.getName(userId)}`, sourceUrl: accountsgb.getRandom(), thumbnailUrl: 'https://telegra.ph/file/2bc10639d4f5cf5685185.jpg' }}}
 let image = [
-'https://telegra.ph/file/77cd4b654273b5cde1ce8.jpg', 
-'https://telegra.ph/file/feb1553dffb7410556c8f.jpg',
-'https://telegra.ph/file/343d26ea0d2621d47539c.jpg',
-'https://telegra.ph/file/2bc10639d4f5cf5685185.jpg'
+'https://telegra.ph/file/e98ac8fe1f41e5ee062a3.jpg', 
+'https://telegra.ph/file/f7d7a32e9ef6752de56e7.jpg'
 ].getRandom()
 await conn.sendMessage(m.chat, {image: { url: image }, caption: mensaje.trim(), mentions: conn.parseMention(mensaje) }, fkontak)   
 /*await conn.sendFile(m.chat, 'https://telegra.ph/file/77cd4b654273b5cde1ce8.jpg', 'fantasy.jpg', mensaje.trim(), fkontak, null, {

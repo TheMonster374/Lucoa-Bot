1a54bfe387a62ed1bc505496547034b0c7f723d9
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-02 23:26:48 -0500

    game-adv.pelicula.js

--- plugins/game-adv.pelicula.js
+++ plugins/game-adv.pelicula.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 const timeout = 60000;
 const poin = 1000;
 const handler = async (m, {conn, usedPrefix}) => {
-  conn.tekatekidos = conn.tekatekidos ? conn.tekatekidos : {};
+  conn.advpeli = conn.advpeli ? conn.advpeli : {};
   const id = m.chat;
-  if (id in conn.tekatekidos) {
+  if (id in conn.advpeli) {
     conn.reply(m.chat, '[⚠] Todavia hay adivinanzas sin responder en este chat', conn.tekatekidos[id][0]);
     throw false;
   }
-  const tekatekidos = JSON.parse(fs.readFileSync(`./src/game/peliculas.json`));
-  const json = tekatekidos[Math.floor(Math.random() * tekatekidos.length)];
+  const advpeli = JSON.parse(fs.readFileSync(`./src/game/peliculas.json`));
+  const json = advpeli[Math.floor(Math.random() * advpeli.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
   const caption = `
@@ -17,12 +17,12 @@ const handler = async (m, {conn, usedPrefix}) => {
 *• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *• Bono:* +${poin} Exp
 `.trim();
-  conn.tekatekidos[id] = [
+  conn.advpeli[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekatekidos[id]) await conn.reply(m.chat, `Se acabo el tiempo!\nla respuesta es: ${json.response}`, conn.tekatekidos[id][0]);
-      delete conn.tekatekidos[id];
+      if (conn.advpeli[id]) await conn.reply(m.chat, `Se acabo el tiempo!\nla respuesta es: ${json.response}`, conn.advpeli[id][0]);
+      delete conn.advpeli[id];
     }, timeout)];
 };
 handler.help = ['pelicula'];

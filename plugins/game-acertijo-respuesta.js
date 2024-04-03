a831d3863cb7be8d18ab5fb6645ad03c8fc7bc7b
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-02 23:19:10 -0500

    game-acertijo-respuesta.js

--- plugins/game-acertijo-respuesta.js
+++ plugins/game-acertijo-respuesta.js
@@ -5,7 +5,7 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
- if (!(id in this.tekateki2)) return m.reply('*ese acertijo ya termino*');
+ if (!(id in this.tekateki)) return m.reply('*ese acertijo ya termino*');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {

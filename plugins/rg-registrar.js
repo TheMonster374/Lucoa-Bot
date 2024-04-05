f03a768463651d54f2dcad2ed0ab4dd6c6c5e63b
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-05 11:52:02 -0500

    rg-registrar.js

--- plugins/rg-registrar.js
+++ plugins/rg-registrar.js
@@ -30,7 +30,7 @@ tu numero de serie
 servira por si quieres
 borrar tu registro
 ┗┅ ━━━━━━━━━━━━ ┅ ━`;
-conn.sendMessage(m.chat, { image: { url: pp }, caption: str, contextInfo: { forwardingScore: 9999, externalAdReply: { showAdAttribution: true, title: packname, body: desc, sourceUrl: null, mediaType: 1, thumbnail: imgRegistro }}}, { quoted: m })
+conn.sendMessage(m.chat, { image: { url: pp }, caption: str, contextInfo: { forwardingScore: 9999, externalAdReply: { showAdAttribution: true, title: packname, body: desc, sourceUrl: 'https://chat.whatsapp.com/Lq5aMlyraBfJaZPBififFu', mediaType: 1, thumbnail: imgRegistro }}}, { quoted: m })
   global.db.data.users[m.sender].money += 10000;
   global.db.data.users[m.sender].exp += 5000;
 };

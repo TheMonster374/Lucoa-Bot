d538ada9dabda067d5774b35af4f16ba8f96dc82
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-06 00:50:17 -0500

    grupo-add.js

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -1,56 +1,73 @@
-import fetch from 'node-fetch'
-/**
- * @type {import('@adiwajshing/baileys')}
- */
-const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@whiskeysockets/baileys')).default
-let handler = async (m, { conn, text, participants }) => {
-    let _participants = participants.map(user => user.id)
-    let users = (await Promise.all(
+const {generateWAMessageFromContent, prepareWAMessageMedia, proto} = (await import('@whiskeysockets/baileys')).default;
+
+import fetch from 'node-fetch';
+
+const {getBinaryNodeChild, getBinaryNodeChildren} = (await import('@whiskeysockets/baileys')).default;
+
+const handler = async (m, {conn, text, participants, args}) => {
+
+  try {
+
+    const _participants = participants.map((user) => user.id);
+
+    const users = (await Promise.all(
+
         text.split(',')
-            .map(v => v.replace(/[^0-9]/g, ''))
-            .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
-            .map(async v => [
-                v,
-                await conn.onWhatsApp(v + '@s.whatsapp.net')
-            ])
-    )).filter(v => v[1][0]?.exists).map(v => v[0] + '@c.us')
-    const response = await conn.query({
-        tag: 'iq',
-        attrs: {
-            type: 'set',
-            xmlns: 'w:g2',
-            to: m.chat,
-        },
-        content: users.map(jid => ({
-            tag: 'add',
-            attrs: {},
-            content: [{ tag: 'participant', attrs: { jid } }]
-        }))
-    })
-    const pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
-    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-    const add = getBinaryNodeChild(response, 'add')
-    const participant = getBinaryNodeChildren(add, 'participant')
-    for (const user of participant.filter(item => item.attrs.error == 403)) {
-        const jid = user.attrs.jid
-        const content = getBinaryNodeChild(user, 'add_request')
-        const invite_code = content.attrs.code
-        const invite_code_exp = content.attrs.expiration
-        let teks = `⚠️️ *Al usuario @${jid.split('@')[0]} solo lo pueden agregar sus contactos :'v pero le envié un enlace de invitación*`
-        m.reply(teks, null, {
-            mentions: conn.parseMention(teks)
-        })
-        await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, await conn.getName(m.chat), 'Hola!!, te invito a que te unas a mi grupo,se que te gustará :3', jpegThumbnail)
+
+            .map((v) => v.replace(/[^0-9]/g, ''))
+
+            .filter((v) => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+
+            .map(async (v) => [v, await conn.onWhatsApp(v + '@s.whatsapp.net')]))).filter((v) => v[1][0]?.exists).map((v) => v[0] + '@c.us');
+
+    const response = await conn.query({tag: 'iq', attrs: {type: 'set', xmlns: 'w:g2', to: m.chat}, content: users.map((jid) => ({tag: 'add', attrs: {}, content: [{tag: 'participant', attrs: {jid}}]}))});
+
+    const pp = await conn.profilePictureUrl(m.chat).catch((_) => null);
+
+    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0);
+
+    const add = getBinaryNodeChild(response, 'add');
+
+    const participant = getBinaryNodeChildren(add, 'participant');
+
+    for (const user of participant.filter((item) => item.attrs.error == 403)) {
+
+      const jid = user.attrs.jid;
+
+      const content = getBinaryNodeChild(user, 'add_request');
+
+      const invite_code = content.attrs.code;
+
+      const invite_code_exp = content.attrs.expiration;
+
+      const teks = `*no fue posible añadir a @${jid.split('@')[0]}, esto puede ocurrir porque el numero este incorrecto, la persona se haya salido recientemente del grupo o la persona haya configurado su privacidad de grupos, se le envio la invitacion al grupo en su privado al usuario *`;
+
+      m.reply(teks, null, {mentions: conn.parseMention(teks)});
+
+      const captionn = `Hey!! Hola, me presento, soy gengar - bot, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!`;
+
+      const messaa = await prepareWAMessageMedia({image: jpegThumbnail}, {upload: conn.waUploadToServer});
+
+      const groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({groupInviteMessage: {groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail}}), {userJid: jid});
+
+      await conn.relayMessage(jid, groupInvite.message, {messageId: groupInvite.key.id});
+
     }
-}
-handler.help = ['add'].map(v => v + ' *<Número>*')
-handler.tags = ['grupo']
-handler.command = ['add']
 
-handler.admin = true
-handler.owner = true
-handler.group = true
-handler.botAdmin = true
+  } catch {
 
+    throw '*no fue posible añadir el  numero que ingreso, esto puede ocurrir porque el numero este incorrecto, la persona se haya salido recientemente del grupo o la persona haya configurado su privacidad de grupos, te aconsejamos enviarle la invitacion manualmente𝚃!!*';
 
-export default handler
+  }
+
+};
+
+handler.help = ['add', '+'].map((v) => v + ' número');
+
+handler.tags = ['grupo'];
+
+handler.command = /^(add)$/i;
+
+handler.admin = handler.group = handler.botAdmin = true;
+
+export default handler;

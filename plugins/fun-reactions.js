2fa8e6de87ece1ca7bcb1cbe508294ad6e648666
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 02:26:52 -0500

    Updatefun-reactions.js

--- plugins/fun-reactions.js
+++ plugins/fun-reactions.js
@@ -6,15 +6,17 @@ if (!text) throw `*Por favor, menciona el usuario*`;
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
     
  let pp = 'https://api.otakugifs.xyz/gif?reaction=kiss' 
- 
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+let username = conn.getName(who)
+  
   try {
     const locale = 'es-ES';
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-
-  
+*-${username} HA BESADO A ${taguser} 😳💋_*
     `.trim();
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -32,6 +34,4 @@ handler.tags = ['fun'];
 handler.command = /^(kiss)$/i;
 handler.register = true;
 export default handler;
-                                
-
-export default handler
+                            

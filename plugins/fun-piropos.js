8532444e55709f806f4cc5bb5ede25b47b2c0d5c
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 01:39:58 -0500

    Update fun-piropos.js

--- plugins/fun-piropos.js
+++ plugins/fun-piropos.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch"
 
 let handler = async (m, { conn, text}) => {
 
-await conn.sendInabakumoriBot(m.chat, `*"${pickRandom(global.piropo)}"*\n\n\t- *_By Monster©_*`, await (await fetch('https://telegra.ph/file/3bcae57f89d9289f8e6d4.jpg')).buffer(), 'ꜰʀᴀꜱᴇꜱ ʙʏ ɪɴᴀʙᴀᴋᴜᴍᴏʀɪ-ʙᴏᴛ', 'Monster 🤍', m)
+await conn.sendNyanCat(m.chat, `*"${pickRandom(global.piropo)}"*\n\n\t- *_By Monster©_*`, await (await fetch('https://telegra.ph/file/3bcae57f89d9289f8e6d4.jpg')).buffer(), 'ꜰʀᴀꜱᴇꜱ ʙʏ ɪɴᴀʙᴀᴋᴜᴍᴏʀɪ-ʙᴏᴛ', 'Monster 🤍', m)
   
 }
 handler.tags = ['fun']

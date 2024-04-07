d233399762bb9e9fb8b618861df18f4e0a5de2ce
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 03:41:36 -0500

    Update fun-reactions.js

--- plugins/fun-reactions.js
+++ plugins/fun-reactions.js
@@ -17,12 +17,10 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
         const pp = gifData.url;
 
         if (m.isGroup) {
-            await conn.sendMessageWithMentions(m.chat, str.trim(), m, { quoted: m });
-            await conn.sendMessage(m.chat, { url: pp, mimetype: 'image/gif' }, 'videoMessage', { quoted: m });
+            conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: m });
         } else {
             const fkontak2 = { 'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo' }, 'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 'participant': '0@s.whatsapp.net' };
-            await conn.sendMessageWithMentions(m.chat, str.trim(), m, { quoted: fkontak2 });
-            await conn.sendMessage(m.chat, { url: pp, mimetype: 'image/gif' }, 'videoMessage', { quoted: fkontak2 });
+            conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: fkontak2 });
         }
     } catch {
         conn.reply(m.chat, '*[❌] ocurrió un error inesperado*', m);
@@ -35,44 +33,8 @@ handler.command = /^(kiss)$/i;
 handler.register = true;
 
 export default handler;
-                                   
-                             
+                                      */
                 
-/*import fetch from 'node-fetch';
-
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) throw `*Por favor, menciona el usuario*`;
-    if (usedPrefix == 'a' || usedPrefix == 'A') return;
-
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let mentionedJid = [who];
-    let username = conn.getName(who);
-
-    try {
-        const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-        const str = `_${taguser} HA BESADO A ${text} 😳💋_`;
-        const apiURL = 'https://api.otakugifs.xyz/gif?reaction=kiss&format=gif';
-        const response = await fetch(apiURL);
-        const gifData = await response.json();
-        const pp = gifData.url;
-
-        if (m.isGroup) {
-            conn.sendMessage(m.chat, { url: pp }, { gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: m });
-        } else {
-            const fkontak2 = { 'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo' }, 'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 'participant': '0@s.whatsapp.net' };
-            conn.sendMessage(m.chat, { url: pp }, { gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: fkontak2 });
-        }
-    } catch {
-        conn.reply(m.chat, '*[❌] ocurrió un error inesperado*', m);
-    }
-};
-
-handler.help = ['kiss'].map((v) => v + ' <@usuario>');
-handler.tags = ['fun'];
-handler.command = /^(kiss)$/i;
-handler.register = true;
-
-export default handler;
 
     
 /*import fetch from 'node-fetch';

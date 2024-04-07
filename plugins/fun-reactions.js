3f35011d3cf2c683247f91ffec5ac4364505b712
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 11:07:13 -0500

    Update fun-reactions.js

--- plugins/fun-reactions.js
+++ plugins/fun-reactions.js
@@ -1,24 +1,28 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) throw `*Por favor, menciona el usuario*`;
-    if (usedPrefix == 'a' || usedPrefix == 'A') return;
-
-let pp = "https://cdn.nekos.life/kiss/kiss_024.gif"
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `el nsfw esta desactivado`;
+    
+if (!text) throw `*Por favor, menciona el usuario*`;
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://cdn.nekos.life/kiss/kiss_024.gif"
 let pp2 = "https://cdn.nekos.life/kiss/kiss_051.gif"
 let pp3 = "https://cdn.nekos.life/kiss/kiss_097.gif"
 let pp4 = "https://cdn.nekos.life/kiss/kiss_083.gif"
 let pp5 = "https://cdn.nekos.life/kiss/kiss_081.gif"
 let pp6 = "https://cdn.nekos.life/kiss/kiss_137.gif"
 
-try {
+  try {
     const locale = 'es-ES';
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-    _${taguser} HA BESADO A ${text} 😳💋_
-   
+
+        _${taguser} HA BESADO A ${text} 😳💋_
+ 
+  
     `.trim();
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -35,5 +39,5 @@ handler.help = ['kiss'].map((v) => v + ' <@usuario>');
 handler.tags = ['fun'];
 handler.command = /^(kiss)$/i;
 handler.register = true;
-
 export default handler;
+

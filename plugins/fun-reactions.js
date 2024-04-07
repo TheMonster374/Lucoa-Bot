d4067352903cef9733c4fca7b6f8806b35ab9dc1
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 02:32:20 -0500

    Update fun-reactions.js

--- plugins/fun-reactions.js
+++ plugins/fun-reactions.js
@@ -5,8 +5,13 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!text) throw `*Por favor, menciona el usuario*`;
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
     
- let pp = 'https://api.otakugifs.xyz/gif?reaction=kiss' 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+ let res = await fetch ('https://api.otakugifs.xyz/gif?reaction=kiss')
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.url) throw '*[❌] ocurrio un error inesperado*'
+
+  
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
   
@@ -23,7 +28,7 @@ let username = conn.getName(who)
       conn.sendMessage(m.chat, { video: { url: [pp] }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
     } else {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, { video: { url: [pp] }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      conn.sendFile(m.chat, json.url, 'video.mp4', gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
     conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);

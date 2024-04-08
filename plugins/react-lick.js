91aae826af306c4cd5644273a6a95a37d26a1bbb
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 23:26:05 -0500

    Create react-lick.js

---
+++ plugins/react-lick.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    
+if (!text) throw `*Por favor, menciona el usuario*`;
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://telegra.ph/file/fde24b24b27768a6f7be9.mp4"
+let pp2 = "https://telegra.ph/file/c80df3ecfc5aaeed94a9e.mp4"
+let pp3 = "https://telegra.ph/file/1aeb8420f47b09706689e.mp4"
+let pp4 = "https://telegra.ph/file/4f72f6358d545de0fe5ca.mp4"
+let pp5 = "https://telegra.ph/file/b688f956a3e258339ce80.mp4"
+let pp6 = "https://telegra.ph/file/935e7514679f1525ec8ec.mp4"
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+
+        _${taguser} HA LAMIDO A ${text} 👅👄_
+ 
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['lick'].map((v) => v + ' <@usuario>');
+handler.tags = ['react'];
+handler.command = /^(lick)$/i;
+handler.register = true;
+export default handler;
+
+

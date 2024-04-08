f9cf5cc3e3d4f8617cc3dbe6644f0391ad39dfd5
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-07 22:45:41 -0500

    Update fun-reactions.js

--- plugins/fun-reactions.js
+++ plugins/fun-reactions.js
@@ -1,27 +1,4 @@
-import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import MessageType from '@whiskeysockets/baileys'
-let handler = async (m, { conn}) => {
-try {   
-let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    if (!who) throw `⚠️ *_Etiquete o mencione a alguien._*\n\n*📌 Ejemplo* : ${usedPrefix + command} @tag` 
-    
-    let user = global.db.data.users[who]
-    let name = conn.getName(who) 
-   let name2 = conn.getName(m.sender) 
-let res = await fetch('https://nekos.life/api/kiss')
-let json = await res.json()
-let { url } = json
-let stiker = await sticker(null, url, `${name2} le dio besos a`, `${name}`)
-conn.sendFile(m.chat, stiker, null, { asSticker: true }, m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })
-} catch (e) { }}
-handler.help = ['kiss *<@user>*']
-handler.tags = ['fun']
-handler.command = /^(kiss|skiss|kis|besos|beso)$/i
-export default handler
-/*
+//kiss
 import fetch from 'node-fetch';
 
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
@@ -64,4 +41,49 @@ handler.tags = ['fun'];
 handler.command = /^(kiss)$/i;
 handler.register = true;
 export default handler;
-*/
+
+//pat
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `el nsfw esta desactivado`;
+    
+if (!text) throw `*Por favor, menciona el usuario*`;
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://telegra.ph/file/2fa66eb61a800b0fc622b.mp4"
+let pp2 = "https://telegra.ph/file/07252543eeec9dfa7c8a4.mp4"
+let pp3 = "https://telegra.ph/file/fd2b06eacd36dff5bbba1.mp4"
+let pp4 = "https://telegra.ph/file/338a37f5214bfd59edf0c.mp4"
+let pp5 = "https://telegra.ph/file/b8811e1304623bb29f65a.mp4"
+let pp6 = "https://telegra.ph/file/4a9d8db2e609b1339789a.mp4"
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+
+        _${taguser} HA ACARICIADO A ${text} 🥰🥹_
+ 
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['pat'].map((v) => v + ' <@usuario>');
+handler.tags = ['fun'];
+handler.command = /^(pat)$/i;
+handler.register = true;
+export default handler;
+
+                 

72945d0f2542ba8828a45c89acc0c86589ab09e2
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-08 23:27:09 -0500

    Create react-sleep.js

---
+++ plugins/react-sleep.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://telegra.ph/file/0b6e9f2a45d8dad8200f1.mp4"
+let pp2 = "https://telegra.ph/file/e7a2d692a963a79cac224.mp4"
+let pp3 = "https://telegra.ph/file/f6862af2808b5ff0a1e90.mp4"
+let pp4 = "https://telegra.ph/file/c861b2d3a7254facd7d3e.mp4"
+let pp5 = "https://telegra.ph/file/debd07567ba30fcaf66d1.mp4"
+let pp6 = "https://telegra.ph/file/c06c80e5d943a223327bf.mp4"
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+
+        _${taguser} ESTA CANSADO, SE IRA A DORMIR 😪💤_
+ 
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['sleep']
+handler.tags = ['react'];
+handler.command = /^(sleep)$/i;
+handler.register = true;
+export default handler;
+
+

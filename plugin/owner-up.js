cdd72a74fead18e54866166446e8097e11b544aa
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-11 00:29:55 -0500

    Create owner-up.js

---
+++ plugin/owner-up.js
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    
+if (!text) throw `*Por favor, menciona el usuario*`;
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://telegra.ph/file/808cef2574fefe25de2cd.mp4"
+let pp2 = "https://telegra.ph/file/7900b16e69af07edc3ef1.mp4"
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+*✨ ¡LA ACTUALIZACION [${text}] HA LLEGADO! ✨*
+
+_Notas de la versión:_
+> ⓘ Canal de WhatsApp 
+
+✨𝑷𝒂𝒓𝒂 𝒄𝒐𝒏𝒐𝒄𝒆𝒓 𝒍𝒂𝒔 𝒇𝒖𝒕𝒖𝒓𝒂𝒔 𝒚 𝒓𝒆𝒄𝒊𝒆𝒏𝒕𝒆𝒔 𝒂𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒄𝒊𝒐𝒏𝒆𝒔, 𝑺𝒊𝒈𝒖𝒆 𝒆𝒍 𝒄𝒂𝒏𝒂𝒍 𝒐𝒇𝒊𝒄𝒊𝒂𝒍 𝒅𝒆 𝙄𝙉𝘼𝘽𝘼𝙆𝙐𝙈𝙊𝙍𝙄-𝘽𝙤𝙩 [🤍]:
+ https://whatsapp.com/channel/0029VaPhM3S3wtbBXOzf6t0j
+
+*✨GRUPO OFICIAL✨:*
+https://chat.whatsapp.com/L2uawSeQhM2I2oe0C3K1sS
+
+*GRUPO SIN REGLAS 😈:*
+https://chat.whatsapp.com/Jy7y9ceuHsS5KASLC6p5x2
+
+*🤍 DONACIONES 🤍:*
+https://paypal.me/monster373505?country.x=CO&locale.x=es_XC
+ 
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { image: { url: [pp, pp2].getRandom() }, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { image: { url: [pp, pp2].getRandom() }, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['upt'].map((v) => v + ' [versión]');
+handler.tags = ['owner'];
+handler.command = /^(upt)$/i;
+handlet.owner = true;
+export default handler;
+
+
+        

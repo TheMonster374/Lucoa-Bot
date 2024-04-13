02090e658be9565a017bb3051992091376c74a70
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-12 19:09:53 -0500

    Update rpg-buy.js

--- plugins/rpg-buy.js
+++ plugins/rpg-buy.js
@@ -1,15 +1,15 @@
 const xpperlimit = 350;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / moneyperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
-    global.db.data.users[m.sender].exp -= xpperlimit * count;
+  if (global.db.data.users[m.sender].money >= moneyperlimit * count) {
+    global.db.data.users[m.sender].money -= moneyperlimit * count;
     global.db.data.users[m.sender].limit += count;
     conn.reply(m.chat, `
 ┌─「 *𝗡𝗢𝗧𝗔 𝗗𝗘 𝗣𝗔𝗚𝗢* 」
 ‣ *𝗖𝗼𝗺𝗽𝗿𝗮 𝗻𝗼𝗺𝗶𝗻𝗮𝗹* : + ${count}💎 
-‣ *𝗚𝗮𝘀𝘁𝗮𝗱𝗼* : -${xpperlimit * count} Coins 🪙
+‣ *𝗚𝗮𝘀𝘁𝗮𝗱𝗼* : -${moneyperlimit * count} Coins 🪙
 └──────────────`, m);
   } else conn.reply(m.chat, `no tienes suficientes *Coins* para comprar *${count}* Diamantes💎`, m);
 };

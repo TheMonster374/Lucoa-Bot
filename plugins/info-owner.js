e20f89b44a179e23aae98c9ae287337a032a4938
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-15 07:08:50 -0500

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;◣w◢;;\nFN:◣w◢⁩⁩\nORG:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩\nTITLE:\nitem1.TEL;waid=573017210155:573017210155\nitem1.X-ABLabel:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:◣w◢\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;◣w◢;;\nFN:◣w◢⁩⁩\nORG:Monster  - [◣w◢]⁩\nTITLE:\nitem1.TEL;waid=5493804170925:5493804170925\nitem1.X-ABLabel:Monster  - [◣w◢]\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:◣w◢\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: '◣w◢', contacts: [{ vcard }] }}, {quoted: m})
   await m.reply('enviar mensaje solo para cosas del bot')
 }

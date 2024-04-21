b4a24b2ea72a40a302fa2fdfb1f00f13c6e93c83
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-21 10:45:09 -0500

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;◣w◢;;\nFN:◣w◢⁩⁩\nORG:Monster  - [◣w◢]⁩\nTITLE:\nitem1.TEL;waid=573136737414:573136737414\nitem1.X-ABLabel:Monster  - [◣w◢]\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:◣w◢\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: '◣w◢', contacts: [{ vcard }] }}, {quoted: m})
-  await m.reply('enviar mensaje solo para cosas del bot')
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;◣w◢ Monster;;\nFN:◣w◢⁩⁩Monster\nORG:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩\nTITLE:\nitem1.TEL;waid=573136737414:573136737414\nitem1.X-ABLabel:𝘈𝘓Σ𝘟Σ𝘐  - 🇲🇹⁩⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:◣w◢\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '◣w◢ Monster', contacts: [{ vcard }] }}, {quoted: m})
+  await m.reply('Contactate con mi creador solo para asuntos sobre el bot')
 }
 handler.help = ['owner']
 handler.tags = ['main']

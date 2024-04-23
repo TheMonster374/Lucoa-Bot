137ca844c3f372d6a6402efdcaca8ffc9733d9d9
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-22 23:12:12 -0500

    Update rg-mysn.js

--- plugins/rg-mysn.js
+++ plugins/rg-mysn.js
@@ -1,9 +1,11 @@
 import {createHash} from 'crypto';
+
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-const handler = async function(m, {conn, text, usedPrefix}) {
+let handler = async (m, {conn, usedPrefix, text}) => {
   const sn = createHash('md5').update(m.sender).digest('hex');
-conn.reply(m.chat,`*_Tu Numero de serie es:_*  ${sn}
-`, fkontak, {contextInfo: {
+
+  conn.reply(m.chat,`*_Tu Numero de serie es:_*  ${sn}`, fkontak, 
+{contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
 externalAdReply: {

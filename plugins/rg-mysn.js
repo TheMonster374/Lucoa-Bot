df0af842911ecb98b025e0eb1d31c84d4e49f760
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-04-22 23:06:23 -0500

    Update rg-mysn.js

--- plugins/rg-mysn.js
+++ plugins/rg-mysn.js
@@ -2,7 +2,18 @@ import {createHash} from 'crypto';
 const handler = async function(m, {conn, text, usedPrefix}) {
   const sn = createHash('md5').update(m.sender).digest('hex');
 m.reply(`*_Tu Numero de serie es:_*  ${sn}
-`.trim());
+`.trim(), {contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
+externalAdReply: {
+showAdAttribution: false,
+title: `GRUPO DE WHATSAPP`,
+body: `🤍 Unete al grupo OFC`,
+mediaType: 1,
+sourceUrl: 'https://chat.whatsapp.com/Jql7XsV7V179fvOmKnEftc',
+thumbnailUrl: 'https://i.imgur.com/yafZkFB.jpeg'
+}}}, { quoted: m })
+});
 };
 handler.help = ['myns'];
 handler.tags = ['rg'];

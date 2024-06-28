7877f13a8ff33675f30b6dcdc13fb8264d3e5e4f
TheMonster374 <84290592+TheMonster374@users.noreply.github.com>
2024-06-28 08:39:17 -0500

    Update Menu-nsfw.js

--- plugins/Menu-nsfw.js
+++ plugins/Menu-nsfw.js
@@ -22,113 +22,121 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `
-  *Hola,* ${taguser}
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
 
- ┏━⊜「 *=͟͟͞INFO USUARIO* 」
-┃🤍 *𝑵𝒐𝒎𝒃𝒓𝒆* : *${taguser}
-┃💎 *𝑫𝒊𝒂𝒎𝒂𝒏𝒕𝒆𝒔* : *${limit}
-┃⬆️ *𝑵𝒊𝒗𝒆𝒍* : *${level}
-┃🪙 *Coins* : *${money}
-┗━━━━━━━━━━━━━⬣
+
+
+let uptime = `${clockString(process.uptime())}`
+const str = `
+    > ℍ𝕠𝕝𝕒 *${taguser}*, 𝕊𝕠𝕪 𝕃𝕦𝕔𝕠𝕒
+  
+┏━☋ ꕥ *👤 𝗨𝗦𝗨𝗔𝗥𝗜𝗢* ꕥ
+┃➥🐲 *𝘕𝘰𝘮𝘣𝘳𝘦* : ${taguser}
+┃➥💎 *𝘋𝘪𝘢𝘮𝘢𝘯𝘵𝘦𝘴* : ${limit}
+┃➥🏆 *𝘕𝘪𝘷𝘦𝘭* : ${level}
+┃➥💴 *𝘠𝘦𝘯𝘦𝘴* : ${money}
+┗━━━━━━━━━━━━━☊
  ${readMore}
-┏━⊜「 *=͟͟͞INFO BOT* 」
-┃◣w◢ *CREADOR*: +573017210155
-┃👤 *TOTAL USUARIOS* : ${totalusr}
-┗━━━━━━━━━━━━━⬣
+┏━☋ ꕥ *🐲 𝗟𝗨𝗖𝗢𝗔-𝗕𝗢𝗧* ꕥ
+┃➥🧁 *𝘊𝘳𝘦𝘢𝘥𝘰𝘳𝘢*: 𝒀𝒖𝒏𝒏𝒚𝒔
+┃➥👤 *𝘜𝘴𝘶𝘢𝘳𝘪𝘰𝘴* : ${totalusr}
+┃➥🕘 *𝘈𝘤𝘵𝘪𝘷𝘢* : ${uptime}
+┃➥✨ *𝘛𝘰𝘵𝘢𝘭 𝘨𝘳𝘶𝘱𝘰𝘴* : ${groupsIn.length}
+┗━━━━━━━━━━━━━☊
  ${readMore}
 
-┏━⊜ NSFW 🔞╗
+❥━☋《ɴꜱꜰᴡ》🔞
 ╭─────────────···
-┃│ ${usedPrefix}fuck <@usuario>
-┃│ ${usedPrefix}cum <@usuario>
-┃│ ${usedPrefix}blowjob <@usuario>
-┃│ ${usedPrefix}genshin
-┃│ ${usedPrefix}swimsuit
-┃│ ${usedPrefix}schoolswimsuit
-┃│ ${usedPrefix}white
-┃│ ${usedPrefix}barefoot
-┃│ ${usedPrefix}touhou
-┃│ ${usedPrefix}gamecg
-┃│ ${usedPrefix}hololive
-┃│ ${usedPrefix}uncensored
-┃│ ${usedPrefix}sunglasses
-┃│ ${usedPrefix}glasses
-┃│ ${usedPrefix}weapon
-┃│ ${usedPrefix}shirtlift
-┃│ ${usedPrefix}chain
-┃│ ${usedPrefix}fingering
-┃│ ${usedPrefix}flatchest
-┃│ ${usedPrefix}torncloth
-┃│ ${usedPrefix}bondage
-┃│ ${usedPrefix}demon
-┃│ ${usedPrefix}wet
-┃│ ${usedPrefix}pantypull
-┃│ ${usedPrefix}headdress
-┃│ ${usedPrefix}headphone
-┃│ ${usedPrefix}tie
-┃│ ${usedPrefix}anusview
-┃│ ${usedPrefix}shorts
-┃│ ${usedPrefix}stokings
-┃│ ${usedPrefix}topless
-┃│ ${usedPrefix}beach
-┃│ ${usedPrefix}bunnygirl
-┃│ ${usedPrefix}bunnyear
-┃│ ${usedPrefix}idol
-┃│ ${usedPrefix}vampire
-┃│ ${usedPrefix}gun
-┃│ ${usedPrefix}maid
-┃│ ${usedPrefix}bra
-┃│ ${usedPrefix}nobra
-┃│ ${usedPrefix}bikini
-┃│ ${usedPrefix}whitehair
-┃│ ${usedPrefix}blonde
-┃│ ${usedPrefix}pinkhair
-┃│ ${usedPrefix}bed
-┃│ ${usedPrefix}ponytail
-┃│ ${usedPrefix}nude
-┃│ ${usedPrefix}dress
-┃│ ${usedPrefix}underwear
-┃│ ${usedPrefix}foxgirl
-┃│ ${usedPrefix}uniform
-┃│ ${usedPrefix}skirt
-┃│ ${usedPrefix}sex
-┃│ ${usedPrefix}sex2
-┃│ ${usedPrefix}sex3
-┃│ ${usedPrefix}breast
-┃│ ${usedPrefix}twintail
-┃│ ${usedPrefix}spreadpussy
-┃│ ${usedPrefix}tears
-┃│ ${usedPrefix}seethrough
-┃│ ${usedPrefix}breasthold
-┃│ ${usedPrefix}drunk
-┃│ ${usedPrefix}fateseries
-┃│ ${usedPrefix}spreadlegs
-┃│ ${usedPrefix}openshirt
-┃│ ${usedPrefix}headband
-┃│ ${usedPrefix}food
-┃│ ${usedPrefix}close
-┃│ ${usedPrefix}tree
-┃│ ${usedPrefix}nipples
-┃│ ${usedPrefix}erectnipples
-┃│ ${usedPrefix}horns
-┃│ ${usedPrefix}greenhair
-┃│ ${usedPrefix}wolfgirl
-┃│ ${usedPrefix}catgirl
-┃│ ${usedPrefix}fucking
-┃│ ${usedPrefix}vtuber
-┃│ ${usedPrefix}xneko
-┃│ ${usedPrefix}xnxx
-┃│ ${usedPrefix}xnxxsearch <query>
-┃│ ${usedPrefix}nsfw
-┃│ ${usedPrefix}tentaculos
-┃│ ${usedPrefix}nsfwloli
-┃│ ${usedPrefix}nsfwcum
-┃│ ${usedPrefix}nsfwbdsm
-┃│ ${usedPrefix}hentaipdf
-┃│ ${usedPrefix}rule34
+┃➽ _${usedPrefix}fuck <@usuario>_
+┃➽ _${usedPrefix}cum <@usuario>_
+┃➽ _${usedPrefix}blowjob <@usuario>_
+┃➽ _${usedPrefix}genshin_
+┃➽ _${usedPrefix}swimsuit_
+┃➽ _${usedPrefix}schoolswimsuit_
+┃➽ _${usedPrefix}white_
+┃➽ _${usedPrefix}barefoot_
+┃➽ _${usedPrefix}touhou_
+┃➽ _${usedPrefix}gamecg_
+┃➽ _${usedPrefix}hololive_
+┃➽ _${usedPrefix}uncensored_
+┃➽ _${usedPrefix}sunglasses_
+┃➽ _${usedPrefix}glasses_
+┃➽ _${usedPrefix}weapon_
+┃➽ _${usedPrefix}shirtlift_
+┃➽ _${usedPrefix}chain_
+┃➽ _${usedPrefix}fingering_
+┃➽ _${usedPrefix}flatchest_
+┃➽ _${usedPrefix}torncloth_
+┃➽ _${usedPrefix}bondage_
+┃➽ _${usedPrefix}demon_
+┃➽ _${usedPrefix}wet_
+┃➽ _${usedPrefix}pantypull_
+┃➽ _${usedPrefix}headdress_
+┃➽ _${usedPrefix}headphone_
+┃➽ _${usedPrefix}tie_
+┃➽ _${usedPrefix}anusview_
+┃➽ _${usedPrefix}shorts_
+┃➽ _${usedPrefix}stokings_
+┃➽ _${usedPrefix}topless_
+┃➽ _${usedPrefix}beach_
+┃➽ _${usedPrefix}bunnygirl_
+┃➽ _${usedPrefix}bunnyear_
+┃➽ _${usedPrefix}idol_
+┃➽ _${usedPrefix}vampire_
+┃➽ _${usedPrefix}gun_
+┃➽ _${usedPrefix}maid_
+┃➽ _${usedPrefix}bra_
+┃➽ _${usedPrefix}nobra_
+┃➽ _${usedPrefix}bikini_
+┃➽ _${usedPrefix}whitehair_
+┃➽ _${usedPrefix}blonde_
+┃➽ _${usedPrefix}pinkhair_
+┃➽ _${usedPrefix}bed_
+┃➽ _${usedPrefix}ponytail_
+┃➽ _${usedPrefix}nude_
+┃➽ _${usedPrefix}dress_
+┃➽ _${usedPrefix}underwear_
+┃➽ _${usedPrefix}foxgirl_
+┃➽ _${usedPrefix}uniform_
+┃➽ _${usedPrefix}skirt_
+┃➽ _${usedPrefix}sex_
+┃➽ _${usedPrefix}sex2_
+┃➽ _${usedPrefix}sex3_
+┃➽ _${usedPrefix}breast_
+┃➽ _${usedPrefix}twintail_
+┃➽ _${usedPrefix}spreadpussy_
+┃➽ _${usedPrefix}tears_
+┃➽ _${usedPrefix}seethrough_
+┃➽ _${usedPrefix}breasthold_
+┃➽ _${usedPrefix}drunk_
+┃➽ _${usedPrefix}fateseries_
+┃➽ _${usedPrefix}spreadlegs_
+┃➽ _${usedPrefix}openshirt_
+┃➽ _${usedPrefix}headband_
+┃➽ _${usedPrefix}food_
+┃➽ _${usedPrefix}close_
+┃➽ _${usedPrefix}tree_
+┃➽ _${usedPrefix}nipples_
+┃➽ _${usedPrefix}erectnipples_
+┃➽ _${usedPrefix}horns_
+┃➽ _${usedPrefix}greenhair_
+┃➽ _${usedPrefix}wolfgirl_
+┃➽ _${usedPrefix}catgirl_
+┃➽ _${usedPrefix}fucking_
+┃➽ _${usedPrefix}vtuber_
+┃➽ _${usedPrefix}xneko_
+┃➽ _${usedPrefix}xnxx_
+┃➽ _${usedPrefix}xnxxsearch <query>_
+┃➽ _${usedPrefix}nsfw_
+┃➽ _${usedPrefix}tentaculos_
+┃➽ _${usedPrefix}nsfwloli_
+┃➽ _${usedPrefix}nsfwcum_
+┃➽ _${usedPrefix}nsfwbdsm_
+┃➽ _${usedPrefix}hentaipdf_
+┃➽ _${usedPrefix}rule34_
 ╰─────────────···
-┗━━━━━━━━━━━━━⬣
+❥━━━━━━━━━━━━━☊
       
 
 `.trim();
@@ -140,7 +148,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch (e) {
-    conn.reply(m.chat, '*[❌] ocurrio un error inesperado al enviar el menu*', m);
+conn.reply(m.chat, '[❌] ERROR \n\n El menu tiene un error', m);
        throw (e)
   }
 };
